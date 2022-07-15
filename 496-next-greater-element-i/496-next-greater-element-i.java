16763d71bf21ebeacf8b1e89106e450e1315c798
vikashsingh-15 <55023442+vikashsingh-15@users.noreply.github.com>
2022-07-15 17:50:05 +0530

    Time: 1 ms (99.99%), Space: 42.4 MB (89.14%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -2,34 +2,61 @@ class Solution {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        Stack<Integer> stk=new Stack<>();
-        
-        int sol []=new int [nums1.length];   
-        
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<nums2.length; i++){
-            map.put(nums2[i],i);
-        }  
-        for(int i=0;i<nums1.length;i++){
-            int pos=map.get(nums1[i]);
-             stk=new Stack<>();
-             stk.push(nums2[nums2.length-1]);
-            
-            for(int j=nums2.length-1; j>=pos; j--){
-                while(stk.size()>0 && stk.peek()<=nums1[i]){
-                    stk.pop();
+        Map<Integer, Integer> keyIdx = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            keyIdx.put(nums2[i], i);
+        }
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = -1;
+            int key = nums1[i];
+            int idx = keyIdx.get(key);
+            for (int j = idx + 1; j < nums2.length; j++) {
+                if (nums2[j] > nums1[i]) {
+                    ans[i] = nums2[j];
+                    break;
                 }
-                if(stk.size()==0){
-                    sol[i]=-1;
-                }
-                else{
-                    sol[i]=stk.peek();
-                } 
-                stk.push(nums2[j]);
             }
         }
+
+        return ans;
         
-        return sol;
+        
+        
+        
+        
+        
+        
+        
+        
+//         Stack<Integer> stk=new Stack<>();
+        
+//         int sol []=new int [nums1.length];   
+        
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int i=0; i<nums2.length; i++){
+//             map.put(nums2[i],i);
+//         }  
+//         for(int i=0;i<nums1.length;i++){
+//             int pos=map.get(nums1[i]);
+//              stk=new Stack<>();
+//              stk.push(nums2[nums2.length-1]);
+            
+//             for(int j=nums2.length-1; j>=pos; j--){
+//                 while(stk.size()>0 && stk.peek()<=nums1[i]){
+//                     stk.pop();
+//                 }
+//                 if(stk.size()==0){
+//                     sol[i]=-1;
+//                 }
+//                 else{
+//                     sol[i]=stk.peek();
+//                 } 
+//                 stk.push(nums2[j]);
+//             }
+//         }
+        
+//         return sol;
         
         
         
