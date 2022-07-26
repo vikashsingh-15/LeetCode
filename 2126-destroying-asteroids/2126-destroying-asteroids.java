588a5161d2ce6fcea4ea4a9d76f39be4b4b5e133
vikashsingh-15 <55023442+vikashsingh-15@users.noreply.github.com>
2022-07-26 18:29:59 +0530

    Time: 37 ms (23.39%), Space: 101 MB (57.60%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] arr) {
+        Arrays.sort(arr);
+        long sum=mass;
+        
+        for(int i=0;i<arr.length;i++){
+            if(sum>=arr[i]){
+                sum+=arr[i];
+            }
+            
+            else {
+                return false;
+            }
+        }
+        
+        return true;
+        
+        
+    }
+}
