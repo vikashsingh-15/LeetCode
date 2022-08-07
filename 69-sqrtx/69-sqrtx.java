6cbdab5430510c5f437ffba49ec822376172f550
vikashsingh-15 <55023442+vikashsingh-15@users.noreply.github.com>
2022-08-07 18:16:45 +0530

    Time: 92 ms (5.02%), Space: 44.1 MB (6.55%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        long start=0;
+        long end=x;
+        while(start<=end){
+            long mid=start+(end-start)/2;
+            System.out.println(mid);
+            if(mid*mid==x){
+                return (int)mid;
+            }
+            
+            else if(mid*mid<x){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }  
+        }
+        
+        return (int) start-1;
+    }
+}
