3821698a1b2612a4d5056acb7197d88e190ff64e
vikashsingh-15 <55023442+vikashsingh-15@users.noreply.github.com>
2022-08-09 14:00:00 +0530

    Time: 2660 ms (5.00%), Space: 144.7 MB (13.23%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        if(arr.length<3){
+             List<List<Integer>> list=new ArrayList<>();
+            return list; 
+        }
+        else{
+        int target=0;
+         Map<Integer, Integer> map = new HashMap<>();
+        HashSet <List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            map.put(arr[i],map.getOrDefault(arr[i],0)-1);
+        
+            for (int j = i + 1; j < arr.length; j++) {
+                map.put(arr[j],map.getOrDefault(arr[j],0)-1);
+                int sum = arr[i] + arr[j];
+                if (map.containsKey(target - sum) && map.get(target - sum) > 0) {
+                    
+                    List<Integer> list = new ArrayList<>();
+                    // System.out.println(arr[i]);
+                        list.add(arr[i]);
+                        list.add(arr[j]);
+                        list.add(target - sum);
+                    // System.out.println(list);
+                        Collections.sort(list);
+                        // System.out.println(list);
+                        set.add(list);
+                }
+                map.put(arr[j],map.getOrDefault(arr[j],0)+1); 
+            }
+        
+        }
+        if(set.size()>0){
+        return new ArrayList(set);
+        }
+        else{
+           List<List<Integer>> list=new ArrayList<>();
+            return list;
+        }
+        }
+        
+    }
+}
