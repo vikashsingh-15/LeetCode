a67adf7b6b6c84cb3af861dd7679f81bec46cc0f
vikashsingh-15 <55023442+vikashsingh-15@users.noreply.github.com>
2022-08-16 19:05:25 +0530

    Time: 3 ms (38.94%), Space: 54.6 MB (68.32%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+            
+        }
+        
+//         int i=0,j=0;
+        
+//         while(i<nums.length){
+//             while(j<nums.length&& nums[j]!=0){
+//                 j++;     
+//             }
+            
+//             if(j==nums.length){break;}//you got a j with zero
+            
+//             i=j+1;
+//             while(i<nums.length&& nums[i]==0){
+//                 i++;     
+//             }
+            
+//             if(i==nums.length){break;}
+            
+//             int temp=nums[i];
+//             nums[i]=nums[j];
+//             nums[j]=temp;
+//             j++;
+            
+            
+//         }
+        
+    }
+}
