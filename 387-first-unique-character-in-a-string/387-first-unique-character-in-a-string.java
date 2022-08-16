3ba40706e7ff5272627d132a74d33a6837a9f7f6
vikashsingh-15 <55023442+vikashsingh-15@users.noreply.github.com>
2022-08-16 18:41:16 +0530

    Time: 3 ms (98.81%), Space: 47.4 MB (84.56%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,21 +1,35 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        int ans=-1;
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
+//         HashMap<Character,Integer> map=new HashMap<>();
+//         int ans=-1;
+//         for(int i=0;i<s.length();i++){
+//             char c=s.charAt(i);
+//             map.put(c,map.getOrDefault(c,0)+1);
+//         }
         
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            int val=map.get(c);
-            if(val<2){
-                ans=i;
-                break;
+//         for(int i=0;i<s.length();i++){
+//             char c=s.charAt(i);
+//             int val=map.get(c);
+//             if(val<2){
+//                 ans=i;
+//                 break;
+//             }
+//         }
+//         return ans;
+        
+        
+        
+        
+
+        int ans = Integer.MAX_VALUE;
+        for(char c='a'; c<='z';c++){
+            int index = s.indexOf(c);//default value of s.indexOf(c);=-1
+            // System.out.println(index);
+            if(index!=-1&&index==s.lastIndexOf(c)){
+                ans = Math.min(ans,index);
             }
         }
-        return ans;
+        return ans==Integer.MAX_VALUE?-1:ans;
         
     }
 }
