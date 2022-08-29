ba89f9dee3d33aaf55e16c5f48969d42b560f511
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-08-29 12:17:48 +0530

    Time: 1 ms (100.00%), Space: 48.4 MB (30.72%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,35 +1,38 @@
 class Solution {
+
     public int[][] diagonalSort(int[][] mat) {
-      int n=mat.length;
-        int count=0;//this keeps the count of elements in frst row & 1st col
-        int m=mat[0].length;
-        int i=0,k=0;
-//this while loop will be sending all the starting element-index of the diagonal
-         while(count<m+n){
-             if(i==n-1&&k<m){++k;}
-             if(i!=n-1){i++;}
-             
-             pass(mat,n-1-i,k,n,m);count++;
-         }
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        for(int i=0;i<m;i++){
+            countSort(mat,i,0);
+        }
+        for(int i=0;i<n;i++){
+            countSort(mat,0,i);
+        }
         return mat;
     }
-    public void pass(int [][]a,int row,int col,int n,int m){
- 
-        //here we perform COUNT SORT IN EVERY DIAGONAL     
-       
-        int []arr=new int [101]; //Frequency Array
-        int i=row,j=col;
-        while(row<n&&col<m){
-            arr[a[row++][col++]]++;
+    
+    public void countSort(int [][]mat,int row,int col){
+        int m = mat.length;
+        int n = mat[0].length;
+        int i=row;int j=col;
+        int dummy[]=new int[101];
+        while(i<m && j<n){
+            dummy[mat[i][j]]++;
+            i++;
+            j++;
         }
-//SORTING ELEMENTS IN ORIGINAL ARAY DIAGONAL        
-       for(int k=0;k<101;k++){
-           if(arr[k]>0){
-               while(arr[k]!=0){
-               a[i++][j++]=k;
-                   --arr[k];
-           }
-           }
-       } 
+         i=row;
+         j=col;
+        for(int k=1;k<dummy.length;k++){
+            while(dummy[k]>0){
+                mat[i][j]=k;
+                dummy[k]--;
+                i++;
+                j++;
+            }
+        }
+        
     }
 }
