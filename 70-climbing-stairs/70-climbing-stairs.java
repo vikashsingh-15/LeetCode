6d7e3acd4b7f95c5524092a1d43779d5441f4921
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-08-29 15:23:56 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (84.76%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,8 +1,10 @@
 class Solution {
 
     public int climbStairs(int n) {
-        int qb[]= new int[n + 1];
-        return climbStairsMemo(n, qb);
+        // int qb[] = new int[n + 1];
+        // return climbStairsMemo(n, qb);
+        return climbStairsTebul(n);
+
     }
 
     public int climbStairsMemo(int n, int[] qb) {
@@ -15,10 +17,24 @@ class Solution {
         if (qb[n] != 0) {
             return qb[n];
         }
-
         int num1 = climbStairsMemo(n - 1, qb);
         int num2 = climbStairsMemo(n - 2, qb);
         qb[n] = num1 + num2;
         return num1 + num2;
+    }
+
+    public int climbStairsTebul(int n) {
+        int dp[] = new int[n + 1];
+        dp[0]=1;
+        
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                dp[i]=dp[i-1];
+            }else{
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+        
+        return dp[n];
     }
 }
