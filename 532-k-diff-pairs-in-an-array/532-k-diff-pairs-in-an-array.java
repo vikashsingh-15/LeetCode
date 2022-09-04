9b6a088c56a748ecd8d3c53d7718d5ad84e044c4
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-09-05 00:32:20 +0530

    Time: 23 ms (20.86%), Space: 48.8 MB (26.90%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,30 +1,46 @@
 class Solution {
 
     public int findPairs(int[] arr, int target) {
-        int apoint = 0;
-        int bpoint = 1;
-        int count = 0;
+        //         //way 1
+        //         int apoint = 0;
+        //         int bpoint = 1;
+        //         int count = 0;
+        //         Arrays.sort(arr);
+        //         while (apoint <= bpoint && bpoint < arr.length) {
+        //             int temp = arr[bpoint];
+        //             while (bpoint < arr.length && arr[bpoint] == temp) {
+        //                 bpoint++;
+        //             }
+        //             bpoint--;
 
-        Arrays.sort(arr);
+        //             int diff = arr[bpoint] - arr[apoint];
+        //             if (diff == target && apoint != bpoint) {
+        //                 bpoint++;
+        //                 count++;
+        //             } else if (diff < target) {
+        //                 bpoint++;
+        //             } else {
+        //                 apoint++;
+        //             }
+        //         }
 
-        while (apoint <= bpoint && bpoint < arr.length) {
-            int temp = arr[bpoint];
-            while (bpoint < arr.length && arr[bpoint] == temp) {
-                bpoint++;
-            }
-            bpoint--;
+        //         return count;
 
-            int diff = arr[bpoint] - arr[apoint];
-            if (diff == target && apoint != bpoint) {
-                bpoint++;
-                count++;
-            } else if (diff < target) {
-                bpoint++;
-            } else {
-                apoint++;
+        //way 2
+
+        Map<Integer, Integer> map = new HashMap();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int result = 0;
+        for (int i : map.keySet()) {
+            if (target > 0 && map.containsKey(i + target) || target == 0 && map.get(i) > 1) {
+                result++;
+                map.put(i + target, map.getOrDefault(i + target, 0) - 1);
             }
         }
-        
-        return count;
+
+        return result;
     }
 }
