f5adaa1d3c765807826a7eed7222d38135bbe2ad
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-09-05 21:22:17 +0530

    Time: 2 ms (86.50%), Space: 43.9 MB (67.12%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,31 +1,50 @@
 import java.util.*;
 class Solution {
-
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
-        int limit = (int) Math.pow(2, nums.length);
-        for (int i = 0; i < limit; i++) {
-            int pos = i;
-            List<Integer> temp = new ArrayList();
-            for (int j = 0; j < nums.length; j++) {
-                int rem = pos % 2;
-                pos = pos / 2;
-                if (rem != 0) {
-                    temp.add(nums[j]);
-                }
-                // // checks the setbit thsi can also be done
-                // if((num & (1<<j))!=0){
-                //     lst.add(nums[i]);
-                // }
-            }
-            Collections.sort(temp);
-            if (!set.contains(temp)) {
-                Collections.sort(temp);
-                set.add(temp);
-                ans.add(temp);
-            }
+         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        findsubsets(0,nums,new ArrayList<>(),res);
+        return res;
+        
+        
+        
+//         //way2
+//         List<List<Integer>> ans = new ArrayList<>();
+//         Set<List<Integer>> set = new HashSet<>();
+//         int limit = (int) Math.pow(2, nums.length);
+//         for (int i = 0; i < limit; i++) {
+//             int pos = i;
+//             List<Integer> temp = new ArrayList();
+//             for (int j = 0; j < nums.length; j++) {
+//                 int rem = pos % 2;
+//                 pos = pos / 2;
+//                 if (rem != 0) {
+//                     temp.add(nums[j]);
+//                 }
+//                 // // checks the setbit thsi can also be done
+//                 // if((num & (1<<j))!=0){
+//                 //     lst.add(nums[i]);
+//                 // }
+//             }
+//             Collections.sort(temp);
+//             if (!set.contains(temp)) {
+//                 Collections.sort(temp);
+//                 set.add(temp);
+//                 ans.add(temp);
+//             }
+//         }
+//         return ans;
+    }
+    
+    
+    
+    public void findsubsets(int id, int[] nums, ArrayList<Integer> li, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(li));
+        for (int i = id; i < nums.length; i++) {
+             if(id != i && nums[i]==nums[i-1]) continue;//this line removes duplicate
+            li.add(nums[i]);
+            findsubsets(i + 1, nums, li, ans);
+            li.remove(li.size() - 1);
         }
-        return ans;
     }
 }
