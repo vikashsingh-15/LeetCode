eca2e9cc83d0a4cabe4fa99294fe99ce911076f1
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-09-07 19:04:36 +0530

    Time: 35 ms (28.75%), Space: 62.9 MB (16.79%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -16,30 +16,43 @@
 class Solution {
     StringBuilder sb;
     public String tree2str(TreeNode root) {
-        sb=new StringBuilder();
-        Helper(root);
-       return sb.toString();
+       //  sb=new StringBuilder();
+       //  Helper(root);
+       // return sb.toString();
+        
+        // Way 2
+        
+        if(root == null) return "";
+        StringBuilder sb = new StringBuilder();
+        
+        int val = root.val;
+        String left = tree2str(root.left);
+        String right = tree2str(root.right);
+        sb.append(val+"");
+        sb.append((left.isEmpty() && right.isEmpty()) ? "" : "("+left+")");
+        sb.append((right.isEmpty()) ? "" : "("+right+")");
+        
+        return sb.toString();
         
     }
-    public void Helper(TreeNode root) {  
-        if(root==null){
-            return;
-        }
-         if(root.left == null && root.right==null){
-            sb.append(root.val);
-            return;
-        } 
-        sb.append(root.val);
-        sb.append('(');
-        Helper(root.left);
-       sb.append(')');
-        if(root.right!=null){
-        sb.append('(');
-        Helper(root.right);
-        sb.append(')');
-        }
-        
-        // return sb;
-    }
+    
+    // public void Helper(TreeNode root) {  
+    //     if(root==null){
+    //         return;
+    //     }
+    //      if(root.left == null && root.right==null){
+    //         sb.append(root.val);
+    //         return;
+    //     } 
+    //     sb.append(root.val);
+    //     sb.append('(');
+    //     Helper(root.left);
+    //    sb.append(')');
+    //     if(root.right!=null){
+    //     sb.append('(');
+    //     Helper(root.right);
+    //     sb.append(')');
+    //     }
+    // }
     
 }
