94f91e8cc7b3ddfd4473907d492eeb1bbe9e93c6
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-09-09 00:07:22 +0530

    Time: 3 ms (82.96%), Space: 49.8 MB (48.74%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,119 +1,107 @@
 class Solution {
 
     public List<Integer> majorityElement(int[] arr) {
-     ////way 1
-        //         int val1 = arr[0];
-        //         int count1 = 1;
+        
+        //way 1
+                int val1 = arr[0];
+                int count1 = 1;
 
-        //         int val2 = arr[0];
-        //         int count2 = 0;
+                int val2 = arr[0];
+                int count2 = 0;
 
-        //         for (int i = 1; i < arr.length; i++) {
-        //             if (arr[i] == val1) {
-        //                 count1++;
-        //             } else if (arr[i] == val2) {
-        //                 count2++;
-        //             } else {
-        //                 if (count1 == 0) {
-        //                     val1 = arr[i];
-        //                     count1++;
-        //                 } else if (count2 == 0) {
-        //                     val2 = arr[i];
-        //                     count2++;
-        //                 } else {
-        //                     count1--;
-        //                     count2--;
-        //                 }
-        //             }
-        //         }
+                for (int i = 1; i < arr.length; i++) {
+                    if (arr[i] == val1) {
+                        count1++;
+                    } else if (arr[i] == val2) {
+                        count2++;
+                    } else {
+                        if (count1 == 0) {
+                            val1 = arr[i];
+                            count1++;
+                        } else if (count2 == 0) {
+                            val2 = arr[i];
+                            count2++;
+                        } else {
+                            count1--;
+                            count2--;
+                        }
+                    }
+                }
 
-        //         List<Integer> list = new ArrayList<>();
-        //         int desti = arr.length / 3;
+                List<Integer> list = new ArrayList<>();
+                int desti = arr.length / 3;
 
-        //         count1 = 0;
-        //         count2 = 0;
+                count1 = 0;
+                count2 = 0;
 
+                for (int i = 0; i < arr.length; i++) {
+                    if (val1 == arr[i]) {
+                        count1++;
+                    }
+                    if (val2 == arr[i]) {
+                        count2++;
+                    }
+                }
+
+                if (count1 > desti) {
+                    list.add(val1);
+                }
+                if ((val1 != val2) && (count2 > desti)) {
+                    list.add(val2);
+                }
+
+                return list;
+
+ // way 2
+
+        //         int n = arr.length / 3;
+        //         Arrays.sort(arr);
+        //         int count = 1;
+        //         ArrayList<Integer> list = new ArrayList();
         //         for (int i = 0; i < arr.length; i++) {
-        //             if (val1 == arr[i]) {
-        //                 count1++;
+        //             if (i < arr.length - 1 && arr[i] == arr[i + 1]) {
+        //                 count = count + 1;
         //             }
-        //             if (val2 == arr[i]) {
-        //                 count2++;
+        //             if (i < arr.length - 1 && arr[i] != arr[i + 1]) {
+        //                 count = 1;
+        //             }
+        //             if (count > n) {
+        //                 list.add(arr[i]);
+        //                 count = 1;
         //             }
         //         }
+        //         Set<Integer> set = new LinkedHashSet<>();
 
-        //         if (count1 > desti) {
-        //             list.add(val1);
-        //         }
-        //         if ((val1 != val2) && (count2 > desti)) {
-        //             list.add(val2);
-        //         }
-
+        //         set.addAll(list);
+        //         list.clear();
+        //         list.addAll(set);
         //         return list;
 
-    // way 2
+//way 3
 
-//         int n = arr.length / 3;
-//         Arrays.sort(arr); 
-//         int count = 1;
-//         ArrayList<Integer> list = new ArrayList(); 
-//         for (int i = 0; i < arr.length; i++) {
-//             if (i < arr.length - 1 && arr[i] == arr[i + 1]) { 
-//                 count = count + 1;
-//             }
-//             if (i < arr.length - 1 && arr[i] != arr[i + 1]) { 
-//                 count = 1;
-//             }
-//             if (count > n) { 
-//                 list.add(arr[i]);
-//                 count = 1;
-//             }
+        //   List<Integer> ans = new ArrayList<Integer>();
+        // Arrays.sort(arr);
+        //   for (int i = 0; i < arr.length; i++)
+        //   {
+        //       int count = 1;
+        //         while (i < arr.length-1 && arr[i] == arr[i + 1]) {
+        //           i++;
+        //           count++;
+        //       }
+        //     if(count>arr.length/3) ans.add(arr[i]);
+        //   }
+        // return ans;
+
+// //way 4
+//         List<Integer> al = new ArrayList<>();
+//         int n = arr.length;
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int i : arr) {
+//             map.put(i, map.getOrDefault(i, 0) + 1);
 //         }
-//         Set<Integer> set = new LinkedHashSet<>(); 
-
-//         set.addAll(list); 
-//         list.clear(); 
-//         list.addAll(set); 
-//         return list;
-        
-    //way 3
-        
-      //   List<Integer> ans = new ArrayList<Integer>();
-      // Arrays.sort(arr);
-      //   for (int i = 0; i < arr.length; i++)
-      //   {
-      //       int count = 1;
-      //         while (i < arr.length-1 && arr[i] == arr[i + 1]) {
-      //           i++;
-      //           count++;
-      //       }
-      //     if(count>arr.length/3) ans.add(arr[i]);
-      //   }
-      // return ans;
-        
-        
-        //way 4
-        List<Integer> al = new ArrayList<>();
-        int n = arr.length;
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i : arr){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        // System.out.println(map);
-        for(int i : map.keySet()){
-            if(map.get(i)>n/3)
-                al.add(i);
-        }
-        return al;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+//         for (int i : map.keySet()) {
+//             if (map.get(i) > n / 3) al.add(i);
+//         }
+//         return al;
     }
 }
