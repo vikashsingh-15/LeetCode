e7f73e280a49e13381c8c4ad32bc63471ff1aad6
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-09-09 23:33:07 +0530

    Time: 0 ms (100.00%), Space: 43 MB (7.41%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,18 +1,46 @@
 
 import java.lang.Math;
 class Solution {
-    public int[] plusOne(int[] digits) {
-        for(int i = digits.length -1; i>=0; i--){
-            if(digits[i] < 9){
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
+    // public int[] plusOne(int[] digits) {
+    //     for(int i = digits.length -1; i>=0; i--){
+    //         if(digits[i] < 9){
+    //             digits[i]++;
+    //             return digits;
+    //         }
+    //         digits[i] = 0;
+    //     }
+    //     int res[] = new int[digits.length +1];
+    //     res[0] = 1;
+    //     return res;   
+    // }
+    
+    public int[] plusOne(int[] A) {
+         int carry=1,p=0;
+        for(int i=A.length-1;i>=p;i--)
+        {
+            int sum=A[i]+carry;
+            A[i]=sum%10;
+            carry=sum/10;
         }
-        int res[] = new int[digits.length +1];
-        res[0] = 1;
-        return res;
-        
-        
+        if(carry>0)
+        {
+            int b[]=new int[A.length+1];
+            b[0]=carry;
+            for(int i=1;i<A.length+1;i++)
+            b[i]=A[i-1];
+            return b;
+        }
+        else
+        {
+          int pp=0;
+          while(A[pp]==0)
+          pp++;
+            int b[]=new int[A.length-pp];
+            for(int i=pp;i<A.length;i++)
+            b[i-pp]=A[i];
+            return b;
+        }
     }
+    
+    
 }
