de37525644e0681776e32773d9d281a814df13c4
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-09-10 09:33:16 +0530

    Time: 5 ms (15.45%), Space: 45.6 MB (9.41%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -2,41 +2,44 @@ class Solution {
 
     public int maxProduct(int[] A) {
         //way 1
-        
-        //         if (A == null || A.length == 0)     return 0;
-        //         int max = A[0], min = A[0], result = A[0];
-        //         for (int i = 1; i < A.length; i++) {
-        //             int temp = max;
-        //             max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
-        //             min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
-        //             if (max > result) {
-        //                 result = max;
-        //             }
-        //         }
-        //         return result;
+                if (A == null || A.length == 0)     return 0;
+                int max = A[0], min = A[0], result = A[0];
+                for (int i = 1; i < A.length; i++) {
+                    // System.out.println("For = "+i);
+                    int temp = max;
+                    max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
+                    // System.out.println("max = "+max);
+                    min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
+                    // System.out.println("min = "+min);
+                    if (max > result) {
+                        result = max;
+                    }
+                    // System.out.println("result = "+result);
+                }
+                return result;
 
         //way2
-        int product = 1;
-        int result = Integer.MIN_VALUE;
+//         int product = 1;
+//         int result = Integer.MIN_VALUE;
 
-        for (int i = 0; i < A.length; i++) {
-            product = product * A[i];
-            result = Math.max(product, result);
-            if (product == 0) {
-                product = 1;
-            }
-        }
+//         for (int i = 0; i < A.length; i++) {
+//             product = product * A[i];
+//             result = Math.max(product, result);
+//             if (product == 0) {
+//                 product = 1;
+//             }
+//         }
 
-        product = 1;
+//         product = 1;
 
-        for (int i = A.length - 1; i >= 0; i--) {
-            product = product * A[i];
-            result = Math.max(product, result);
-            if (product == 0) {
-                product = 1;
-            }
-        }
+//         for (int i = A.length - 1; i >= 0; i--) {
+//             product = product * A[i];
+//             result = Math.max(product, result);
+//             if (product == 0) {
+//                 product = 1;
+//             }
+//         }
 
-        return result;
+//         return result;
     }
 }
