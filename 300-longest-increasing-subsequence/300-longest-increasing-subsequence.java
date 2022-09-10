cbda5c6f53fc3b0644e15954b6dafe3f5cc80db4
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-09-10 10:06:32 +0530

    Time: 103 ms (23.24%), Space: 43.9 MB (82.19%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,19 +1,35 @@
 class Solution {
+
     public int lengthOfLIS(int[] nums) {
-        int arr[]=new int [nums.length];
-        int ans=Integer.MIN_VALUE;
-        arr[0]=1;
-        for(int i=1;i<nums.length;i++){
-            int max=0;
-            for(int j=0;j<i;j++){  
-                if(nums[j]<nums[i] && arr[j]>max){
-                   max=arr[j];
-                } 
+        //         //way 1
+        //         int arr[]=new int [nums.length];
+        //         int ans=Integer.MIN_VALUE;
+        //         arr[0]=1;
+        //         for(int i=1;i<nums.length;i++){
+        //             int max=0;
+        //             for(int j=0;j<i;j++){
+        //                 if(nums[j]<nums[i] && arr[j]>max){
+        //                    max=arr[j];
+        //                 }
+        //             }
+        //             arr[i]=max+1;
+        //             ans=Math.max(ans,arr[i]);//we can do that outside a loop also,just to chech max in arr;
+        //         }
+
+        //         return ans==Integer.MIN_VALUE?1:ans;
+
+        //way 2
+        int lis[] = new int[nums.length];
+        Arrays.fill(lis, 1);
+        int ans = 1;
+        for (int i = 1; i < nums.length; i++) { //to get longest increasing subsequence ending at this index i
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && lis[j] >= lis[i]) {
+                    lis[i] = lis[j] + 1;
+                }
             }
-            arr[i]=max+1;
-            ans=Math.max(ans,arr[i]);//we can do that outside a loop also,just to chech max in arr;
+            ans = Math.max(ans, lis[i]); //update ans if we have maximum than cur ans
         }
-        
-        return ans==Integer.MIN_VALUE?1:ans;
+        return ans;
     }
 }
