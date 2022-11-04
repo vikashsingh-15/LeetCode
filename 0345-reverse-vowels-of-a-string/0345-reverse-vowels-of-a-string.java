072ac506a903eac72ed0a9af6204cb4f14a621d1
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-11-04 13:46:51 +0530

    Time: 5 ms (86.05%), Space: 45.2 MB (43.69%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -24,5 +24,11 @@ class Solution {
 
     private boolean isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+        
+        
     }
+    
+    //  public boolean isVowel(char ch) {
+    //     return "aeiouAEIOU".indexOf(ch) >= 0;
+    // }
 }
