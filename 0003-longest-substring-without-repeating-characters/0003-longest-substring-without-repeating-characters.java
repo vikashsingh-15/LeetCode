4d38dcb9d00e2ff8d04c2b7c4e3929fcff18acbe
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2022-11-04 22:09:07 +0530

    Time: 5 ms (95.71%), Space: 43.9 MB (76.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,28 +1,51 @@
 class Solution {
+
     public int lengthOfLongestSubstring(String str) {
-        int apoint=0;
-        int bpoint=0;
-        int max=0;
-        HashMap<Character,Integer>map=new HashMap<>();
-        
-        while(bpoint<=str.length()-1){
-            char c=str.charAt(bpoint);
+        ////Way 1
+        //         int apoint=0;
+        //         int bpoint=0;
+        //         int max=0;
+        //         HashMap<Character,Integer>map=new HashMap<>();
+
+        //         while(bpoint<=str.length()-1){
+        //             char c=str.charAt(bpoint);
+        //             bpoint++;
+        //             map.put(c,map.getOrDefault(c,0)+1);
+        //             if(map.get(c)==1){
+        //                 max=Math.max(max,map.size());
+        //             }else{
+        //                 while(str.charAt(apoint)!=c){
+        //                     map.remove(str.charAt(apoint));
+        //                     apoint++;
+
+        //                 }
+
+        //                 map.put(str.charAt(apoint),map.get(str.charAt(apoint))-1);
+        //                     apoint++;
+        //             }
+        //         }
+
+        //        return max;
+
+        ////way 2
+        int apoint = 0;
+        int bpoint = 0;
+        int max = 0;
+
+        int[] arrChar = new int[128];
+
+        while (bpoint < str.length()) {
+            char c = str.charAt(bpoint);
             bpoint++;
-            map.put(c,map.getOrDefault(c,0)+1);
-            if(map.get(c)==1){
-                max=Math.max(max,map.size());
-            }else{
-                while(str.charAt(apoint)!=c){
-                    map.remove(str.charAt(apoint));
-                    apoint++;
-                    
-                }
-                
-                map.put(str.charAt(apoint),map.get(str.charAt(apoint))-1);
-                    apoint++;
+
+            arrChar[c]++;
+
+            while (arrChar[c] > 1) {
+                arrChar[str.charAt(apoint++)]--;
             }
+            
+            max = Math.max(max , bpoint - apoint );
         }
-        
-       return max; 
+        return max;
     }
 }
