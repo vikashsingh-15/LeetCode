01f9ead0fc70c2b5b6399654d7ec9e22b707a580
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-01-30 12:16:07 +0530

    Time: 2 ms (52.94%), Space: 40.1 MB (96.57%) - LeetHub

---
+++ 1864-minimum-number-of-swaps-to-make-the-binary-string-alternating/1864-minimum-number-of-swaps-to-make-the-binary-string-alternating.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int minSwaps(String str) {
+       int count0 = 0;
+        int count1 = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '0') {
+                count0++;
+            } else if (c == '1') {
+                count1++;
+            }
+        }
+        if (Math.abs(count0 - count1) > 1) {
+            return -1;
+        } else {
+            // 1 at first
+            int i = 0;
+            int cost = 0;
+            while (i < str.length()) {
+                if (i % 2 == 0) {
+                    if (str.charAt(i) != '1') {
+                        cost++;
+                    }
+                } else {
+                    if (str.charAt(i) != '0') {
+                        cost++;
+                    }
+                }
+                i++;
+            }
+
+            // 0 at first
+            int j = 0;
+            int cost1 = 0;
+            while (j < str.length()) {
+                if (j % 2 == 0) {
+                    if (str.charAt(j) != '0') {
+                        cost1++;
+                    }
+                } else {
+                    if (str.charAt(j) != '1') {
+                        cost1++;
+                    }
+                }
+                j++;
+            }
+
+            if (cost % 2 == 0 && cost1 % 2 == 0) {
+                if (cost / 2 < cost1 / 2) {
+                    return cost / 2;
+                    // System.out.println(cost / 2);
+                } else {
+                    return cost1 / 2;
+                    // System.out.println(cost1 / 2);
+                }
+            }
+
+            else {
+                if (cost % 2 == 0)
+                    return cost / 2;
+                    // System.out.println(cost / 2);
+                else
+                    return cost1 / 2;
+                    // System.out.println(cost1 / 2);
+            }  
+    }
+    }
+}
