90ddca2172a2c7af9b4d2789ce26b7b9cdca02cb
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-02-03 00:01:57 +0530

    Time: 65 ms (54.07%), Space: 57.8 MB (84.98%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -14,21 +14,44 @@ class Solution {
         for (var i = 0; i < nums.length; i++) {
             set.add(nums[i]);
         }
+        ////way 1
 
-        int count = 0;
-        int max = 0;
-        int prev = set.first();
+        //         int count = 0;
+        //         int max = 0;
+        //         int prev = set.first();
 
-        for (Integer value : set) {
-            if (value - 1 == prev) {
+        //         for (Integer value : set) {
+        //             if (value - 1 == prev) {
+        //                 count++;
+        //             } else {
+        //                 count = 0;
+        //             }
+        //             prev = value;
+        //             max = Math.max(count, max);
+        //         }
+        //         return max + 1;
+        
+        
+        ////Way 2
+        
+        int count=0;
+        int max=0;
+        int prev=set.first();
+        
+        Integer arr[]=set.toArray(new Integer[0]);
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]-1==prev){
                 count++;
-            } else {
-                count = 0;
+            }else{
+                count=0;
             }
-            prev = value;
-            max = Math.max(count, max);
+            prev = arr[i];
+            max = Math.max(count, max);     
         }
-        return max + 1;
+        
+        return max+1;
+      
 
 
     }
