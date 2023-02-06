935ad19e16c251cf262136a6f685941ed613111c
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-02-06 11:04:52 +0530

    Time: 0 ms (100.00%), Space: 42 MB (95.65%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,17 +1,28 @@
 class Solution {
 
     public int[] shuffle(int[] nums, int n) {
-        int part = nums.length / n;
-        int length2 = nums.length / part;
-        int ans[]=new int [nums.length];
-        int k=0;
+        ////way 1
+//         int part = nums.length / n;
+//         int length2 = nums.length / part;
+//         int ans[]=new int [nums.length];
+//         int k=0;
 
-        for (int i = 0; i < length2; i++) {
-            ans[k++]=nums[i];
-            for (int j = i + length2; j < nums.length; j = j + length2) {
-                ans[k++]=nums[j];  
-            }
+//         for (int i = 0; i < length2; i++) {
+//             ans[k++]=nums[i];
+//             for (int j = i + length2; j < nums.length; j = j + length2) {
+//                 ans[k++]=nums[j];  
+//             }
+//         }
+//         return ans;
+        
+        
+        
+        //WAY 2
+        int[] result = new int[2*n];
+        for(int i = 0; i < n; i ++) {
+            result[2*i] = nums[i];
+            result[2*i+1] = nums[i+n];
         }
-        return ans;
+        return result;
     }
 }
