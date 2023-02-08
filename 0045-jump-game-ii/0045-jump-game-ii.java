a3d92e40d19ead50a0616845ae3e3feca8cd736f
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-02-08 12:11:23 +0530

    Time: 1 ms (99.45%), Space: 42.4 MB (93.90%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,21 +1,37 @@
 class Solution {
 
     public int jump(int[] nums) {
-        Integer dp[] = new Integer[nums.length];
-        dp[nums.length - 1] = 0;
+        // //way 1
+        // Integer dp[] = new Integer[nums.length];
+        // dp[nums.length - 1] = 0;
+        // int steps = 0;
+        // for (int i = nums.length - 2; i >= 0; i--) {
+        //     steps = nums[i];
+        //     int min = Integer.MAX_VALUE;
+        //     for (int j = 1; j <= steps && i + j < nums.length; j++) {
+        //         if (dp[i + j] != null) {
+        //             min = Math.min(dp[i + j], min);
+        //         }
+        //         if (min != Integer.MAX_VALUE) {
+        //             dp[i] = min + 1;
+        //         }
+        //     }
+        // }
+        // return dp[0];
+        
+        //way 2
+        
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0; 
         int steps = 0;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            steps = nums[i];
-            int min = Integer.MAX_VALUE;
-            for (int j = 1; j <= steps && i + j < nums.length; j++) {
-                if (dp[i + j] != null) {
-                    min = Math.min(dp[i + j], min);
-                }
-                if (min != Integer.MAX_VALUE) {
-                    dp[i] = min + 1;
-                }
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
             }
         }
-        return dp[0];
+        return steps;
     }
 }
