773a20a9339f6b09c69269535cbd98de1795d26c
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-02-11 14:36:13 +0530

    Time: 4 ms (89.57%), Space: 42.6 MB (89.80%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
@@ -1,14 +1,16 @@
 class Solution {
-    public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
+    public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
         Set<Integer>[][] graph = new HashSet[2][n];
         for (int i = 0; i < n; i++) {
             graph[0][i] = new HashSet<>();
             graph[1][i] = new HashSet<>();
         }
-        for (int[] re : red_edges) {
+        for (int i = 0; i < redEdges.length; i++) {
+            int[] re = redEdges[i];
             graph[0][ re[0] ].add(re[1]);
         }
-        for (int[] blu : blue_edges) {
+        for (int i = 0; i < blueEdges.length; i++) {
+            int[] blu = blueEdges[i];
             graph[1][ blu[0] ].add(blu[1]);
         }
         int[][] res = new int[2][n];
@@ -23,7 +25,9 @@ class Solution {
             int[] cur = q.poll();
             int vert = cur[0];
             int colr = cur[1];
-            for (int nxt : graph[1 - colr][vert]) {
+            Set<Integer> nextNodes = graph[1 - colr][vert];
+            for (int j = 0; j < nextNodes.size(); j++) {
+                int nxt = (Integer) nextNodes.toArray()[j];
                 if (res[1 - colr][nxt] == 2 * n) {
                     res[1 - colr][nxt] = 1 + res[colr][vert];
                     q.offer(new int[] {nxt, 1 - colr});
@@ -39,3 +43,47 @@ class Solution {
         
     }
 }
+
+
+
+//class Solution {
+//     public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
+//         Set<Integer>[][] graph = new HashSet[2][n];
+//         for (int i = 0; i < n; i++) {
+//             graph[0][i] = new HashSet<>();
+//             graph[1][i] = new HashSet<>();
+//         }
+//         for (int[] re : red_edges) {
+//             graph[0][ re[0] ].add(re[1]);
+//         }
+//         for (int[] blu : blue_edges) {
+//             graph[1][ blu[0] ].add(blu[1]);
+//         }
+//         int[][] res = new int[2][n];
+//         for (int i = 1; i < n; i++) {
+//             res[0][i] = 2 * n;
+//             res[1][i] = 2 * n;
+//         }
+//         Queue<int[]> q = new LinkedList<>();
+//         q.offer(new int[] {0, 0});
+//         q.offer(new int[] {0, 1});
+//         while (!q.isEmpty()) {
+//             int[] cur = q.poll();
+//             int vert = cur[0];
+//             int colr = cur[1];
+//             for (int nxt : graph[1 - colr][vert]) {
+//                 if (res[1 - colr][nxt] == 2 * n) {
+//                     res[1 - colr][nxt] = 1 + res[colr][vert];
+//                     q.offer(new int[] {nxt, 1 - colr});
+//                 }
+//             }
+//         }
+//         int[] ans = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             int t = Math.min(res[0][i], res[1][i]);
+//             ans[i] = (t == 2 * n) ? -1 : t;
+//         }
+//         return ans;
+        
+//     }
+// }
