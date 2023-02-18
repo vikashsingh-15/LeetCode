df59992a8eaaab881e90dc4e36d5b1196cd790aa
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-02-18 16:37:19 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (92.38%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,20 +1,32 @@
 class Solution {
-
     public String gcdOfStrings(String str1, String str2) {
-        if (str1 == "") {
-            return str2;
-        } else if (str2 == "") {
-            return str1;
-        } else if (!(str1 + str2).equals(str2 + str1)) {
-            return "";
-        } else if (str1.length() > str2.length()) {
-            StringBuilder sb = new StringBuilder(str1);
-            sb.delete(str1.length() - str2.length(), str1.length());
-            return gcdOfStrings(sb.toString(), str2);
-        } else {
-            StringBuilder sb1 = new StringBuilder(str2);
-            sb1.delete(str2.length() - str1.length(), str2.length());
-            return gcdOfStrings(str1, sb1.toString());
+        // if (str1 == "") {
+        //     return str2;
+        // } else if (str2 == "") {
+        //     return str1;
+        // } else if (!(str1 + str2).equals(str2 + str1)) {
+        //     return "";
+        // } else if (str1.length() > str2.length()) {
+        //     StringBuilder sb = new StringBuilder(str1);
+        //     sb.delete(str1.length() - str2.length(), str1.length());
+        //     return gcdOfStrings(sb.toString(), str2);
+        // } else {
+        //     StringBuilder sb1 = new StringBuilder(str2);
+        //     sb1.delete(str2.length() - str1.length(), str2.length());
+        //     return gcdOfStrings(str1, sb1.toString());
+        // }
+        
+//Way 2
+        if (str1.length() < str2.length()) {
+            return gcdOfStrings(str2, str1);
         }
+        if (!str1.startsWith(str2)) {
+            return "";
+        }
+        if (str2.isEmpty()) {
+            return str1;
+        }
+        return gcdOfStrings(str1.substring(str2.length()), str2);
+        
     }
 }
