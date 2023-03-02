fd2f7b030515791804af41615e2b7b75d507e808
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-03-02 17:50:34 +0530

    Time: 1 ms (98.42%), Space: 42.3 MB (31.50%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,36 +1,69 @@
 class Solution {
+
     public int compress(char[] chars) {
+        ////way 1
+        //         if (chars == null || chars.length == 0) {
+        //             return 0;
+        //         }
+        //         int writeIndex = 0;
+        //         char prevChar = chars[0];
+        //         int count = 1;
+        //         for (int i = 1; i < chars.length; i++) {
+        //             if (chars[i] == prevChar) {
+        //                 count++;
+        //             } else {
+        //                 chars[writeIndex++] = prevChar;
+        //                 if (count > 1) {
+        //                     String countStr = Integer.toString(count);
+        //                     for (int j = 0; j < countStr.length(); j++) {
+        //                         chars[writeIndex++] = countStr.charAt(j);
+        //                     }
+        //                 }
+        //                 prevChar = chars[i];
+        //                 count = 1;
+        //             }
+        //         }
+        //         chars[writeIndex++] = prevChar;
+        //         if (count > 1) {
+        //             String countStr = Integer.toString(count);
+        //             for (int j = 0; j < countStr.length(); j++) {
+        //                 chars[writeIndex++] = countStr.charAt(j);
+        //             }
+        //         }
+
+        //         return writeIndex;
+
+        ////WAY 2
         if (chars == null || chars.length == 0) {
             return 0;
         }
-        
-        int writeIndex = 0;
-        char prevChar = chars[0];
+
+        int apoint = 0;
         int count = 1;
-        
+        char prev = chars[0];
         for (int i = 1; i < chars.length; i++) {
-            if (chars[i] == prevChar) {
-                count++;
+            char current = chars[i];
+            if (current == prev) {
+                count += 1;
             } else {
-                chars[writeIndex++] = prevChar;
+                chars[apoint++] = prev;
                 if (count > 1) {
                     String countStr = Integer.toString(count);
                     for (int j = 0; j < countStr.length(); j++) {
-                        chars[writeIndex++] = countStr.charAt(j);
+                        chars[apoint++] = countStr.charAt(j);
                     }
                 }
-                prevChar = chars[i];
+                prev = current;
                 count = 1;
             }
         }
-        chars[writeIndex++] = prevChar;
+        chars[apoint++] = prev;
         if (count > 1) {
             String countStr = Integer.toString(count);
             for (int j = 0; j < countStr.length(); j++) {
-                chars[writeIndex++] = countStr.charAt(j);
+                chars[apoint++] = countStr.charAt(j);
             }
         }
-        
-        return writeIndex;
+        return apoint;
     }
 }
