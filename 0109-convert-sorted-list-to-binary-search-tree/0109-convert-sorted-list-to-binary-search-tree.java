f35ccb29c8e0ad79025be28d5e9a43d00dc6cdb1
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-03-11 22:24:51 +0530

    Time: 1 ms (40.56%), Space: 43.7 MB (36.94%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -25,20 +25,44 @@
  */
 class Solution {
 
-    public TreeNode sortedListToBST(ListNode head) {
-        if (head == null) return null;
-        if (head.next == null) return new TreeNode(head.val);
-        ListNode slow = head, fast = head.next;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode mid = slow.next;
-        slow.next = null;
-        TreeNode root = new TreeNode(mid.val);
+    // public TreeNode sortedListToBST(ListNode head) {
+    //     if (head == null) return null;
+    //     if (head.next == null) return new TreeNode(head.val);
+    //     ListNode slow = head, fast = head.next;
+    //     while (fast.next != null && fast.next.next != null) {
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
+    //     ListNode mid = slow.next;
+    //     slow.next = null;
+    //     TreeNode root = new TreeNode(mid.val);
+    //     root.left = sortedListToBST(head);
+    //     root.right = sortedListToBST(mid.next);
+    //     return root;
+    // }
 
-        root.left = sortedListToBST(head);
-        root.right = sortedListToBST(mid.next);
-        return root;
+    ////WAY 2
+
+    private TreeNode createBST(Integer[] nodeValues, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nodeValues[mid]);
+        node.left = createBST(nodeValues, left, mid - 1);
+        node.right = createBST(nodeValues, mid + 1, right);
+        return node;
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        List<Integer> nodeValues = new ArrayList<Integer>();
+        ListNode curr = head;
+        int size = 0;
+        while (curr != null) {
+            nodeValues.add(curr.val);
+            curr = curr.next;
+            size++;
+        }
+        return createBST(nodeValues.toArray(new Integer[size]), 0, size - 1);
     }
 }
