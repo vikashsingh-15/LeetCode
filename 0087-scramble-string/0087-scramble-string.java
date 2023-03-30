c44fdc1750b06ccb4b2bf5fcf32cf58127abea5a
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-03-30 11:23:20 +0530

    Time: 9 ms (81.20%), Space: 42.4 MB (84.62%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isScramble(String s1, String s2) {
+        int n = s1.length();
+        if (n != s2.length()) {
+            return false;
+        }
+        boolean[][][] dp = new boolean[n + 1][n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[1][i][j] = true;
+                }
+            }
+        }
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                for (int j = 0; j <= n - l; j++) {
+                    for (int k = 1; k < l; k++) {
+                        if ((dp[k][i][j] && dp[l - k][i + k][j + k]) ||
+                            (dp[k][i][j + l - k] && dp[l - k][i + k][j])) {
+                            dp[l][i][j] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[n][0][0];
+    }
+}
