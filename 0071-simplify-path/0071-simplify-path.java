3527bc35a324348776a906e858e86701892c5624
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-07-15 20:45:28 +0530

    Time: 5 ms (63.87%), Space: 43.6 MB (59.11%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,19 +1,38 @@
 class Solution {
     public String simplifyPath(String path) {
-          String[] paths = path.split("/");
-        int left = -1, right = 0;
-        while (right < paths.length) {
-        	if (paths[right].equals(".."))
-        		left = left == -1 ? -1 : left - 1;
-        	else if (!paths[right].equals(".") && !paths[right].equals(""))
-        		paths[++left] = paths[right];
-        	right++;
+        //   String[] paths = path.split("/");
+        // int left = -1, right = 0;
+        // while (right < paths.length) {
+        // 	if (paths[right].equals(".."))
+        // 		left = left == -1 ? -1 : left - 1;
+        // 	else if (!paths[right].equals(".") && !paths[right].equals(""))
+        // 		paths[++left] = paths[right];
+        // 	right++;
+        // }
+        // if (left == -1)
+        // 	return "/";
+        // StringBuilder buf = new StringBuilder();
+        // for (int i = 0; i <= left; i++)
+        // 	buf.append('/').append(paths[i]);
+        // return buf.toString();
+        
+        
+        //way 2
+        
+        Stack<String>stack=new Stack<>();
+        String[] directories = path.split("/"); 
+        for(String dir:directories){
+            if(dir.equals(".") || dir.isEmpty())continue;
+            else if(dir.equals("..")){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }else{
+                stack.push(dir);
+            }
         }
-        if (left == -1)
-        	return "/";
-        StringBuilder buf = new StringBuilder();
-        for (int i = 0; i <= left; i++)
-        	buf.append('/').append(paths[i]);
-        return buf.toString();
-    }
+        
+        return "/" + String.join("/", stack); 
+        
+        }
 }
