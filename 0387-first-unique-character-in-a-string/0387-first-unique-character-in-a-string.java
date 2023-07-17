fc9651f4fa55bdef92aca4cdd4afcf79cfc3eeb9
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-07-17 12:50:59 +0530

    Time: 39 ms (39.83%), Space: 44.3 MB (37.51%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        int ans=-1;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            int val=map.get(c);
+            if(val<2){
+                return i;
+                // break;
+            }
+        }
+        return ans;
+        
+        
+        
+        
+
+        
+//         for(int i=0;i<s.length();i++){
+//             char c=s.charAt(i);
+//             int firstIndex=s.indexOf(c);
+//             int lastIndex=s.lastIndexOf(c);
+//             if(lastIndex==firstIndex){
+//                 return i;
+//             } 
+//         }
+//         return -1;
+        
+        
+        
+        
+        
+//         int ans = Integer.MAX_VALUE;
+        
+//         for(char c='a'; c<='z';c++){
+//             int index = s.indexOf(c);//default value of s.indexOf(c);=-1
+//             // System.out.println(index);
+//             if(index!=-1&&index==s.lastIndexOf(c)){
+//                 ans = Math.min(ans,index);
+//             }
+//         }
+//         return ans==Integer.MAX_VALUE?-1:ans;
+        
+    }
+}
