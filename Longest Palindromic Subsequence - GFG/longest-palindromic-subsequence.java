445750484c662298f4488d0deb8fa9816a5240b1
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-07-19 21:26:36 +0530

    Added solution - LeetHub

---
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Solution obj = new Solution();
+                    System.out.println(obj.longestPalinSubseq(s));
+                }
+                
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int longestPalinSubseq(String s)
+    {
+       int n = s.length();
+        int[][] dp = new int[n][n];
+        //move on gap i.e, diagonally
+        for (int gap = 0; gap < n; gap++) {
+            for (int j = gap, i = 0; j < n; i++, j++) {
+                if (gap == 0) {
+                    dp[i][j] = 1;
+                } else if (gap == 1) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j) ? 2 : 1;
+                } else {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = 2 + dp[i + 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1];
+
+    }
+}
