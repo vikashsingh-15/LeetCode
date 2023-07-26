38e318c8c3a3e0ff2fec305109e8e2a2ceac3e58
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-07-27 00:35:04 +0530

    Time: 265 ms (14.53%), Space: 59.1 MB (38.41%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int length = dist.length;
+       int start=1;
+        int end=Integer.MAX_VALUE;
+        int ans=-1;
+        
+        while(start<end){
+             int mid = start + (end-start)/2;
+            if(isPossible(dist,hour,mid)){
+                ans = mid;
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+        return ans;  
+    }
+    
+    
+    
+     public boolean isPossible(int[] dist,double hour,int speed){
+         int length=dist.length;
+        double ans = 0;
+        for(int i=0;i<length;i++){
+            double time = dist[i]*1.0/speed;
+            if(i!=length-1)ans = ans + Math.ceil(time);
+            else ans += time;
+            if(ans>hour) return false;
+        }
+        return ans<=hour;
+    }
+    
+    
+}
