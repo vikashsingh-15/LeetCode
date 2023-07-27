4c122178038d5749330f656b339ec6c075e2313c
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-07-27 11:18:11 +0530

    Time: 19 ms (86.36%), Space: 56.9 MB (63.64%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,25 +1,29 @@
 class Solution {
+
     public long maxRunTime(int n, int[] batteries) {
-    long sumPower = 0;
-        for (int power : batteries)
+        long sumPower = 0;
+        for (int power : batteries) {
             sumPower += power;
-        long left = 1, right = sumPower / n;
-        
-        while (left < right){
-            long time = (left+right+1) / 2;
-            if (check(batteries, n, time))
-                left = time;
-            else
-                right = time - 1;
         }
-        return left;
+        long left = 1, right = sumPower;
+
+        while (left < right) {
+            long mid = (left + right+1) / 2;
+            if (check(batteries, n, mid)) {
+                left = mid;
+            } else {
+                right = mid-1;
+            }
+        }
+        return right;
+        //return left;
     }
-    
-    public boolean check(int [] B, int n, long time){
+
+    public boolean check(int[] B, int n, long time) {
         long sum = 0;
-        for(int battery: B){
-            sum+=Math.min(battery, time);
+        for (int battery : B) {
+            sum += Math.min(battery, time);
         }
-        return (sum>=(long)time*n);
+        return (sum >= (long) time * n);
     }
 }
