5fe81855710834417cc37462f903e1a2b4d92b53
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-04 14:48:58 +0530

    Time: 1 ms (80.30%), Space: 44.4 MB (12.84%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -8,7 +8,6 @@ class Solution {
         //             seenOnce = ~seenTwice & (seenOnce ^ num);
         //             seenTwice = ~seenOnce & (seenTwice ^ num);
         //         }
-
         //         return seenOnce;
 
         //WAY 2
@@ -53,15 +52,33 @@ class Solution {
 
         //way 4
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int num : nums) {
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+        // for (int num : map.keySet()) {
+        //     if (map.get(num) == 1) {
+        //         return num;
+        //     }
+        // }
+        // return -1;
+        
+        //way 5
+        
+         int ones = 0;
+        int twos = 0;
+
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            // Update 'twos' when 'ones' has the same bit as the current number
+            twos |= (ones & num);
+            // Update 'ones' to keep track of the bits that appear exactly once
+            ones ^= num;
+            // Mask to clear the bits that appear three times in both 'ones' and 'twos'
+            int mask = ~(ones & twos);
+            ones &= mask;
+            twos &= mask;
         }
-        for (int num : map.keySet()) {
-            if (map.get(num) == 1) {
-                return num;
-            }
-        }
-        return -1;
+
+        return ones;
     }
 }
