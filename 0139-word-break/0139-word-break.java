72e313a71cce077464c6b6f9bd456224949f679a
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-04 14:31:53 +0530

    Time: 7 ms (60.52%), Space: 43.6 MB (46.68%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -5,7 +5,7 @@ class Solution {
         boolean[] dp = new boolean[n + 1];
         dp[0] = true;
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (dp[j] && wordSet.contains(s.substring(j, i))) {
                     dp[i] = true;
