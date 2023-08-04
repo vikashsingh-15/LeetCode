ef1fedd47463c0a73f426a5fd3f04a1117ab186f
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-04 14:27:30 +0530

    Added solution - LeetHub

--- Reverse a Stack - GFG/reverse-a-stack.java
+++ Reverse a Stack - GFG/reverse-a-stack.java
@@ -56,13 +56,12 @@ class Solution
     }
 
      static void insertAtBottom(Stack<Integer> stack, int element) {
-        if (stack.isEmpty()) {
-            stack.push(element);
-            return;
-        }
-
-        int temp = stack.pop();
-        insertAtBottom(stack, element);
-        stack.push(temp);
+       if(stack.isEmpty()){
+           stack.push(element);
+           return;
+       }
+       int temp=stack.pop();
+       insertAtBottom(stack,element);
+       stack.push(temp);
     }
 }
