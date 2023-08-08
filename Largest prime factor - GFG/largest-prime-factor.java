8b385621bc9747116d126b5aee1b4c4d240dc2f0
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-09 00:50:12 +0530

    Added solution - LeetHub

---
+++ Largest prime factor - GFG/largest-prime-factor.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.largestPrimeFactor(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long largestPrimeFactor(int N) {
+         long maxPrime = -1;
+
+        // Divide the number by 2 until it becomes odd
+        while (N % 2 == 0) {
+            maxPrime = 2;
+            N /= 2;
+        }
+
+        // Check for odd factors from 3 to the square root of N
+        for (int i = 3; i <= Math.sqrt(N); i += 2) {
+            while (N % i == 0) {
+                maxPrime = i;
+                N /= i;
+            }
+        }
+
+        // If N becomes a prime greater than 2
+        if (N > 2) {
+            maxPrime = N;
+        }
+
+        return maxPrime;
+    }
+}
