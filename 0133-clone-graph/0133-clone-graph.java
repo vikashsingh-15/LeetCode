104f00d8abfec449d000c2013a78444fd3f0b5be
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-09 20:36:02 +0530

    Time: 26 ms (58.44%), Space: 42 MB (64.17%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -51,3 +51,31 @@ class Solution {
         return newNode;
     }
 }
+
+
+// class Solution {
+//     public Node cloneGraph(Node node) {
+//       if (node == null) {
+//             return null;
+//         }
+
+//         Map<Node, Node> visited = new HashMap<>();
+//         Queue<Node> queue = new LinkedList<>();
+//         queue.offer(node);
+//         visited.put(node, new Node(node.val));
+
+//         while (!queue.isEmpty()) {
+//             Node curr = queue.poll();
+
+//             for (Node neighbor : curr.neighbors) {
+//                 if (!visited.containsKey(neighbor)) {
+//                     visited.put(neighbor, new Node(neighbor.val));
+//                     queue.offer(neighbor);
+//                 }
+//                 visited.get(curr).neighbors.add(visited.get(neighbor));
+//             }
+//         }
+
+//         return visited.get(node);
+//     }
+// }
