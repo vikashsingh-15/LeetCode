5dba92afeb2c5357a6e5c345e9db75eb865a4ee4
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-13 00:25:21 +0530

    Time: 0 ms (100%), Space: 40.8 MB (61.5%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,18 +1,33 @@
 class Solution {
 
     public double myPow(double x, int n) {
-       if(x == 0.0f) return 0.0d;
-        long b = n;
-        double res = 1.0;
-        if(b < 0) {
-            x = 1 / x;
-            b = -b;
+        if (n == 0) {
+            return 1.0;
         }
-        while(b > 0) {
-            if(b % 2 == 1) res *= x;
-            x *= x;
-             b /= 2;
+
+        if (n < 0) {
+            return 1.0 / (x * myPow(x, -(n + 1)));
         }
-        return res;
+
+        if (n % 2 == 0) {
+            return myPow (x * x, n / 2);
+        } else {
+            return x * myPow (x, n - 1);
+        }
+        ////way 2
+
+        //         if (x == 0.0f) return 0.0d;
+        //         long b = n;
+        //         double res = 1.0;
+        //         if (b < 0) {
+        //             x = 1 / x;
+        //             b = -b;
+        //         }
+        //         while (b > 0) {
+        //             if (b % 2 == 1) res *= x;
+        //             x *= x;
+        //             b /= 2;
+        //         }
+        //         return res;
     }
 }
