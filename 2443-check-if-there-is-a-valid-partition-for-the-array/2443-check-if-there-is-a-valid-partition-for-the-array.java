c6aa066fa9b8765687be303ff794c817c5bb5663
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-13 13:56:53 +0530

    Time: 7 ms (82.52%), Space: 56.6 MB (78.64%) - LeetHub

--- 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,23 +1,44 @@
+// class Solution {
+//     public boolean validPartition(int[] nums) {
+//        int n=nums.length;
+//         boolean dp[]=new boolean[n+1];
+//         dp[n]=true;
+//         for(int i=n-1;i>=0;i--){
+//             if(i+1<n){
+// 				//cond.1 for 2 same ele
+//                 if(nums[i]==nums[i+1] && dp[i+2]){
+//                     dp[i]=true;
+//                 }
+//                 if(i+2<n){
+// 					//cond. 2 for 3 same ele
+// 					//cond. 3 for difference between adjacent elements is 1.
+//                     if( ( (nums[i]==nums[i+1] && nums[i+1]==nums[i+2]) || (nums[i]+1==nums[i+1] && nums[i+1]+1==nums[i+2]) ) && dp[i+3] ){
+//                         dp[i]=true;
+//                     }
+//                 }
+//             }
+//         }
+//         return dp[0];
+//     }
+// }
+
 class Solution {
+
     public boolean validPartition(int[] nums) {
-       int n=nums.length;
-        boolean dp[]=new boolean[n+1];
-        dp[n]=true;
-        for(int i=n-1;i>=0;i--){
-            if(i+1<n){
-				//cond.1 for 2 same ele
-                if(nums[i]==nums[i+1] && dp[i+2]){
-                    dp[i]=true;
-                }
-                if(i+2<n){
-					//cond. 2 for 3 same ele
-					//cond. 3 for difference between adjacent elements is 1.
-                    if( ( (nums[i]==nums[i+1] && nums[i+1]==nums[i+2]) || (nums[i]+1==nums[i+1] && nums[i+1]+1==nums[i+2]) ) && dp[i+3] ){
-                        dp[i]=true;
-                    }
-                }
+        int n = nums.length;
+        boolean[] dp = new boolean[n + 1];
+        dp[n] = true;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + 1 < n && ((nums[i] == nums[i + 1] && dp[i + 2]))) {
+                dp[i] = true;
+            }
+
+            if (i + 2 < n && (((nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2]) || (nums[i] + 1 == nums[i + 1] && nums[i + 1] + 1 == nums[i + 2])) && dp[i + 3])) {
+                dp[i] = true;
             }
         }
+
         return dp[0];
     }
 }
