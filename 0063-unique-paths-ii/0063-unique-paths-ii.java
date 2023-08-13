009a97f52e95866bca51356c2bf697608f926feb
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-13 13:23:16 +0530

    Time: 0 ms (100%), Space: 40.4 MB (81.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,24 +1,26 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-          if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0 || obstacleGrid[0][0] == 1) {
+    public int uniquePathsWithObstacles(int[][] arr) {
+       int n = arr.length;
+        int m = arr[0].length;
+        if (arr[n-1][m-1] == 1) {
             return 0;
         }
-
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-
-        int[] previous = new int[n];
-        int[] current = new int[n];
-        previous[0] = 1;
-
-        for (int i = 0; i < m; i++) {
-            current[0] = obstacleGrid[i][0] == 1 ? 0 : previous[0];
-            for (int j = 1; j < n; j++) {
-                current[j] = obstacleGrid[i][j] == 1 ? 0 : current[j-1] + previous[j];
+        
+        int[][] dp = new int[n][m];
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = m-1; j >= 0; j--) {
+                if (i == n-1 && j == m-1 && arr[i][j] == 0) {
+                    dp[i][j] = 1;
+                } else if (j == m-1 && arr[i][j] == 0) {
+                    dp[i][j] = dp[i+1][j];
+                } else if (i == n-1 && arr[i][j] == 0) {
+                    dp[i][j] = dp[i][j+1];
+                } else if (arr[i][j] == 0) {
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+                }
             }
-            System.arraycopy(current, 0, previous, 0, n);
         }
-
-        return previous[n-1];
+        
+        return dp[0][0];
     }
 }
