6f0c0f0dff7ebaab5ec7c3c957f8d28989405855
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-16 14:06:11 +0530

    Updated Solution - GfG to GitHub

--- Medium/Nth catalan number/nth-catalan-number.java
+++ Medium/Nth catalan number/nth-catalan-number.java
@@ -37,9 +37,10 @@ class Solution {
         catalan[1] = 1;
 
         for (int i = 2; i <= n; i++) {
-            catalan[i] = 0;
+            catalan[i]=0;
             for (int j = 0; j < i; j++) {
-                catalan[i] = (int) ((catalan[i] + (long) catalan[j] * catalan[i - j - 1]) % MOD);
+                catalan[i]=(int)((catalan[i]+(long)catalan[j]*catalan[i-j-1])%MOD);
+                // catalan[i] = (int) ((catalan[i] + (long) catalan[j] * catalan[i - j - 1]) % MOD);
             }
         }
 
