58fe324d03e4bea3fd4fc9e37a5ec7f9437f25f0
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-17 11:55:44 +0530

    Time: 1 ms (32.05%), Space: 40.3 MB (43.37%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -13,15 +13,60 @@
  *     }
  * }
  */
+
 class Solution {
+    LinkedList<ArrayList<Integer>> list = new LinkedList<>();
     public int sumNumbers(TreeNode root) {
-         return helper(root,0);
+        helper(root, new ArrayList<Integer>());
+        int sum = 0;
+        int num = 0;
+        for (int i = 0; i < list.size(); i++) {
+            // System.out.println(list.get(i));
+            for (int j : list.get(i)) {
+                num = num * 10 + j;
+            }
+            sum = sum + num;
+            num = 0;
+        }
+        return sum;
     }
-    int helper(TreeNode root,int sum){
 
-        if(root==null)return 0;
-        if(root.left==null && root.right==null)return sum*10+root.val;
-        return helper(root.left,root.val+sum*10)+helper(root.right,sum*10+root.val);
-
+    void helper(TreeNode root, ArrayList<Integer> path) {
+        if (root == null) return;
+        path.add(root.val);
+        if (root.left == null && root.right == null) {
+            list.add(new ArrayList<Integer>(path));
+        } else {
+            helper(root.left, path);
+            helper(root.right, path);
+        }
+        path.remove(path.size() - 1);
     }
 }
+
+
+// way 2
+
+// class Solution {
+
+//     public int sumNumbers(TreeNode root) {
+//         return dfs(root, 0);
+//     }
+
+//     static int dfs(TreeNode node, int currentNum) {
+//         if (node == null) {
+//             return 0;
+//         }
+
+//         currentNum = currentNum * 10 + node.val;
+
+//         if (node.left == null && node.right == null) {
+//             return currentNum;
+//         }
+
+//         int leftSum = dfs(node.left, currentNum);
+//         int rightSum = dfs(node.right, currentNum);
+
+//         return leftSum + rightSum;
+//     }
+// }
