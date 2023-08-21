09691ed98284eead6ef95b14bd2b1702f46da02f
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-21 13:15:16 +0530

    Time: 36 ms (64.96%), Space: 44.1 MB (74.78%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,18 +1,39 @@
+// class Solution {
+
+//     public boolean repeatedSubstringPattern(String s) {
+//         int n = s.length();
+//         for (int len = 1; len <= n / 2; len++) {
+//             String pattern = s.substring(0, len);
+//             StringBuilder repeated = new StringBuilder();
+//             for (int i = 0; i < n/len; i++) {
+//                 repeated.append(pattern);
+
+//                 if (repeated.toString().equals(s)) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
 class Solution {
-
     public boolean repeatedSubstringPattern(String s) {
-        int n = s.length();
+        int n = s.length();   
         for (int len = 1; len <= n / 2; len++) {
-            String pattern = s.substring(0, len);
-            StringBuilder repeated = new StringBuilder();
-            for (int i = 0; i < n/len; i++) {
-                repeated.append(pattern);
-
+            if (n % len == 0) {
+                String pattern = s.substring(0, len);
+                StringBuilder repeated = new StringBuilder();   
+                for (int i = 0; i < n / len; i++) {
+                    repeated.append(pattern);
+                }   
                 if (repeated.toString().equals(s)) {
                     return true;
                 }
             }
-        }
+        }  
         return false;
     }
 }
+
