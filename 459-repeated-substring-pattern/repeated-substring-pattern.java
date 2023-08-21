af6d6dbc76454fc2cdb1ff7f9e3cab41aa3dd2b1
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-21 13:15:36 +0530

    Time: 501 ms (6.96%) | Memory: 45.3 MB (5.09%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -17,23 +17,22 @@
 //     }
 // }
 
-
 class Solution {
+
     public boolean repeatedSubstringPattern(String s) {
-        int n = s.length();   
+        int n = s.length();
         for (int len = 1; len <= n / 2; len++) {
             if (n % len == 0) {
                 String pattern = s.substring(0, len);
-                StringBuilder repeated = new StringBuilder();   
+                StringBuilder repeated = new StringBuilder();
                 for (int i = 0; i < n / len; i++) {
                     repeated.append(pattern);
-                }   
-                if (repeated.toString().equals(s)) {
-                    return true;
+                    if (repeated.toString().equals(s)) {
+                        return true;
+                    }
                 }
             }
-        }  
+        }
         return false;
     }
 }
-
