9272c0b83a07b87d74b58cdb68014df391108380
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-21 12:54:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Surround the 1's/surround-the-1s.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] matrix = new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++)
+                    matrix[i][j] = Integer.parseInt(S[j]);
+            }
+            Solution ob = new Solution();
+            int ans = ob.Count(matrix);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int  Count(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int count = 0;
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+    
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 1) {
+                    int zeroCount = 0;
+                    for(int k=0;k<8;k++){
+                        int newX=i +dx[k];
+                        int newY=j+ dy[k];
+                        if (newX>=0 && newX<n && newY>=0 && newY<m  && matrix[newX][newY] == 0) {
+                            zeroCount++;
+                        }
+
+                    }
+                   
+                    if (zeroCount>0 && zeroCount % 2 == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+        
+    }
+}
