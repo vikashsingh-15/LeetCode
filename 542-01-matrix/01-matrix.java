9e2b2221e3fa780624047b775d443694851328d5
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-22 01:29:51 +0530

    Time: 13 ms (71.18%) | Memory: 45.2 MB (88.13%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,24 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
+
     public int[][] updateMatrix(int[][] mat) {
-         int m = mat.length;
+        int m = mat.length;
         int n = mat[0].length;
         int[][] result = new int[m][n];
+
         Queue<int[]> queue = new LinkedList<>();
 
-        // Initialize result matrix with maximum possible distance
+        // Initialize result matrix with -1 and enqueue cells with value 1
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    result[i][j] = 0;
-                    queue.offer(new int[]{i, j});
+                if (mat[i][j] == 1) {
+                    result[i][j] = -1;
                 } else {
-                    result[i][j] = Integer.MAX_VALUE;
+                    queue.offer(new int[]{i, j});
                 }
             }
         }
 
         int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
+        // Perform BFS traversal
         while (!queue.isEmpty()) {
             int[] cell = queue.poll();
             int row = cell[0];
@@ -28,11 +33,9 @@ class Solution {
                 int newRow = row + direction[0];
                 int newCol = col + direction[1];
 
-                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
-                    if (result[newRow][newCol] > result[row][col] + 1) {
-                        result[newRow][newCol] = result[row][col] + 1;
-                        queue.offer(new int[]{newRow, newCol});
-                    }
+                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && result[newRow][newCol] == -1) {
+                    result[newRow][newCol] = result[row][col] + 1;
+                    queue.offer(new int[]{newRow, newCol});
                 }
             }
         }
