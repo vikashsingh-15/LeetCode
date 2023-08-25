4812714442a13bf687c7c03d4454ebee1ded094e
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-25 11:51:26 +0530

    Time: 72 ms (88.70%) | Memory: 60.3 MB (60.32%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.java
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.java
@@ -3,7 +3,10 @@ class Solution {
          int n = nums1.length;
         int[][] res = new int[n][2];
         for(int i=0; i<n; i++){
-            res[i] = new int[]{nums1[i], nums2[i]};
+            res[i][0]=nums1[i];
+            res[i][1]=nums2[i];
+            // or
+            // res[i] = new int[]{nums1[i], nums2[i]};
         }
 
         Arrays.sort(res, (a, b) -> b[1] - a[1]);
@@ -11,13 +14,14 @@ class Solution {
         long ans=0;
         long sum=0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=0; i<n; i++){
-            sum += res[i][0];
-            pq.offer(res[i][0]);
-            if(pq.size() == k){
-                ans = Math.max(ans, sum*res[i][1]);
-                sum -= pq.poll();
+        for(int i=0;i<n;i++){
+            pq.add(res[i][0]);
+            sum+=res[i][0];
+            if(pq.size()==k){
+                ans=Math.max(ans,sum*res[i][1]);
+                sum-=pq.poll();
             }
+
         }
         return ans;
     }
