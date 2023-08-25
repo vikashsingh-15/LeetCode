2b8763bba22e4631c0f11b9a80d92d7b86640dc0
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-25 16:12:15 +0530

    Time: 9 ms (17.67%) | Memory: 40.4 MB (96.33%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -73,6 +73,52 @@
 
 ////way 3
 //DP _ TABULATION
+
+// class Solution {
+
+//     public boolean isInterleave(String s1, String s2, String s3) {
+//         int rows = s1.length();
+//         int cols = s2.length();
+//         int len = s3.length();
+
+//         if (rows + cols != len) {
+//             return false;
+//         }
+
+//         boolean[][] dp = new boolean[rows + 1][cols + 1];
+
+//         dp[0][0] = true;
+
+//         for (int i = 1; i <= rows; i++) {
+//             if (s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0]) {
+//                 dp[i][0] = true;
+//             }else{
+//                 dp[i][0]=false;
+//             }
+//         }
+
+//         for (int j = 1; j <= cols; j++) {
+//             if (s2.charAt(j - 1) == s3.charAt(j - 1) &&  dp[0][j - 1]) {
+//                 dp[0][j] = true;
+//             }else{
+//                 dp[0][j]=false;
+//             }
+//         }
+
+//         for (int i = 1; i <= rows; i++) {
+//             for (int j = 1; j <= cols; j++) {
+//                 int s3point = i + j - 1;
+//                 if ((s1.charAt(i - 1) == s3.charAt(s3point) && dp[i - 1][j]) || (s2.charAt(j - 1) == s3.charAt(s3point) && dp[i][j - 1])) {
+//                     dp[i][j] = true;
+//                 }
+//             }
+//         }
+//         return dp[rows][cols];
+//     }
+// }
+
+
+
 class Solution {
 
     public boolean isInterleave(String s1, String s2, String s3) {
@@ -88,27 +134,26 @@ class Solution {
 
         dp[0][0] = true;
 
-        for (int i = 1; i <= rows; i++) {
-            if (s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0]) {
-                dp[i][0] = true;
-            }else{
-                dp[i][0]=false;
-            }
-        }
+        
 
-        for (int j = 1; j <= cols; j++) {
-            if (s2.charAt(j - 1) == s3.charAt(j - 1) &&  dp[0][j - 1]) {
-                dp[0][j] = true;
-            }else{
-                dp[0][j]=false;
-            }
-        }
-
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= cols; j++) {
                 int s3point = i + j - 1;
-                if ((s1.charAt(i - 1) == s3.charAt(s3point) && dp[i - 1][j]) || (s2.charAt(j - 1) == s3.charAt(s3point) && dp[i][j - 1])) {
-                    dp[i][j] = true;
+                int s2point=j-1;
+                int s1point=i-1;
+                if(i==0 && j==0){
+                    dp[i][j]=true;
+                }else if(i==0){
+                    if(s2.charAt(s2point)==s3.charAt(s3point) && dp[0][j-1]){
+                        dp[i][j]=true;
+                    }
+                }else if(j==0){
+                     if(s1.charAt(s1point)==s3.charAt(s3point) && dp[i-1][0]){
+                        dp[i][j]=true;
+                    }
+                }else{
+                     dp[i][j] = (s1.charAt(s1point) == s3.charAt(s3point) && dp[i - 1][j])
+                            || (s2.charAt(s2point) == s3.charAt(s3point) && dp[i][j - 1]);
                 }
             }
         }
