20a5bdfde7188b35a452a106fd877ab1e8c1ed5f
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-25 15:57:49 +0530

    Time: 7 ms (33.37%) | Memory: 40.6 MB (77.17%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -30,51 +30,88 @@
 //     }
 // }
 
-class Solution {
+//// way 2
 
-    public boolean isInterleave(String s1, String s2, String s3) {
-        if(s1.length()+s2.length()!=s3.length()){
-            return false;
-        }
-         Boolean[][] memo = new Boolean[s1.length() + 1][s2.length() + 1];
-        return isInterleaveHelper(s1,s2,s3,0,0,0,memo);
-    }
+//// optimized Recursion
 
-    public boolean isInterleaveHelper(String s1, String s2, String s3, int s1Point, int s2Point, int s3Point,Boolean[][] memo) {
-        if(s3Point==s3.length()){
-            return true;
-        }
-        if (memo[s1Point][s2Point] != null) {
-            return memo[s1Point][s2Point];
-        }
-        if((s1Point<s1.length()) && (s1.charAt(s1Point)==s3.charAt(s3Point))){
-            boolean f1=isInterleaveHelper(s1,s2,s3,s1Point+1,s2Point,s3Point+1,memo);
-            memo[s1Point][s2Point]=f1;
-            if(f1==true){
-                return true;
-            }
-        }
-         if(s2Point<s2.length() && s2.charAt(s2Point)==s3.charAt(s3Point)){
-            boolean f2=isInterleaveHelper(s1,s2,s3,s1Point,s2Point+1,s3Point+1,memo);
-            memo[s1Point][s2Point]=f2;
-            if(f2==true){
-                return true;
-            }
-        }
-        memo[s1Point][s2Point]=false;
-         return false;
-
-    }
-}
 // class Solution {
 
 //     public boolean isInterleave(String s1, String s2, String s3) {
-
-        
+//         if(s1.length()+s2.length()!=s3.length()){
+//             return false;
+//         }
+//          Boolean[][] memo = new Boolean[s1.length() + 1][s2.length() + 1];
+//         return isInterleaveHelper(s1,s2,s3,0,0,0,memo);
 //     }
 
-//     public boolean isInterleaveHelper(String s1, String s2, String s3, int s1Point, int s2Point, int s3Point) {
-
+//     public boolean isInterleaveHelper(String s1, String s2, String s3, int s1Point, int s2Point, int s3Point,Boolean[][] memo) {
+//         if(s3Point==s3.length()){
+//             return true;
+//         }
+//         if (memo[s1Point][s2Point] != null) {
+//             return memo[s1Point][s2Point];
+//         }
+//         if((s1Point<s1.length()) && (s1.charAt(s1Point)==s3.charAt(s3Point))){
+//             boolean f1=isInterleaveHelper(s1,s2,s3,s1Point+1,s2Point,s3Point+1,memo);
+//             memo[s1Point][s2Point]=f1;
+//             if(f1==true){
+//                 return true;
+//             }
+//         }
+//          if(s2Point<s2.length() && s2.charAt(s2Point)==s3.charAt(s3Point)){
+//             boolean f2=isInterleaveHelper(s1,s2,s3,s1Point,s2Point+1,s3Point+1,memo);
+//             memo[s1Point][s2Point]=f2;
+//             if(f2==true){
+//                 return true;
+//             }
+//         }
+//         memo[s1Point][s2Point]=false;
+//          return false;
 
 //     }
 // }
+
+////way 3
+//DP _ TABULATION
+class Solution {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int rows = s1.length();
+        int cols = s2.length();
+        int len = s3.length();
+
+        if (rows + cols != len) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[rows + 1][cols + 1];
+
+        dp[0][0] = true;
+
+        for (int i = 1; i <= rows; i++) {
+            if (s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0]) {
+                dp[i][0] = true;
+            }else{
+                dp[i][0]=false;
+            }
+        }
+
+        for (int j = 1; j <= cols; j++) {
+            if (s2.charAt(j - 1) == s3.charAt(j - 1) &&  dp[0][j - 1]) {
+                dp[0][j] = true;
+            }else{
+                dp[0][j]=false;
+            }
+        }
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                int s3point = i + j - 1;
+                if ((s1.charAt(i - 1) == s3.charAt(s3point) && dp[i - 1][j]) || (s2.charAt(j - 1) == s3.charAt(s3point) && dp[i][j - 1])) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[rows][cols];
+    }
+}
