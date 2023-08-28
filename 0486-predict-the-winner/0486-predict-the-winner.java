0410644753b098a5936f776f130389423799ca18
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-28 20:29:01 +0530

    Time: 0 ms (100%), Space: 39.6 MB (80.97%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,40 +1,43 @@
-class Solution {
-        public boolean predictTheWinner(int[] nums) {
-        return canFirstPlayerWin(nums, 0, nums.length - 1)>=0;
-    }
-
-    private int canFirstPlayerWin(int[] nums, int start, int end) {
-        if (start == end) {
-            return nums[start];
-        }
-        int chooseStart = nums[start] - canFirstPlayerWin(nums, start + 1, end);
-        int chooseEnd = nums[end] - canFirstPlayerWin(nums, start, end - 1);
-        return Math.max(chooseStart, chooseEnd);
-    }
-
-}
-
-
 // class Solution {
-//     public boolean predictTheWinner(int[] nums) {
-
-//         int n = nums.length;
-
-//         int[][] dp = new int[n][n];
-//         for (int gap = 0; gap < n; gap++) {
-//             for (int i = 0,j=gap; j < n;j++, i++) {
-//                if(gap==0){
-//                    dp[i][i] = nums[i];
-//                }else{
-//                     dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
-//                }
-
-//             }
-//         }
-//         return dp[0][n - 1] >= 0;
-
+//         public boolean predictTheWinner(int[] nums) {
+//         return canFirstPlayerWin(nums, 0, nums.length - 1)>=0;
 //     }
+
+//     private int canFirstPlayerWin(int[] nums, int start, int end) {
+//         if (start == end) {
+//             return nums[start];
+//         }
+//         int chooseStart = nums[start] - canFirstPlayerWin(nums, start + 1, end);
+//         int chooseEnd = nums[end] - canFirstPlayerWin(nums, start, end - 1);
+//         return Math.max(chooseStart, chooseEnd);
+//     }
+
 // }
+
+
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+
+        int n = nums.length;
+
+        int[][] dp = new int[n][n];
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0,j=gap; j < n;j++, i++) {
+               if(gap==0){
+                   dp[i][i] = nums[i];
+               }else{
+                   int chooseFirst =nums[i]-dp[i+1][j];
+                   int chooseLast =nums[j]-dp[i][j-1];
+                   dp[i][j]=Math.max(chooseFirst,chooseLast);
+                    // dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+               }
+
+            }
+        }
+        return dp[0][n - 1] >= 0;
+
+    }
+}
 
 
 
