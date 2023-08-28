8ae1e2fc000e8257bcdadabeaa0aefd6d120ebed
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-08-28 20:00:31 +0530

    Time: 53 ms (25.64%) | Memory: 40.5 MB (12.17%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,66 @@
+class Solution {
+        public boolean predictTheWinner(int[] nums) {
+        return canFirstPlayerWin(nums, 0, nums.length - 1)>=0;
+    }
+
+    private int canFirstPlayerWin(int[] nums, int start, int end) {
+        if (start == end) {
+            return nums[start];
+        }
+        int chooseStart = nums[start] - canFirstPlayerWin(nums, start + 1, end);
+        int chooseEnd = nums[end] - canFirstPlayerWin(nums, start, end - 1);
+        return Math.max(chooseStart, chooseEnd);
+    }
+
+}
+
+
+// class Solution {
+//     public boolean predictTheWinner(int[] nums) {
+
+//         int n = nums.length;
+
+//         int[][] dp = new int[n][n];
+//         for (int gap = 0; gap < n; gap++) {
+//             for (int i = 0,j=gap; j < n;j++, i++) {
+//                if(gap==0){
+//                    dp[i][i] = nums[i];
+//                }else{
+//                     dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+//                }
+
+//             }
+//         }
+//         return dp[0][n - 1] >= 0;
+
+//     }
+// }
+
+
+
+
+//way 2
+
+//wrong approach will fail for 1,567,1,1 this kind of testcase
+
+// class Solution {
+
+//     public boolean predictTheWinner(int[] nums) {
+//         if(nums.length<=2){
+//             return true;
+//         }
+//         int sumEven = 0;
+//         int sumOdd = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (i % 2 == 0) {
+//                 sumEven += nums[i];
+//             } else {
+//                 sumOdd += nums[i];
+//             }
+//         }
+
+//         return sumEven >= sumOdd;
+//     }
+// }
+
