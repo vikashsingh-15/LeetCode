62e27f9c5db900637ceeb989c060a3ea18c5a9af
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-09-28 20:24:34 +0530

    Time: 27 ms (100.00%) | Memory: 55.5 MB (38.88%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -24,3 +24,44 @@ class Solution {
         return result;
     }
 }
+
+
+//// way 2 -> TLE-Memory Limit Exceeded
+
+// class Solution {
+//     public int numSubseq(int[] nums, int target) {
+//        Arrays.sort(nums);
+//         List<List<Integer>> subsequences = generateSubsequences(nums);
+//         int count = 0;
+//         int MOD = 1000000007;
+
+//         for (List<Integer> subsequence : subsequences) {
+//             if (!subsequence.isEmpty()) {
+//                 int sum = subsequence.get(0) + subsequence.get(subsequence.size() - 1);
+//                 if (sum <= target) {
+//                     count = (count + 1) % MOD;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+//      public  List<List<Integer>> generateSubsequences(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         generate(nums, 0, new ArrayList<>(), result);
+//         return result;
+//     }
+
+//     private  void generate(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+//         if (index == nums.length) {
+//             result.add(new ArrayList<>(current));
+//             return;
+//         }
+
+//         current.add(nums[index]);
+//         generate(nums, index + 1, current, result);
+
+//         current.remove(current.size() - 1);
+//         generate(nums, index + 1, current, result);
+//     }
+// }
