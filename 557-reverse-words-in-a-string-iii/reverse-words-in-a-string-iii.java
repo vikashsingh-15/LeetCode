9d2ff9d1538754c5fe980f4240281f7a29bb6842
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-10-01 22:58:28 +0530

    Time: 10 ms (39.98%) | Memory: 43.9 MB (86.13%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,21 +1,24 @@
-class Solution {
+// class Solution {
 
-    public String reverseWords(String s) {
-        s = s + " ";
-        String str = "";
-        String word = "";
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c != ' ') {
-                word = String.valueOf(c) + word;
-            } else {
-                str = str + word + " ";
-                word = "";
-            }
-        }
-        return str.trim();
-    }
-}
+//     public String reverseWords(String s) {
+//         s = s + " ";
+//         String str = "";
+//         String word = "";
+//         for (int i = 0; i < s.length(); i++) {
+//             char c = s.charAt(i);
+//             if (c != ' ') {
+//                 word = String.valueOf(c) + word;
+//             } else {
+//                 str = str + word + " ";
+//                 word = "";
+//             }
+//         }
+//         return str.trim();
+//     }
+// }
+
+////way 2
+
 // class Solution {
 //     public String reverseWords(String s) {
 //         String[] strArr = s.split("\\s+");
@@ -32,3 +35,26 @@ class Solution {
 //         return res.toString().trim();
 //     }
 // }
+
+
+
+////way 3
+
+class Solution {
+
+    public String reverseWords(String s) {
+        final StringBuilder result = new StringBuilder();
+        final StringBuilder word = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                word.append(s.charAt(i));
+            } else {
+                result.append(word.reverse());
+                result.append(" ");
+                word.setLength(0);
+            }
+        }
+        result.append(word.reverse());
+        return result.toString();
+    }
+}
