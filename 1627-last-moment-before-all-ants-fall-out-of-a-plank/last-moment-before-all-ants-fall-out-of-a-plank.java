d6e6cb51112e15cf0678359fa6432864bf300451
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-04 22:48:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (42.86%) - LeetSync

---
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+    int maxTimeToLeft = 0;
+    int maxTimeToRight = 0;
+
+    for (int antPosition : left) {
+        maxTimeToLeft = Math.max(maxTimeToLeft, antPosition);
+    }
+    // System.out.println("max Time left = "+maxTimeToLeft);
+
+    for (int antPosition : right) {
+        maxTimeToRight = Math.max(maxTimeToRight, n - antPosition);
+    }
+    //  System.out.println("max Time right = "+maxTimeToRight);
+
+    return Math.max(maxTimeToLeft, maxTimeToRight);
+}
+
+}
