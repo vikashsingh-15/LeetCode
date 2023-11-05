3805f445b7ac80f33a0f2f385874c5d63bf7101d
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-05 11:52:15 +0530

    Time: 1 ms (52.90%) | Memory: 56.4 MB (74.64%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
@@ -1,32 +1,63 @@
+// class Solution {
+//     public int getWinner(int[] arr, int k) {
+//         int maxElement=arr[0];
+//         Queue<Integer> queue=new LinkedList<>();
+//         for(int i=1;i<arr.length;i++){
+//             maxElement=Math.max(maxElement,arr[i]);
+//             queue.offer(arr[i]);
+//         }
+
+//         int curr=arr[0];
+//         int winStreak=0;
+
+//         while(!queue.isEmpty()){
+//             int opponent=queue.poll();
+
+//             if(curr>opponent){
+//                 queue.offer(opponent);
+//                 winStreak++;
+//             }else{
+//                 queue.offer(curr);
+//                 curr=opponent;
+//                 winStreak=1;
+//             }
+//            if (winStreak == k || curr == maxElement) {
+//                 return curr;
+//             }
+//         }
+        
+//         return -1;
+
+//     }
+// }
+
+
+
 class Solution {
     public int getWinner(int[] arr, int k) {
-        int maxElement=arr[0];
-        Queue<Integer> queue=new LinkedList<>();
-        for(int i=1;i<arr.length;i++){
-            maxElement=Math.max(maxElement,arr[i]);
-            queue.offer(arr[i]);
+         int maxElement = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            maxElement = Math.max(maxElement, arr[i]);
         }
-
-        int curr=arr[0];
-        int winStreak=0;
-
-        while(!queue.isEmpty()){
-            int opponent=queue.poll();
-
-            if(curr>opponent){
-                queue.offer(opponent);
-                winStreak++;
-            }else{
-                queue.offer(curr);
-                curr=opponent;
-                winStreak=1;
+        
+        int curr = arr[0];
+        int winstreak = 0;
+        
+        for (int i = 1; i < arr.length; i++) {
+            int opponent = arr[i];
+            
+            if (curr > opponent) {
+                winstreak++;
+            } else {
+                curr = opponent;
+                winstreak = 1;
             }
-           if (winStreak == k || curr == maxElement) {
+            
+            if (winstreak == k || curr == maxElement) {
                 return curr;
             }
         }
         
         return -1;
-
     }
 }
