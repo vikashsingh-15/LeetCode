41dca9ed5df7b008b5b01f7b19058cee8f7f8f6b
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-07 17:00:42 +0530

    Time: 78 ms (60.66%) | Memory: 85.7 MB (86.34%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.java
+++ 1955-seat-reservation-manager/seat-reservation-manager.java
@@ -1,23 +1,18 @@
 class SeatManager {
-    private PriorityQueue<Integer> seats; // Declare a priority queue to manage seat reservations.
-
-    // Constructor to initialize the SeatManager with 'n' seats.
+    private PriorityQueue<Integer> seats; 
     public SeatManager(int n) {
         seats = new PriorityQueue<>();
-        // Initialize the priority queue with seat numbers from 1 to 'n'.
+     
         for (int i = 1; i <= n; i++) {
             seats.offer(i);
         }
     }
-
-    // Reserve a seat.
     public int reserve() {
-            int reservedSeat = seats.poll(); // Get the smallest seat number from the queue.
-            return reservedSeat; // Return the reserved seat number.
+            int reservedSeat = seats.poll(); 
+            return reservedSeat; 
     }
 
-    // Unreserve a seat.
     public void unreserve(int seatNumber) {
-        seats.offer(seatNumber); // Add the unreserved seat back to the priority queue.
+        seats.offer(seatNumber);
     }
 }
