b053786576059f4b400c02a82ad621decd573cc7
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-08 15:07:50 +0530

    Time: 1 ms (95.30%) | Memory: 39.3 MB (96.99%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.java
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.java
@@ -1,14 +1,34 @@
+// class Solution {
+//     public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+//         int minStep = Math.max(Math.abs(sx - fx), Math.abs(sy - fy));
+//         if(minStep==0){
+//            if (t == 1) {
+//                 return false;
+//             }else{
+//                 return true;
+//             }
+//         }
+
+//         return minStep <= t;
+//     }
+// }
+
+
 class Solution {
     public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int minStep = Math.max(Math.abs(sx - fx), Math.abs(sy - fy));
-        if(minStep==0){
-           if (t == 1) {
+        if (sx == fx && sy == fy) {
+            if (t == 1) {
                 return false;
-            }else{
-                return true;
             }
         }
 
-        return minStep <= t;
+        int diffX=Math.abs(sx - fx);
+        int diffY=Math.abs(sy - fy);
+
+        int maxDiff = Math.max(diffX, diffY);
+        if(maxDiff<=t){
+            return true;
+        }
+        return false;
     }
 }
