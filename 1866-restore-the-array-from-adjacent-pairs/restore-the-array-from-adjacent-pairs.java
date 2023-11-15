545791936a0ac12020c4b68b379a3499902b8c13
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-16 00:44:45 +0530

    Time: 109 ms (42.33%) | Memory: 118.9 MB (7.31%) - LeetSync

--- 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.java
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.java
@@ -1,30 +1,83 @@
+// class Solution {
+//     public int[] restoreArray(int[][] adjacentPairs) {
+//         Map<Integer, List<Integer>> graph = new HashMap<>();
+
+//         for (int[] pair : adjacentPairs) {
+//             graph.computeIfAbsent(pair[0], k -> new ArrayList<>()).add(pair[1]);
+//             graph.computeIfAbsent(pair[1], k -> new ArrayList<>()).add(pair[0]);
+//         }
+
+//         List<Integer> result = new ArrayList<>();
+
+//         for (Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
+//             if (entry.getValue().size() == 1) {
+//                 result.add(entry.getKey());
+//                 result.add(entry.getValue().get(0));
+//                 break;
+//             }
+//         }
+
+//         while (result.size() < adjacentPairs.length + 1) {
+//             int last = result.get(result.size() - 1);
+//             int prev = result.get(result.size() - 2);
+//             List<Integer> candidates = graph.get(last);
+//             int nextElement = candidates.get(0) != prev ? candidates.get(0) : candidates.get(1);
+//             result.add(nextElement);
+//         }
+
+//         return result.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
+
 class Solution {
+    private List<Integer> result = new ArrayList<>();
+
     public int[] restoreArray(int[][] adjacentPairs) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < adjacentPairs.length; i++) {
+            int u = adjacentPairs[i][0];
+            int v = adjacentPairs[i][1];
 
-        for (int[] pair : adjacentPairs) {
-            graph.computeIfAbsent(pair[0], k -> new ArrayList<>()).add(pair[1]);
-            graph.computeIfAbsent(pair[1], k -> new ArrayList<>()).add(pair[0]);
-        }
+            if (map.containsKey(u)) {
+                map.get(u).add(v);
+            } else {
+                map.put(u, new ArrayList<>());
+                map.get(u).add(v);
+            }
 
-        List<Integer> result = new ArrayList<>();
-
-        for (Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
-            if (entry.getValue().size() == 1) {
-                result.add(entry.getKey());
-                result.add(entry.getValue().get(0));
-                break;
+            if (map.containsKey(v)) {
+                map.get(v).add(u);
+            } else {
+                map.put(v, new ArrayList<>());
+                map.get(v).add(u);
             }
         }
 
-        while (result.size() < adjacentPairs.length + 1) {
-            int last = result.get(result.size() - 1);
-            int prev = result.get(result.size() - 2);
-            List<Integer> candidates = graph.get(last);
-            int nextElement = candidates.get(0) != prev ? candidates.get(0) : candidates.get(1);
-            result.add(nextElement);
+        int startPoint = -1;
+
+        for (Integer key : map.keySet()) {
+            List<Integer> value = map.get(key);
+            if (value.size() == 1) {
+                startPoint = key;
+                break;
+            }
+        }
+        solve(startPoint, Integer.MIN_VALUE, map);
+        int[] resultArray = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resultArray[i] = result.get(i);
         }
 
-        return result.stream().mapToInt(Integer::intValue).toArray();        
+        return resultArray;
+    }
+
+    private void solve(int u, int prev, Map<Integer, List<Integer>> map) {
+        result.add(u);
+
+        for (int v : map.get(u)) {
+            if (v != prev) {
+                solve(v, u, map);
+            }
+        }
     }
 }
