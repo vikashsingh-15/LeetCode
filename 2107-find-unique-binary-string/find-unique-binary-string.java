69e68810433845c0e115a3e6dea27b22f18420ee
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-16 12:12:37 +0530

    Time: 12 ms (21.09%) | Memory: 41.5 MB (19.82%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,11 +1,35 @@
+// class Solution {
+
+//     public String findDifferentBinaryString(String[] nums) {
+//         StringBuilder sb = new StringBuilder();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             sb.append(nums[i].charAt(i) == '0' ? "1" : "0");
+//         }
+//         return new String(sb);
+//     }
+// }
+
 class Solution {
 
     public String findDifferentBinaryString(String[] nums) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < nums.length; i++) {
-            sb.append(nums[i].charAt(i) == '0' ? "1" : "0");
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        String binaryString = "";
+        for (String num : nums) {
+            int decimalValue = Integer.parseInt(num, 2);
+            set.add(decimalValue);
         }
-        return new String(sb);
+
+        for (int i = 0; i <= 16; i++) {
+            if (set.contains(i)) {
+                continue;
+            } else {
+                binaryString = String.format("%" + n + "s", Integer.toBinaryString(i)).replace(' ', '0');
+                break;
+            }
+        }
+
+        return binaryString;
     }
 }
