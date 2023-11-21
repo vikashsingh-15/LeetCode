e9ef97742d51698303262beb609204a7158283d8
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-22 01:45:35 +0530

    Updated Solution - GfG to GitHub

--- Easy/Symmetric Tree/symmetric-tree.java
+++ Easy/Symmetric Tree/symmetric-tree.java
@@ -128,17 +128,75 @@ class Node{
 */
 // complete this function
 // return true/false if the is Symmetric or not
+// class GfG
+// {
+//     // return true/false denoting whether the tree is Symmetric or not
+//     public static boolean isSymmetric(Node root)
+//     {
+//          return helper(root, root);
+//     }
+//     public static boolean helper(Node root1, Node root2){
+//         if(root1==null && root2==null)return true;
+//         if(root1==null || root2==null)return false;
+//         if(root1.data != root2.data)return false;
+//         return helper(root1.left, root2.right) && helper(root1.right, root2.left);
+//     }
+// }
+
+
 class GfG
 {
     // return true/false denoting whether the tree is Symmetric or not
     public static boolean isSymmetric(Node root)
     {
-         return helper(root, root);
+         if (root == null) {
+            return true;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            ArrayList<Integer> levelOrder = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node temp = queue.poll();
+
+                if (temp != null) {
+                    if (temp.left != null) {
+                        levelOrder.add(temp.left.data);
+                    }
+                    if (temp.right != null) {
+                        levelOrder.add(temp.right.data);
+                    }
+
+                    queue.add(temp.left);
+                    queue.add(temp.right);
+                }
+            }
+
+            if (levelOrder.size() % 2 != 0) {
+                return false;
+            }
+            if (!isSymetryLevel(levelOrder)) {
+                return false;
+            }
+        }
+        return true;
+         
     }
-    public static boolean helper(Node root1, Node root2){
-        if(root1==null && root2==null)return true;
-        if(root1==null || root2==null)return false;
-        if(root1.data != root2.data)return false;
-        return helper(root1.left, root2.right) && helper(root1.right, root2.left);
+    public static boolean isSymetryLevel(ArrayList<Integer>list){
+         while (list.size() > 0) {
+            int first = list.get(0);
+            int last = list.get(list.size() - 1);
+
+            if (first != last) {
+                return false;
+            }
+            list.remove(0);
+            list.remove(list.size() - 1);
+        }
+
+        return true;
     }
 }
