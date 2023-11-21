7cbf2ded3cdce8d8590a57d586eeb71dbe536310
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-22 01:46:55 +0530

    Update 0101-symmetric-tree.java

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -27,3 +27,66 @@ class Solution {
         return helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
+
+
+
+/*
+class GfG
+{
+    // return true/false denoting whether the tree is Symmetric or not
+    public static boolean isSymmetric(Node root)
+    {
+         if (root == null) {
+            return true;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            ArrayList<Integer> levelOrder = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node temp = queue.poll();
+
+                if (temp != null) {
+                    if (temp.left != null) {
+                        levelOrder.add(temp.left.data);
+                    }
+                    if (temp.right != null) {
+                        levelOrder.add(temp.right.data);
+                    }
+
+                    queue.add(temp.left);
+                    queue.add(temp.right);
+                }
+            }
+
+            if (levelOrder.size() % 2 != 0) {
+                return false;
+            }
+            if (!isSymetryLevel(levelOrder)) {
+                return false;
+            }
+        }
+        return true;
+         
+    }
+    public static boolean isSymetryLevel(ArrayList<Integer>list){
+         while (list.size() > 0) {
+            int first = list.get(0);
+            int last = list.get(list.size() - 1);
+
+            if (first != last) {
+                return false;
+            }
+            list.remove(0);
+            list.remove(list.size() - 1);
+        }
+
+        return true;
+    }
+}
+
+
+*/
