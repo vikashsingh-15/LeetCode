d2a78d89beb1b0c45c01124fdeaeaf5c072b3010
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2023-11-22 21:11:14 +0530

    Time: 32 ms (61.44%) | Memory: 82.2 MB (5.08%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
@@ -1,66 +1,89 @@
 class Solution {
+
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        int count = 0;
-
-        List<List<Integer>> lists = new ArrayList<>();
-        for (int i = 0; i < nums.size(); i++) {
+        Map<Integer, List<Integer>> diagonalMap = new HashMap<>();
+        int totalElement = 0;
+        for (int i = nums.size()-1; i >=0; i--) {
             List<Integer> row = nums.get(i);
-
             for (int j = 0; j < row.size(); j++) {
-                int idx = i + j;
-
-                if (lists.size() < idx + 1) {
-                    lists.add(new ArrayList<>());
-                }
-                lists.get(idx).add(row.get(j));
+                int value = row.get(j);
+                int key = i + j;
+                diagonalMap.putIfAbsent(key, new ArrayList<>());
+                diagonalMap.get(key).add(value);
+                totalElement++;
                 
-                count ++;
             }
         }
 
-        int[] res = new int[count];
-        int idx = 0;
-        for (List<Integer> list : lists) {
-            for (int i = list.size() - 1; i >= 0; i--) {
-                res[idx++] = list.get(i); 
+        int[] ans = new int[totalElement];
+        int i = 0;
+        int curr = 0;
+
+        while (diagonalMap.containsKey(curr)) {
+            for (int num : diagonalMap.get(curr)) {
+                ans[i] = num;
+                i++;
             }
+            curr++;
         }
-        return res;
+
+        return ans;
     }
 }
-
-// import java.util.ArrayList;
-// import java.util.List;
-
 // class Solution {
 //     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-//         if (nums == null || nums.size() == 0) {
-//             return new int[0];
-//         }
-
-//         List<Integer> list = new ArrayList<>();
-
+//         int count = 0;
+//         List<List<Integer>> lists = new ArrayList<>();
 //         for (int i = 0; i < nums.size(); i++) {
-//              int valI = i;
-//             for (int j = 0; j < i + 1; j++) {
-//                     List<Integer> row = nums.get(valI);
-//                     if (j < row.size()) {
-//                         int data = row.get(j);
-//                         list.add(data);
-//                     }
-//                     valI--;
+//             List<Integer> row = nums.get(i);
+//             for (int j = 0; j < row.size(); j++) {
+//                 int idx = i + j;
+//                 if (lists.size() < idx + 1) {
+//                     lists.add(new ArrayList<>());
+//                 }
+//                 lists.get(idx).add(row.get(j));
+//                 count ++;
 //             }
 //         }
-
-
-
-
-
-//         int[] resultArray = new int[list.size()];
-//         for (int i = 0; i < list.size(); i++) {
-//             resultArray[i] = list.get(i);
+//         int[] res = new int[count];
+//         int idx = 0;
+//         for (List<Integer> list : lists) {
+//             for (int i = list.size() - 1; i >= 0; i--) {
+//                 res[idx++] = list.get(i);
+//             }
 //         }
-
-//         return resultArray;
+//         return res;
+//     }
+// }
+////fail at test case 53/54
+// class Solution {
+//     public int[] findDiagonalOrder(List<List<Integer>> nums) {
+//         Map<Integer, List<Integer>> diagonalMap = new HashMap<>();
+//         int totalElement = 0;
+//         for (int i = 0; i < nums.size(); i++) {
+//             List<Integer> row = nums.get(i);
+//             // totalElement += row.size();
+//             for (int j = 0; j < row.size(); j++) {
+//                 int value = row.get(j);
+//                 int key = i + j;
+//                 diagonalMap.putIfAbsent(key, new ArrayList<>());
+//                 diagonalMap.get(key).add(value);
+//                 totalElement++;
+//                 // if (map.containsKey(key)) {
+//                 //     map.put(key, new ArrayList<>());
+//                 // } else {
+//                 //     map.get(key).add(value);
+//                 // }
+//             }
+//         }
+//         int[] result = new int[totalElement];
+//         int index = 0;
+//         for (int key : diagonalMap.keySet()) {
+//         List<Integer> diagonalElements = diagonalMap.get(key);
+//              for (int i = diagonalElements.size() - 1; i >= 0; i--) {
+//                 result[index++] = diagonalElements.get(i);
+//             }
+//         }
+//         return result;
 //     }
 // }
