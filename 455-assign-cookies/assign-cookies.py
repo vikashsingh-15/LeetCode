9bdab4cf9784605160587a4520a534239ceee25e
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-01-01 20:42:39 +0530

    Time: 291 ms (9.28%) | Memory: 19.1 MB (13.41%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -1,18 +1,39 @@
+# class Solution:
+#     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+#         g.sort();
+#         s.sort();
+#         content_children = 0;
+
+#         i,j=0,0;
+
+#         while i<len(g) and j<len(s):
+#             if(s[j]>=g[i]):
+#                 content_children+=1;
+#                 i+=1;
+#                 j+=1;
+
+#             else:
+#                 j+=1;
+#         return content_children;
+        
+
+# Way 2
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        g.sort();
-        s.sort();
-        content_children = 0;
+       
+        g.sort()
+        s.sort()
 
-        i,j=0,0;
+        content_children = 0  
+        i, j = 0, 0
 
-        while i<len(g) and j<len(s):
-            if(s[j]>=g[i]):
-                content_children+=1;
-                i+=1;
-                j+=1;
-
-            else:
-                j+=1;
-        return content_children;
         
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                content_children += 1 
+                i += 1 
+                
+            j += 1 
+
+        return content_children
+
