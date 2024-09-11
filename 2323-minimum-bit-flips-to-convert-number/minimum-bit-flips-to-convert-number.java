5c10282d933ed0637559f30777fb753cc037d00e
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-09-12 00:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (79.96%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,16 +1,36 @@
+// class Solution {
+//     public int minBitFlips(int start, int goal) {
+//         int xorValue = start ^ goal;
+        
+//         // Convert the XOR value to a binary string
+//         String binaryDiff = Integer.toBinaryString(xorValue);
+        
+//         // Count the number of '1's in the binary string
+//         int countOfFlips = 0;
+//         for (char bit : binaryDiff.toCharArray()) {
+//             if (bit == '1') {
+//                 countOfFlips++;
+//             }
+//         }
+        
+//         return countOfFlips;
+//     }
+// }
+
+//way 2
+
 class Solution {
     public int minBitFlips(int start, int goal) {
+        // Calculate the XOR value to find differing bits
         int xorValue = start ^ goal;
         
-        // Convert the XOR value to a binary string
-        String binaryDiff = Integer.toBinaryString(xorValue);
-        
-        // Count the number of '1's in the binary string
+        // Count the number of 1's in the binary representation of xorValue
         int countOfFlips = 0;
-        for (char bit : binaryDiff.toCharArray()) {
-            if (bit == '1') {
-                countOfFlips++;
-            }
+        while (xorValue > 0) {
+            // Increment the count if the least significant bit is 1
+            countOfFlips += (xorValue & 1);
+            // Right shift xorValue to check the next bit
+            xorValue >>= 1;
         }
         
         return countOfFlips;
