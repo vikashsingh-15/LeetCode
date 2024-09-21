36634a652adde7ff2de0e724c7b885b02994c2f8
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-09-21 23:12:38 +0530

    Time: 1 ms (100.00%) | Memory: 48.5 MB (37.78%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,21 +1,41 @@
+// class Solution {
+//     public List<Integer> lexicalOrder(int n) {
+//         List<Integer> list = new ArrayList<>();
+//         int curr = 1;
+//         for (int i = 1; i <= n; i++) {
+//             list.add(curr);
+//             if (curr * 10 <= n) {
+//                 curr = curr * 10;
+//             } else {
+//                 if (curr >= n) {
+//                     curr = curr / 10;
+//                 }
+//                 curr++;
+//                 while (curr % 10 == 0)
+//                     curr /= 10;
+//             }
+//         }
+//         return list;
+
+//     }
+// }
+
+
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> list = new ArrayList<>();
-        int curr = 1;
-        for (int i = 1; i <= n; i++) {
-            list.add(curr);
-            if (curr * 10 <= n) {
-                curr = curr * 10;
-            } else {
-                if (curr >= n) {
-                    curr = curr / 10;
-                }
-                curr++;
-                while (curr % 10 == 0)
-                    curr /= 10;
-            }
+    List<Integer> result = new ArrayList<>();
+       for (int i = 1; i <= 9; i++) {
+            dfs(i, n, result);
         }
-        return list;
-
+        return result;
+    }
+     private void dfs(int curr, int n, List<Integer> result) {
+        if (curr > n) return;  
+        result.add(curr); 
+        for (int i = 0; i <= 9; i++) {
+            int next = curr * 10 + i;
+            if (next > n) break;  
+            dfs(next, n, result); 
+        }
     }
 }
