164f23c12ae0d6f1c593f390ee5c430fe722b885
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-10-05 21:37:19 +0530

    Time: 5 ms (96.14%) | Memory: 42.7 MB (53.57%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,59 @@
+class Solution {
+
+    public boolean checkInclusion(String s1, String s2) {
+        // int n = s1.length();
+        // int m = s2.length();
+        // if(n>m) return false;
+        // int[] arr = new int[26];
+        // for(int i=0;i<n;i++){
+        //     arr[s1.charAt(i)-'a']++;
+        //  }
+        //  for(int i = 0;i<m;i++){
+        //     arr[s2.charAt(i)-'a']--;
+        //     if(i-n >= 0){
+        //         arr[s2.charAt(i-n)-'a']++;
+        //     }
+        //     if(zero(arr)) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //way 2
+        
+        int n1 = s1.length(), n2 = s2.length();
+        if (n1 > n2) {
+            return false;
+        }
+        
+        int[] freq = new int[26];
+        for (int i = 0; i < n1; i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        
+        int left = 0, right = 0;
+        while (right < n2) {
+            char ch = s2.charAt(right);
+            freq[ch - 'a']--;
+            while (freq[ch - 'a'] < 0) {
+                freq[s2.charAt(left) - 'a']++;
+                left++;
+            }
+            if (right - left + 1 == n1) {
+                return true;
+            }
+            right++;
+        }
+        return false;
+
+    }
+
+    // public boolean zero(int[] arr) {
+    //     for (int i = 0; i < 26; i++) {
+    //         if (arr[i] != 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+}
