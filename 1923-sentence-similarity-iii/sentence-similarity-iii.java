9249960b7d89efe92b30ec929fb8008f146d64ba
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-10-07 00:26:40 +0530

    Time: 1 ms (96.43%) | Memory: 41.1 MB (98.41%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -1,16 +1,44 @@
+// class Solution {
+//     public boolean areSentencesSimilar(String s1, String s2) {
+//         Deque<String> deque1=new ArrayDeque<>(Arrays.asList(s1.split(" ")));
+//         Deque<String> deque2=new ArrayDeque<>(Arrays.asList(s2.split(" ")));
+//         while(!deque1.isEmpty()&&!deque2.isEmpty()&&deque1.peek().equals(deque2.peek())){
+// deque1.poll();
+// deque2.poll();
+//         }
+
+//         while(!deque1.isEmpty() && !deque2.isEmpty()&&deque1.peekLast().equals(deque2.peekLast())){
+//             deque1.pollLast();
+//             deque2.pollLast();
+//         }
+// return deque1.isEmpty() || deque2.isEmpty();
+//     }
+// }
+
 class Solution {
     public boolean areSentencesSimilar(String s1, String s2) {
-        Deque<String> deque1=new ArrayDeque<>(Arrays.asList(s1.split(" ")));
-        Deque<String> deque2=new ArrayDeque<>(Arrays.asList(s2.split(" ")));
-        while(!deque1.isEmpty()&&!deque2.isEmpty()&&deque1.peek().equals(deque2.peek())){
-deque1.poll();
-deque2.poll();
+        if(s1.length()>s2.length()){
+            return areSentencesSimilar(s2,s1);
         }
 
-        while(!deque1.isEmpty() && !deque2.isEmpty()&&deque1.peekLast().equals(deque2.peekLast())){
-            deque1.pollLast();
-            deque2.pollLast();
+        String[] arr1=s1.split(" ");
+        String[] arr2=s2.split(" ");
+        int start=0;
+        int start1=0;
+        int start2=0;
+        int end1=arr1.length-1;
+        int end2=arr2.length-1;
+        int count1=arr1.length-1;
+        int count2=arr2.length-1;
+
+        while(start<=count1 && arr1[start].equals(arr2[start])){
+            start++;
         }
-return deque1.isEmpty() || deque2.isEmpty();
+
+        while(end1>=start && arr1[end1].equals(arr2[end2])){
+            end1--;
+            end2--;
+        }
+        return end1<start;
     }
 }
