d953e55c7204f22b4b06886159d9c483eb2c88bb
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-12-16 12:23:05 +0530

    Time: 3 ms (33.80%) | Memory: 44.5 MB (89.91%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,15 +1,42 @@
+// class Solution {
+//     public int[] getFinalState(int[] nums, int k, int multiplier) {
+
+//         for(int j=0;j<k;j++){
+//             int minIdx=0;
+//             for(int i=0;i<nums.length;i++){
+//                 if(nums[i]<nums[minIdx]){
+//                     minIdx=i;
+//                 }
+//             }
+//             nums[minIdx]*=multiplier;
+//         }
+//         return nums;
+//     }
+// }
+
+////way 2
+
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-
-        for(int j=0;j<k;j++){
-            int minIdx=0;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]<nums[minIdx]){
-                    minIdx=i;
-                }
+        PriorityQueue<int []> map=new PriorityQueue<>((a,b)->{
+            int value=Integer.compare(a[0],b[0]);
+            if(value==0){
+                return Integer.compare(a[1],b[1]);
             }
-            nums[minIdx]*=multiplier;
+            return value;
+        });
+        for(int i=0;i<nums.length;i++){
+            map.offer(new int[] {nums[i],i});
         }
+
+        while(k-->0){
+           int[] x=map.poll();
+            int index=x[1];
+            nums[index]*=multiplier;
+            map.offer(new int[]{nums[index],index});
+        }
+
         return nums;
     }
 }
+
