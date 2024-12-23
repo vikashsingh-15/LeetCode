801d446c2b38cecea2aac5c5cd3af41014e7aa4a
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-12-23 16:01:28 +0530

    Time: 51 ms (72.60%) | Memory: 59.3 MB (96.58%) - LeetSync

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -0,0 +1,88 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+//// This is wrong thought and process
+// class Solution {
+// public int minimumOperations(TreeNode root) {
+// Queue<TreeNode>queue=new LinkedList<>();
+// queue.add(root);
+// int totalSwap=0;
+// while(!queue.isEmpty()){
+// int levelSize=queue.size();
+// int levelValue[]=new int[levelSize];
+// for(int i=0;i<levelSize;i++){
+// TreeNode node=queue.poll();
+// levelValue[i]=node.val;
+// if(node.left!=null) queue.add(node.left);
+// if(node.right!=null) queue.add(node.right);
+// }
+// int [] tempArr=levelValue.clone();
+// Arrays.sort(tempArr);
+// int count=0;
+
+// for(int i=0;i<tempArr.length;i++){
+// if(tempArr[i]!=levelValue[i]){
+// count++;
+// }
+// }
+// totalSwap+=(int) Math.round(count / 2.0);
+// }
+// return totalSwap;
+// }
+// }
+
+class Solution {
+    public int minimumOperations(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int totalSwap = 0;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            int levelValue[] = new int[levelSize];
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                levelValue[i] = node.val;
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            totalSwap += getMinSwaps(levelValue);
+        }
+        return totalSwap;
+    }
+
+    private int getMinSwaps(int[] arr) {
+        int swap = 0;
+        int[] target = arr.clone();
+        Arrays.sort(target);
+
+        Map<Integer, Integer> pos = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            pos.put(arr[i], i);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != target[i]) {
+                swap++;
+                int curPos = pos.get(target[i]);
+                pos.put(arr[i], curPos);
+                arr[curPos] = arr[i];
+            }
+        }
+        return swap;
+    }
+}
