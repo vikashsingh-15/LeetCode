7ec1efd9b2c0f34d20116dd9853765fab54087ff
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-12-26 11:14:14 +0530

    Time: 639 ms (15.09%) | Memory: 41.2 MB (80.82%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,22 @@
-class Solution {
-    int totalWays=0;
+public class Solution {
+    int totalWays = 0;
+
     public int findTargetSumWays(int[] nums, int target) {
-        helper(nums,0,0,target);
-        return totalWays;        
+        helper(nums, 0, 0, target);
+        return totalWays;
     }
+
     private void helper(int[] nums, int index, int sum, int target) {
-    if (index == nums.length) { // Base case: processed all elements
-        if (sum == target) {
-            totalWays++;
+        if (index == nums.length) {
+            if (sum == target) {
+                totalWays++;
+            }
+            return;
+
+        } else {
+            helper(nums, index + 1, sum + nums[index], target);
+            helper(nums, index + 1, sum - nums[index], target);
         }
-        return; // Stop recursion here
     }
-    // Recursive calls for adding and subtracting the current number
-    helper(nums, index + 1, sum + nums[index], target);
-    helper(nums, index + 1, sum - nums[index], target);
-}
+
 }
