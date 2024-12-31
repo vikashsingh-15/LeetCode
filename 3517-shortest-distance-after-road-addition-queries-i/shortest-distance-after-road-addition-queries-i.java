7409a590d3df8a2343e371c8d7632f4034bb529f
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2024-12-31 16:40:51 +0530

    Time: 58 ms (64.98%) | Memory: 45.6 MB (83.05%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -34,13 +34,52 @@
 //     }
 // }
 
+////correct way 2
+
+// class Solution {
+//     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+
+//         Map<Integer, List<Integer>> map = new HashMap<>();
+//         int[] dp = new int[n];
+
+//         dp[n - 1] = 0;
+//         for (int i = n - 2; i >= 0; i--) {
+//             dp[i] = 1 + dp[i + 1];
+//         }
+
+//         int[] result = new int[queries.length];
+
+//         for (int q = 0; q < queries.length; q++) {
+//             int u = queries[q][0], v = queries[q][1];
+//             dp[u] = Math.min(dp[u], 1 + dp[v]);
+
+//             map.putIfAbsent(u, new ArrayList<>());
+//             map.get(u).add(v);
+
+//             for (int i = u; i >= 0; i--) {
+//                 dp[i] = Math.min(dp[i], 1 + dp[i + 1]);
+//                 if (map.containsKey(i)) {
+//                     for (int j = 0; j < map.get(i).size(); j++) {
+//                         int end = map.get(i).get(j);
+//                         dp[i] = Math.min(dp[i], 1 + dp[end]);
+//                     }
+//                 }
+//             }
+//             result[q] = dp[0];
+//         }
+
+//         return result;
+//     }
+// }
+
+////way 3
+
 class Solution {
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-
         Map<Integer, List<Integer>> map = new HashMap<>();
         int[] dp = new int[n];
 
-        dp[n - 1] = 0;
+        dp[n - 1] = 0; 
         for (int i = n - 2; i >= 0; i--) {
             dp[i] = 1 + dp[i + 1];
         }
@@ -49,16 +88,16 @@ class Solution {
 
         for (int q = 0; q < queries.length; q++) {
             int u = queries[q][0], v = queries[q][1];
-            dp[u] = Math.min(dp[u], 1 + dp[v]);
-
             map.putIfAbsent(u, new ArrayList<>());
             map.get(u).add(v);
 
+            dp[u] = Math.min(dp[u], 1 + dp[v]); 
+
             for (int i = u; i >= 0; i--) {
                 dp[i] = Math.min(dp[i], 1 + dp[i + 1]);
+
                 if (map.containsKey(i)) {
-                    for (int j = 0; j < map.get(i).size(); j++) {
-                        int end = map.get(i).get(j);
+                    for (int end : map.get(i)) {
                         dp[i] = Math.min(dp[i], 1 + dp[end]);
                     }
                 }
