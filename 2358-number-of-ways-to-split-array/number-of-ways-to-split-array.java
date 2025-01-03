e8f5de5466274caa50856cbb1114b602f9fcef62
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-01-03 12:05:22 +0530

    Time: 2 ms (100.00%) | Memory: 58.4 MB (60.21%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,25 +1,50 @@
+// class Solution {
+//     public int waysToSplitArray(int[] nums) {
+//         long sum=0;
+//         long  preArr[]=new long [nums.length];
+//         preArr[0]=nums[0];
+//         sum=nums[0];
+//      for(int i=1;i<nums.length;i++){
+//         sum+=nums[i];
+//         preArr[i]=nums[i]+preArr[i-1];
+
+//      }  
+
+//      int count=0;
+
+//      for(int i=0;i<nums.length-1;i++){
+//         long rightSum=sum-preArr[i];
+//         if(preArr[i]>=rightSum){
+//             count++;
+//         }
+
+//      } 
+//     return count;
+
+//     }
+// }
+
+//w ay 2
+
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long sum=0;
-        long  preArr[]=new long [nums.length];
-        preArr[0]=nums[0];
-        sum=nums[0];
-     for(int i=1;i<nums.length;i++){
-        sum+=nums[i];
-        preArr[i]=nums[i]+preArr[i-1];
-
-     }  
-
-     int count=0;
-
-     for(int i=0;i<nums.length-1;i++){
-        long rightSum=sum-preArr[i];
-        if(preArr[i]>=rightSum){
-            count++;
+        long sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
         }
-        
-     } 
-    return count;
+        long leftSum=0;
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            leftSum+=nums[i];
+            long rightSum = sum - leftSum;
+            if (leftSum >= rightSum) {
+                count++;
+            }
+
+        }
+        return count;
 
     }
 }
