ee78a4e43240c75c2228287c473891af99881aa3
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-01-05 22:28:06 +0530

    Time: 7 ms (74.53%) | Memory: 74.4 MB (56.13%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.java
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -26,31 +26,27 @@
 class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
         char[] result = s.toCharArray();
-        int diffArr[]=new int[s.length()];
+        int[] diffArr = new int[s.length()];
 
-        for (int j = 0; j < shifts.length; j++) {
-            int start = shifts[j][0];
-            int end = shifts[j][1];
-            int direction = shifts[j][2];
-            if(direction==1){
+        for (int[] shift : shifts) {
+            int start = shift[0], end = shift[1], direction = shift[2];
+            if (direction == 1) { // Backward shift
                 diffArr[start]++;
-                if(end+1<s.length()){
-                    diffArr[end+1]--;
+                if (end + 1 < s.length()) {
+                    diffArr[end + 1]--;
                 }
-            }else{
+            } else { // Forward shift
                 diffArr[start]--;
-                 if(end+1<s.length()){
-                    diffArr[end+1]++;
+                if (end + 1 < s.length()) {
+                    diffArr[end + 1]++;
                 }
             }
-
         }
 
-           int numberOfShifts = 0;
+        int numberOfShifts = 0;
         for (int i = 0; i < s.length(); i++) {
-            numberOfShifts = (numberOfShifts + diffArr[i]) % 26; 
-            if (numberOfShifts < 0) numberOfShifts += 26;
-            result[i] = (char) ('a' + ((result[i] - 'a' + numberOfShifts) % 26));
+            numberOfShifts = (numberOfShifts + diffArr[i]) % 26;
+            result[i] = (char) ('a' + (result[i] - 'a' + numberOfShifts + 26) % 26); 
         }
 
         return new String(result); 
