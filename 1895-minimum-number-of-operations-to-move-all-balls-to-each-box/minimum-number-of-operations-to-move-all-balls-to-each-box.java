2da7c6f1bcaebb1e6a0e32b1e3aff8bdcf0868ab
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-01-06 11:49:30 +0530

    Time: 4 ms (76.56%) | Memory: 44.6 MB (77.78%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,19 +1,47 @@
+// class Solution {
+//     public int[] minOperations(String str) {
+//         int[] ans = new int[str.length()];
+
+//         for(int i=0;i<str.length();i++){
+//             int count=0;
+//             for(int j=0;j<str.length();j++){
+//                 if(i==j){
+
+//                 }if(str.charAt(j)=='1'){
+//                     count+=Math.abs(j-i);
+//                 }
+
+//             }
+//             ans[i]=count;
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+//way 2
 class Solution {
     public int[] minOperations(String str) {
-        int[] ans = new int[str.length()];
-
-        for(int i=0;i<str.length();i++){
-            int count=0;
-            for(int j=0;j<str.length();j++){
-                if(i==j){
-
-                }if(str.charAt(j)=='1'){
-                    count+=Math.abs(j-i);
-                }
-
-            }
-            ans[i]=count;
+         int n = str.length();
+        int[] answer = new int[n];
+        int count = 0;
+        int operation=0;
+        for(int i=0;i<n;i++){
+            answer[i] += operation;
+            count+=str.charAt(i)=='1'?1:0;
+            operation+=count;
         }
-        return ans;
+       
+
+         count = 0;
+         operation=0;
+        for(int i=n-1;i>=0;i--){
+            answer[i] += operation;
+            count+=str.charAt(i)=='1'?1:0;
+            operation+=count;
+        }
+
+        return answer;
     }
 }
