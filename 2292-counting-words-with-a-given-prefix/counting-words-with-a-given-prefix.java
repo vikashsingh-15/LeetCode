6c330e12b162285ba6b390cc188f1536e6beaac8
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-01-09 16:40:52 +0530

    Time: 4 ms (10.57%) | Memory: 45.5 MB (5.10%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,12 +1,68 @@
-class Solution {
+// class Solution {
 
+//     public int prefixCount(String[] words, String pref) {
+//         int count = 0;
+//         for (String word : words) {
+//             if (word.startsWith(pref)) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+class Solution {
     public int prefixCount(String[] words, String pref) {
         int count = 0;
+        Trie trie = new Trie();
         for (String word : words) {
-            if (word.startsWith(pref)) {
-                count++;
+            trie.addWord(word);
+        }
+        return trie.countPrefix(pref);
+    }
+
+    public class Trie {
+        class Node {
+            Node[] links;
+            int count;
+
+            Node() {
+                links = new Node[26];
+                count = 0;
             }
         }
-        return count;
+
+        final private Node root;
+
+        public Trie() {
+            root = new Node();
+        }
+
+        public void addWord(String word) {
+            Node curr = root;
+
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+
+                if (curr.links[ch - 'a'] == null) {
+                    curr.links[ch - 'a'] = new Node();
+                }
+                curr = curr.links[ch - 'a'];
+                curr.count++;
+            }
+        }
+
+        public int countPrefix(String pref) {
+            Node curr = root;
+            for (int i = 0; i < pref.length(); i++) {
+                char c = pref.charAt(i);
+                if (curr.links[c - 'a'] == null) {
+                    return 0; // Prefix not found
+                }
+                curr = curr.links[c - 'a'];
+            }
+            return curr.count; // Return count at last node
+        }
+
     }
 }
