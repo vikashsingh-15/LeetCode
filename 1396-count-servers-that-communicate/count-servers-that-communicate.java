f19bac86c2457177d2d89c150562cd01f86ede33
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-01-23 17:55:14 +0530

    Time: 6 ms (20.54%) | Memory: 51.7 MB (85.80%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -1,36 +1,71 @@
+// class Solution {
+//     public int countServers(int[][] grid) {
+//         int n=grid.length;
+//         int m=grid[0].length;
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(grid[i][j]==1&& (checkRow(i,grid) || checkCol(j,grid))){
+//                     count++;
+//                 }
+//             }
+
+//         } 
+//         return count;
+//     }
+
+//     public static boolean  checkRow(int row,int [][]grid){
+//        int serverCount = 0;
+//         for (int j = 0; j < grid[0].length; j++) {
+//             if (grid[row][j] == 1) {
+//                 serverCount++;
+//             }
+//         }
+//         return serverCount > 1;
+//     }
+
+//      public static boolean  checkCol(int col,int [][]grid){
+//         int serverCount = 0;
+//         for (int i = 0; i < grid.length; i++) {
+//             if (grid[i][col] == 1) {
+//                 serverCount++;
+//             }
+//         }
+//         return serverCount > 1;
+//     }
+// }
+
+
+//way 2
+
 class Solution {
     public int countServers(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
         int count=0;
+
+
+        Map <Integer,Integer>mapx=new HashMap<>();
+        Map <Integer,Integer>mapy=new HashMap<>();
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1&& (checkRow(i,grid) || checkCol(j,grid))){
+                if(grid[i][j]==1){
+                    mapx.put(i,mapx.getOrDefault(i,0)+1);
+                    mapy.put(j,mapy.getOrDefault(j,0)+1);
+                }
+            }
+
+        } 
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && (mapx.get(i)>1 ||mapy.get(j)>1  )){
                     count++;
                 }
             }
 
         } 
         return count;
-    }
-
-    public static boolean  checkRow(int row,int [][]grid){
-       int serverCount = 0;
-        for (int j = 0; j < grid[0].length; j++) {
-            if (grid[row][j] == 1) {
-                serverCount++;
-            }
-        }
-        return serverCount > 1;
-    }
-
-     public static boolean  checkCol(int col,int [][]grid){
-        int serverCount = 0;
-        for (int i = 0; i < grid.length; i++) {
-            if (grid[i][col] == 1) {
-                serverCount++;
-            }
-        }
-        return serverCount > 1;
     }
 }
