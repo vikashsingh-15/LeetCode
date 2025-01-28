485bc4664fe78870453d5bb74fff93ba00697e05
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-01-28 14:19:53 +0530

    Time: 5 ms (65.83%) | Memory: 44.9 MB (86.55%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int findMaxFish(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int maxFishCount = 0;
+
+
+        boolean visited[][]=new boolean[n][m];
+
+         for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (grid[row][col] > 0 && !visited[row][col]) {
+                    maxFishCount = Math.max(
+                        maxFishCount,
+                        dfs(grid, row, col,visited)
+                    );
+                }
+            }
+        }
+
+        return maxFishCount;
+
+        
+    }
+
+    public static int  dfs(int[][] grid,int row,int col,boolean[][] visited){
+
+        if(row<0 || row>=grid.length||col<0 || col>=grid[0].length|| grid[row][col]==0 || visited[row][col]){
+            return 0;
+        }
+
+        visited[row][col]=true;
+
+        int top=dfs(grid,row+1,col,visited);
+        int down=dfs(grid,row-1,col,visited);
+        int left=dfs(grid,row,col-1,visited);
+        int right=dfs(grid,row,col+1,visited);
+
+        return grid[row][col]+top+down+left+right;
+
+
+
+
+
+
+
+    }
+}
