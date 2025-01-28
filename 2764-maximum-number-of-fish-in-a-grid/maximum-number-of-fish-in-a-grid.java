ea567b0250cf07d58df2e95614fa0eccec9b64f2
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-01-28 14:24:17 +0530

    Time: 5 ms (65.83%) | Memory: 45.4 MB (13.73%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -1,3 +1,92 @@
+// class Solution {
+//     public int findMaxFish(int[][] grid) {
+//         int n=grid.length;
+//         int m=grid[0].length;
+//         int maxFishCount = 0;
+
+
+//         boolean visited[][]=new boolean[n][m];
+
+//          for (int row = 0; row < n; row++) {
+//             for (int col = 0; col < m; col++) {
+//                 if (grid[row][col] > 0 && !visited[row][col]) {
+//                     maxFishCount = Math.max(
+//                         maxFishCount,
+//                         dfs(grid, row, col,visited)
+//                     );
+//                 }
+//             }
+//         }
+
+//         return maxFishCount;
+
+        
+//     }
+
+//     public static int  dfs(int[][] grid,int row,int col,boolean[][] visited){
+
+//         if(row<0 || row>=grid.length||col<0 || col>=grid[0].length|| grid[row][col]==0 || visited[row][col]){
+//             return 0;
+//         }
+
+//         visited[row][col]=true;
+
+//         int top=dfs(grid,row+1,col,visited);
+//         int down=dfs(grid,row-1,col,visited);
+//         int left=dfs(grid,row,col-1,visited);
+//         int right=dfs(grid,row,col+1,visited);
+
+//         return grid[row][col]+top+down+left+right;
+//     }
+// }
+
+////WAY 2
+
+// class Solution {
+//     public int findMaxFish(int[][] grid) {
+//         int n=grid.length;
+//         int m=grid[0].length;
+//         int maxFishCount = 0;
+
+
+//         boolean visited[][]=new boolean[n][m];
+
+//          for (int row = 0; row < n; row++) {
+//             for (int col = 0; col < m; col++) {
+//                 if (grid[row][col] > 0 ) {
+//                     maxFishCount = Math.max(
+//                         maxFishCount,
+//                         dfs(grid, row, col,visited)
+//                     );
+//                 }
+//             }
+//         }
+
+//         return maxFishCount;
+
+        
+//     }
+
+//     public static int  dfs(int[][] grid,int row,int col,boolean[][] visited){
+
+//         if(row<0 || row>=grid.length||col<0 || col>=grid[0].length|| grid[row][col]==0 || visited[row][col]){
+//             return 0;
+//         }
+
+//         visited[row][col]=true;
+
+//         int top=dfs(grid,row+1,col,visited);
+//         int down=dfs(grid,row-1,col,visited);
+//         int left=dfs(grid,row,col-1,visited);
+//         int right=dfs(grid,row,col+1,visited);
+
+//         return grid[row][col]+top+down+left+right;
+//     }
+// }
+
+////way 3
+
+
 class Solution {
     public int findMaxFish(int[][] grid) {
         int n=grid.length;
@@ -10,10 +99,8 @@ class Solution {
          for (int row = 0; row < n; row++) {
             for (int col = 0; col < m; col++) {
                 if (grid[row][col] > 0 ) {
-                    maxFishCount = Math.max(
-                        maxFishCount,
-                        dfs(grid, row, col,visited)
-                    );
+                   grid[row][col]=dfs(grid, row, col,visited);
+                   maxFishCount=Math.max(maxFishCount,grid[row][col]);
                 }
             }
         }
@@ -37,12 +124,5 @@ class Solution {
         int right=dfs(grid,row,col+1,visited);
 
         return grid[row][col]+top+down+left+right;
-
-
-
-
-
-
-
     }
 }
