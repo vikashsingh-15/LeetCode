8c531f63ba8fb7c3912544219fcee05786ea5844
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-02 18:32:57 +0530

    Time: 11 ms (57.14%) | Memory: 45.6 MB (48.45%) - LeetSync

---
+++ 826-soup-servings/soup-servings.java
@@ -0,0 +1,66 @@
+// class Solution {
+//   private Map<Pair, Double> mp = new HashMap<>();
+
+//     private double recur(int a, int b) {
+//         if (a <= 0 && b <= 0) return 0.5;
+//         if (a <= 0 && b > 0) return 1;
+//         if (a > 0 && b <= 0) return 0;
+//         if (mp.containsKey(new Pair(a, b))) return mp.get(new Pair(a, b));
+
+//         double op1 = recur(a - 100, b);
+//         double op2 = recur(a - 75, b - 25);
+//         double op3 = recur(a - 50, b - 50);
+//         double op4 = recur(a - 25, b - 75);
+
+//         double result = (op1 + op2 + op3 + op4)/4.0;
+//         mp.put(new Pair(a, b), result);
+//         return result;
+//     }
+
+//     public double soupServings(int n) {
+//         if (n >= 4800) return 1;
+//         double ans = recur(n, n);
+//         return ans;
+//     }
+// }
+
+//way 2
+
+class Solution {
+    private Map<Pair, Double> mp = new HashMap<>();
+
+    private double recur(int a, int b) {
+        
+        if(a<=0 && b<=0){
+            return 0.5;
+        }
+       
+        if(a<=0){
+            return 1;
+        }
+
+        if(b<=0){
+            return 0;
+        }
+        
+        if (mp.containsKey(new Pair(a, b))) return mp.get(new Pair(a, b));
+
+        double op1 = recur(a - 100, b);
+        double op2 = recur(a - 75, b - 25);
+        double op3 = recur(a - 50, b - 50);
+        double op4 = recur(a - 25, b - 75);
+
+        double result = (op1 + op2 + op3 + op4)/4.0;
+        mp.put(new Pair(a, b), result);
+        return result;
+       
+    }
+
+    public double soupServings(int n) {
+
+
+        if (n >= 4800) return 1;
+        double ans = recur(n, n);
+        return ans;
+    }
+}
