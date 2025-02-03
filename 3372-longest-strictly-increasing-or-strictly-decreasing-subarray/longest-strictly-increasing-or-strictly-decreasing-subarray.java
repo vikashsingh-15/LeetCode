ac874fedd6e354e45bf1fa5ec1d20a65161b0f98
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-03 11:51:43 +0530

    Time: 1 ms (93.94%) | Memory: 42.7 MB (73.06%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,26 +1,56 @@
+// class Solution {
+//     public int longestMonotonicSubarray(int[] nums) {
+//         int ans=0;
+//         int increasing=0;
+//         int decreasing =0;
+
+
+//         for(int i=0;i<nums.length-1;i++){
+//             if(nums[i]<nums[i+1]){
+//                increasing+=1;
+//                decreasing=0;
+//             }
+//             else if(nums[i]>nums[i+1]){
+//                  decreasing+=1;
+//                increasing=0;
+//             }
+//             else{
+//                 decreasing=0;
+//                 increasing=0;
+//             }
+
+//            ans=Math.max(ans,Math.max(increasing,decreasing));
+//         }
+//         return ans+1;
+//     }
+// }
+
+
+////way 2
+
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
-        int ans=0;
-        int increasing=0;
-        int decreasing =0;
+        int ans=1;
+        int increasing=1;
+        int decreasing =1;
 
 
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]<nums[i+1]){
                increasing+=1;
-               decreasing=0;
+               decreasing=1;
             }
             else if(nums[i]>nums[i+1]){
                  decreasing+=1;
-               increasing=0;
+               increasing=1;
             }
             else{
-                decreasing=0;
-                increasing=0;
+                decreasing=1;
+                increasing=1;
             }
 
            ans=Math.max(ans,Math.max(increasing,decreasing));
         }
-        return ans+1;
+        return ans;
     }
 }
