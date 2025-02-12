f5683db4f8ddd49778d02b6d542da9c0a4ff54ec
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-12 19:33:33 +0530

    Time: 42 ms (93.13%) | Memory: 112.9 MB (75.07%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,30 @@
+import java.util.LinkedHashMap;
+
+class LRUCache {
+    private int max; 
+    private LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache(int capacity) {
+        this.max = capacity;
+        map = new LinkedHashMap<>(capacity,1, true); 
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        return map.get(key);
+    }
+
+    public void put(int key, int value) {
+     
+        map.put(key, value);
+
+        
+        if (map.size() > max) {
+            // Remove the first key (least recently used)
+            Integer lruKey = map.keySet().iterator().next();
+            map.remove(lruKey);
+        }
+    }
+}
