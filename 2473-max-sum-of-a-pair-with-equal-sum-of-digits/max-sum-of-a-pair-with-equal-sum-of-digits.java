6faf6167246d1b054ffe33afc70f0b4436324789
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-12 14:47:33 +0530

    Time: 55 ms (33.04%) | Memory: 56.6 MB (71.35%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,34 +1,77 @@
+// class Solution {
+//     public int maximumSum(int[] nums) {
+//         Map<Integer,List<Integer>>map=new HashMap<>();
+
+//         for(int i=0;i<nums.length;i++){
+//             int digitSum=sumOfDigit(nums[i]);
+//             map.putIfAbsent(digitSum,new ArrayList<>());
+//             map.get(digitSum).add(nums[i]);
+//         }
+
+//         int maxSum = -1;
+
+//         for( Integer key:map.keySet()){
+//             List<Integer> list=map.get(key);
+//             if(list.size()>1){
+//                 Collections.sort(list, Collections.reverseOrder());
+//                 int currentSum = list.get(0) + list.get(1);
+//                 maxSum = Math.max(maxSum, currentSum);
+//             }
+//         }
+
+//         return maxSum;
+        
+//     }
+
+//     public static int sumOfDigit(int x){
+//         int sum=0;
+//         while(x!=0){
+//             sum+=x%10;
+//             x=x/10;
+//         }
+//         return sum;
+//     }
+// }
+
+////way 2
+
+import java.util.*;
+
 class Solution {
     public int maximumSum(int[] nums) {
-        Map<Integer,List<Integer>>map=new HashMap<>();
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
 
-        for(int i=0;i<nums.length;i++){
-            int digitSum=sumOfDigit(nums[i]);
-            map.putIfAbsent(digitSum,new ArrayList<>());
-            map.get(digitSum).add(nums[i]);
+        for (int num : nums) {
+            int digitSum = sumOfDigit(num);
+            map.putIfAbsent(digitSum, new PriorityQueue<>(2)); 
+
+            PriorityQueue<Integer> pq = map.get(digitSum);
+            pq.offer(num);
+         
+            if (pq.size() > 2) {
+                pq.poll(); 
+            }
         }
 
         int maxSum = -1;
-
-        for( Integer key:map.keySet()){
-            List<Integer> list=map.get(key);
-            if(list.size()>1){
-                Collections.sort(list, Collections.reverseOrder());
-                int currentSum = list.get(0) + list.get(1);
-                maxSum = Math.max(maxSum, currentSum);
+        for (PriorityQueue<Integer> pq : map.values()) {
+            if (pq.size() == 2) {
+                int sum = pq.poll() + pq.poll(); 
+                maxSum = Math.max(maxSum, sum);
             }
         }
 
         return maxSum;
-        
     }
 
-    public static int sumOfDigit(int x){
-        int sum=0;
-        while(x!=0){
-            sum+=x%10;
-            x=x/10;
+   
+    public static int sumOfDigit(int x) {
+        int sum = 0;
+        while (x != 0) {
+            sum += x % 10;
+            x /= 10;
         }
         return sum;
     }
 }
+
