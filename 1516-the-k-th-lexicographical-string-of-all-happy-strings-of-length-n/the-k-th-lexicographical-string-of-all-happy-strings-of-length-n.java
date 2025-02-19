54f8782eae2db715483732c935ba2faf7ddbd8cf
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-19 19:45:16 +0530

    Time: 13 ms (53.53%) | Memory: 45 MB (45.56%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,37 +1,70 @@
+// class Solution {
+//     public String getHappyString(int n, int k) {
+
+//         ArrayList<String> str = new ArrayList<>();
+//         ArrayList<ArrayList<String>> list = new ArrayList<>();
+
+//         helper(0, n, str, list);
+//         if (k > list.size()) {
+//             return "";
+//         }
+//           StringBuilder result = new StringBuilder();
+//         for (String ch : list.get(k - 1)) {  
+//             result.append(ch);
+//         }
+//         return result.toString();
+//     }
+
+//     public void helper(int index, int n, ArrayList<String> current, ArrayList<ArrayList<String>> list) {
+//         if (current.size() == n) {
+//             list.add(new ArrayList<>(current));
+//             return;
+//         }
+//         String chars = "abc";
+
+//         for (int i = 0; i < chars.length(); i++) {
+//             char c = chars.charAt(i);
+
+//             if (current.isEmpty() || current.get(current.size() - 1).charAt(0) != c) {
+//                 current.add(String.valueOf(c));
+//                 helper(index + 1, n, current, list);
+//                 current.remove(current.size() - 1);
+//             }
+
+//         }
+//     }
+
+// }
+
+
+////
+
+import java.util.ArrayList;
+
 class Solution {
     public String getHappyString(int n, int k) {
-
-        ArrayList<String> str = new ArrayList<>();
-        ArrayList<ArrayList<String>> list = new ArrayList<>();
-
-        helper(0, n, str, list);
+        ArrayList<String> list = new ArrayList<>();
+        helper(0, n, new StringBuilder(), list);
         if (k > list.size()) {
             return "";
         }
-          StringBuilder result = new StringBuilder();
-        for (String ch : list.get(k - 1)) {  
-            result.append(ch);
-        }
-        return result.toString();
+        return list.get(k - 1);
     }
 
-    public void helper(int index, int n, ArrayList<String> current, ArrayList<ArrayList<String>> list) {
-        if (current.size() == n) {
-            list.add(new ArrayList<>(current));
+    public void helper(int index, int n, StringBuilder current, ArrayList<String> list) {
+        if (current.length() == n) {
+            list.add(current.toString());
             return;
         }
-        String chars = "abc";
 
+        String chars = "abc";
         for (int i = 0; i < chars.length(); i++) {
             char c = chars.charAt(i);
-
-            if (current.isEmpty() || current.get(current.size() - 1).charAt(0) != c) {
-                current.add(String.valueOf(c));
+            if (current.length() == 0 || current.charAt(current.length() - 1) != c) {
+                current.append(c);
                 helper(index + 1, n, current, list);
-                current.remove(current.size() - 1);
+                current.deleteCharAt(current.length() - 1);
             }
-
         }
     }
-
 }
