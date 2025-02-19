d0a3191f34bece4ad5825b93fc9f9e4e3103b6bd
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-19 19:48:17 +0530

    Time: 2 ms (83.83%) | Memory: 42.4 MB (70.39%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -39,6 +39,39 @@
 
 ////
 
+// import java.util.ArrayList;
+
+// class Solution {
+//     public String getHappyString(int n, int k) {
+//         ArrayList<String> list = new ArrayList<>();
+//         helper(0, n, new StringBuilder(), list);
+//         if (k > list.size()) {
+//             return "";
+//         }
+//         return list.get(k - 1);
+//     }
+
+//     public void helper(int index, int n, StringBuilder current, ArrayList<String> list) {
+//         if (current.length() == n) {
+//             list.add(current.toString());
+//             return;
+//         }
+
+//         String chars = "abc";
+//         for (int i = 0; i < chars.length(); i++) {
+//             char c = chars.charAt(i);
+//             if (current.length() == 0 || current.charAt(current.length() - 1) != c) {
+//                 current.append(c);
+//                 helper(index + 1, n, current, list);
+//                 current.deleteCharAt(current.length() - 1);
+//             }
+//         }
+//     }
+// }
+
+////way 3
+
+
 import java.util.ArrayList;
 
 class Solution {
