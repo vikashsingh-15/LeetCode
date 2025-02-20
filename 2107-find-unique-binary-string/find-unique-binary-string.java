a0f35ddb25bbccb1a582e13a62f38d6571b20c29
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-20 21:38:27 +0530

    Time: 275 ms (9.27%) | Memory: 56.6 MB (5.09%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -10,26 +10,64 @@
 //     }
 // }
 
+////way 2
+
+// class Solution {
+
+//     public String findDifferentBinaryString(String[] nums) {
+//         int n = nums.length;
+//         Set<Integer> set = new HashSet<>();
+//         String binaryString = "";
+//         for (String num : nums) {
+//             int decimalValue = Integer.parseInt(num, 2);
+//             set.add(decimalValue);
+//         }
+
+//         for (int i = 0; i <= 16; i++) {
+//             if (set.contains(i)) {
+//                 continue;
+//             } else {
+//                 binaryString = String.format("%" + n + "s", Integer.toBinaryString(i)).replace(' ', '0');
+//                 break;
+//             }
+//         }
+
+//         return binaryString;
+//     }
+// }
+
+
+////Way 3
+
 class Solution {
-
     public String findDifferentBinaryString(String[] nums) {
-        int n = nums.length;
-        Set<Integer> set = new HashSet<>();
-        String binaryString = "";
+        int n=nums[0].length();
+        ArrayList<String>list=new ArrayList<>();
+         HashSet<String> set = new HashSet<>();
         for (String num : nums) {
-            int decimalValue = Integer.parseInt(num, 2);
-            set.add(decimalValue);
+            set.add(num);
         }
+        helper(n, new StringBuilder(),list);
 
-        for (int i = 0; i <= 16; i++) {
-            if (set.contains(i)) {
-                continue;
-            } else {
-                binaryString = String.format("%" + n + "s", Integer.toBinaryString(i)).replace(' ', '0');
-                break;
+        for (String binary : list) {
+            if (!set.contains(binary)) {
+                return binary;
             }
         }
 
-        return binaryString;
+        return "";
+    }
+
+    public void helper(int n,StringBuilder current,ArrayList<String>list){
+         if (current.length() == n) {
+            list.add(current.toString());  // Add to the list
+            return;
+        }
+         
+        helper(n,current.append(0),list);
+        current.deleteCharAt(current.length() - 1);
+
+        helper(n,current.append(1),list);
+        current.deleteCharAt(current.length() - 1);
     }
 }
