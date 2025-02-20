93b80748331736a04abd301d3f8b8fa1de4b318d
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-20 22:02:28 +0530

    Time: 203 ms (12.27%) | Memory: 55.3 MB (6.79%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -39,35 +39,73 @@
 
 ////Way 3
 
+// class Solution {
+//     public String findDifferentBinaryString(String[] nums) {
+//         int n=nums[0].length();
+//         ArrayList<String>list=new ArrayList<>();
+//          HashSet<String> set = new HashSet<>();
+//         for (String num : nums) {
+//             set.add(num);
+//         }
+//         helper(n, new StringBuilder(),list);
+
+//         for (String binary : list) {
+//             if (!set.contains(binary)) {
+//                 return binary;
+//             }
+//         }
+
+//         return "";
+//     }
+
+//     public void helper(int n,StringBuilder current,ArrayList<String>list){
+//          if (current.length() == n) {
+//             list.add(current.toString());  // Add to the list
+//             return;
+//         }
+         
+//         helper(n,current.append(0),list);
+//         current.deleteCharAt(current.length() - 1);
+
+//         helper(n,current.append(1),list);
+//         current.deleteCharAt(current.length() - 1);
+//     }
+// }
+
+////way 4
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        int n=nums[0].length();
-        ArrayList<String>list=new ArrayList<>();
-         HashSet<String> set = new HashSet<>();
+        int n = nums[0].length();
+        HashSet<String> set = new HashSet<>();
         for (String num : nums) {
             set.add(num);
         }
-        helper(n, new StringBuilder(),list);
 
+        ArrayList<String> list = new ArrayList<>();
+        generateBinaryStrings(n, "", list);
+
+        // Find a string that is not in the original set
         for (String binary : list) {
             if (!set.contains(binary)) {
                 return binary;
             }
         }
 
-        return "";
+        return "";  // This line should never be reached
     }
 
-    public void helper(int n,StringBuilder current,ArrayList<String>list){
-         if (current.length() == n) {
-            list.add(current.toString());  // Add to the list
+    public void generateBinaryStrings(int n, String current, ArrayList<String> list) {
+        if (current.length() == n) {
+            list.add(current);
             return;
         }
-         
-        helper(n,current.append(0),list);
-        current.deleteCharAt(current.length() - 1);
 
-        helper(n,current.append(1),list);
-        current.deleteCharAt(current.length() - 1);
+        generateBinaryStrings(n, current + "0", list);
+        generateBinaryStrings(n, current + "1", list);
     }
 }
