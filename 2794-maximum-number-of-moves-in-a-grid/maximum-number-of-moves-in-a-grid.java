de289c4abf855fb00a4912fc386347cc413b4531
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-24 20:24:22 +0530

    Time: 15 ms (21.07%) | Memory: 56.3 MB (65.41%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
@@ -1,40 +1,83 @@
-class Solution {
-    int dp[][];
-    int m,n;
+// class Solution {
+//     int dp[][];
+//     int m,n;
+//     public int maxMoves(int[][] grid) {
+//          m = grid.length;
+//          n = grid[0].length;
+//         dp = new int[m][n];
+//         int maxMoves = 0;
+
+//         for(int i=0;i<m;i++){
+//              maxMoves = Math.max(maxMoves, dfs(grid, i, 0));
+//         }
+//         return maxMoves;
+//     }
+
+//      private int dfs(int[][] grid, int row, int col) {
+
+//         if(col==n-1)return 0;
+
+//         if(dp[row][col]!=0){
+//             return dp[row][col];
+//         }
+
+//         int maxSteps=0;
+
+//         if (col + 1 < n && grid[row][col + 1] > grid[row][col]) {
+//             maxSteps = Math.max(maxSteps, 1 + dfs(grid, row, col + 1));
+//         }
+//         if (row - 1 >= 0 && col + 1 < n && grid[row - 1][col + 1] > grid[row][col]) {
+//             maxSteps = Math.max(maxSteps, 1 + dfs(grid, row - 1, col + 1));
+//         }
+//         if (row + 1 < m && col + 1 < n && grid[row + 1][col + 1] > grid[row][col]) {
+//             maxSteps = Math.max(maxSteps, 1 + dfs(grid, row + 1, col + 1));
+//         }
+
+//         dp[row][col] = maxSteps;
+//         return maxSteps;
+
+//      }
+// }
+
+
+////way 2
+
+ class Solution {
     public int maxMoves(int[][] grid) {
-         m = grid.length;
-         n = grid[0].length;
-        dp = new int[m][n];
-        int maxMoves = 0;
 
-        for(int i=0;i<m;i++){
-             maxMoves = Math.max(maxMoves, dfs(grid, i, 0));
+        int m=grid.length;
+        int n=grid[0].length;
+        int dp[][] =new int [m][n];
+
+        int max=0;
+
+        for(int col=n-1;col>=0;col--){
+            for(int row=0;row<m;row++){
+
+                int side=0;
+                int diagUp=0;
+                int diagDown=0;
+
+               if (row - 1 >= 0 &&  col+1<n  && grid[row - 1][col + 1] > grid[row][col]) {
+                    diagUp = dp[row - 1][col + 1] + 1;
+                }
+                if (col + 1 < n  && grid[row][col + 1] > grid[row][col]) {
+                    side = dp[row][col + 1] + 1;
+                }
+                if (row + 1 < m && col+1<n && grid[row + 1][col + 1] > grid[row][col]) {
+                    diagDown = dp[row + 1][col + 1] + 1;
+                }
+
+                dp[row][col] = Math.max(diagUp, Math.max(side, diagDown));
+                
+                if(col==0){
+                    max=Math.max(max,dp[row][col]);
+                }
+
+            }
+
         }
-        return maxMoves;
+
+        return max;
     }
-
-     private int dfs(int[][] grid, int row, int col) {
-
-        if(col==n-1)return 0;
-
-        if(dp[row][col]!=0){
-            return dp[row][col];
-        }
-
-        int maxSteps=0;
-
-        if (col + 1 < n && grid[row][col + 1] > grid[row][col]) {
-            maxSteps = Math.max(maxSteps, 1 + dfs(grid, row, col + 1));
-        }
-        if (row - 1 >= 0 && col + 1 < n && grid[row - 1][col + 1] > grid[row][col]) {
-            maxSteps = Math.max(maxSteps, 1 + dfs(grid, row - 1, col + 1));
-        }
-        if (row + 1 < m && col + 1 < n && grid[row + 1][col + 1] > grid[row][col]) {
-            maxSteps = Math.max(maxSteps, 1 + dfs(grid, row + 1, col + 1));
-        }
-
-        dp[row][col] = maxSteps;
-        return maxSteps;
-
-     }
 }
