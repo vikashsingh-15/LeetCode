2695fa4cda2bb8cdf8a12836263b160373626ee0
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-27 12:35:58 +0530

    Time: 169 ms (66.27%) | Memory: 45.3 MB (68.34%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,24 +1,63 @@
+// class Solution {
+//     public int lenLongestFibSubseq(int[] arr) {
+//         int n = arr.length;
+//         HashSet<Integer> set = new HashSet<>();
+//         for (int num : arr) {
+//             set.add(num);
+//         }
+//         int maxLength = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 int prev = arr[j];
+//                 int expected = arr[i] + arr[j];
+//                 int currLength = 2;
+
+//                     while (set.contains(expected)) {
+//                         int temp = prev;
+//                         prev = expected;
+//                         expected = temp + prev;
+//                         currLength++;
+//                         maxLength = Math.max(currLength, maxLength);
+//                     }
+
+//             }
+//         }
+
+//         return maxLength;
+//     }
+// }
+
+
+
+////
+
+
 class Solution {
     public int lenLongestFibSubseq(int[] arr) {
-        int n=arr.length;
-        HashSet<Integer>set=new HashSet<>();
-        for(int num:arr){
+        int n = arr.length;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
             set.add(num);
         }
-        int maxLength=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int prev=arr[j];
-                int expected=arr[i]+arr[j];
-                int currLength=2;
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int prev = arr[j];
+                int expected = arr[i] + arr[j];
+                int currLength = 2;
 
-                while(set.contains(expected)){
-                    int temp=prev;
-                    prev=expected;
-                    expected=temp+prev;
-                    currLength++;
-                    maxLength=Math.max(currLength,maxLength);
+                if (set.contains(expected)) {
+                    while (set.contains(expected)) {
+                        int temp = prev;
+                        prev = expected;
+                        expected = temp + prev;
+                        currLength++;
+                        maxLength = Math.max(currLength, maxLength);
+                    }
+                }else{
+                    continue;
                 }
+
             }
         }
 
@@ -26,78 +65,80 @@ class Solution {
     }
 }
 
-////way 2 --> memory Limit Exceed
+
+
+//// way 2 --> memory Limit Exceed
 
 // class Solution {
-//     int maxLength = 0;
+// int maxLength = 0;
 
-//     public int lenLongestFibSubseq(int[] arr) {
-//         List<List<Integer>> allSubsequence = new ArrayList<>();
-//         generateSubsequences(arr, 0, new ArrayList<>(), allSubsequence);
-        
-//         for (List<Integer> seq : allSubsequence) {
-//             if (seq.size() >= 3 && isFibonacci(seq)) {
-//                 maxLength = Math.max(maxLength, seq.size());
-//             }
-//         }
+// public int lenLongestFibSubseq(int[] arr) {
+// List<List<Integer>> allSubsequence = new ArrayList<>();
+// generateSubsequences(arr, 0, new ArrayList<>(), allSubsequence);
 
-//         return maxLength;
-//     }
-
-//     private void generateSubsequences(int[] arr, int index, List<Integer> current, List<List<Integer>> allList) {
-//         if (index >= arr.length) {
-//             allList.add(new ArrayList<>(current));
-//             return;
-//         }
-
-//         current.add(arr[index]);
-//         generateSubsequences(arr, index + 1, current, allList);
-//         current.remove(current.size() - 1);
-//         generateSubsequences(arr, index + 1, current, allList);
-//     }
-
-//     private boolean isFibonacci(List<Integer> seq) {
-//         for (int i = 2; i < seq.size(); i++) {
-//             if (seq.get(i - 2) + seq.get(i - 1) != seq.get(i)) {
-//                 return false; 
-//             }
-//         }
-//         return true;
-//     }
+// for (List<Integer> seq : allSubsequence) {
+// if (seq.size() >= 3 && isFibonacci(seq)) {
+// maxLength = Math.max(maxLength, seq.size());
+// }
 // }
 
+// return maxLength;
+// }
 
-////way 3--TLE
+// private void generateSubsequences(int[] arr, int index, List<Integer>
+// current, List<List<Integer>> allList) {
+// if (index >= arr.length) {
+// allList.add(new ArrayList<>(current));
+// return;
+// }
+
+// current.add(arr[index]);
+// generateSubsequences(arr, index + 1, current, allList);
+// current.remove(current.size() - 1);
+// generateSubsequences(arr, index + 1, current, allList);
+// }
+
+// private boolean isFibonacci(List<Integer> seq) {
+// for (int i = 2; i < seq.size(); i++) {
+// if (seq.get(i - 2) + seq.get(i - 1) != seq.get(i)) {
+// return false;
+// }
+// }
+// return true;
+// }
+// }
+
+//// way 3--TLE
 
 // class Solution {
-//     private int maxLen = 0;
+// private int maxLen = 0;
 
-//     public int lenLongestFibSubseq(int[] arr) {
-//         List<Integer> currentSeq = new ArrayList<>();
-//         backtrack(arr, 0, currentSeq);
-//         return maxLen >= 3 ? maxLen : 0; 
-//     }
+// public int lenLongestFibSubseq(int[] arr) {
+// List<Integer> currentSeq = new ArrayList<>();
+// backtrack(arr, 0, currentSeq);
+// return maxLen >= 3 ? maxLen : 0;
+// }
 
-//     private void backtrack(int[] arr, int index, List<Integer> currentSeq) {
-//         if (currentSeq.size() >= 3 && !isFibonacci(currentSeq)) {
-//             return; 
-//         }else{
-//         maxLen = Math.max(maxLen, currentSeq.size()); 
-//         }
+// private void backtrack(int[] arr, int index, List<Integer> currentSeq) {
+// if (currentSeq.size() >= 3 && !isFibonacci(currentSeq)) {
+// return;
+// }else{
+// maxLen = Math.max(maxLen, currentSeq.size());
+// }
 
-//         for (int i = index; i < arr.length; i++) {
-//             currentSeq.add(arr[i]); 
-//             backtrack(arr, i + 1, currentSeq); 
-//             currentSeq.remove(currentSeq.size() - 1); 
-//         }
-//     }
+// for (int i = index; i < arr.length; i++) {
+// currentSeq.add(arr[i]);
+// backtrack(arr, i + 1, currentSeq);
+// currentSeq.remove(currentSeq.size() - 1);
+// }
+// }
 
-//     private boolean isFibonacci(List<Integer> seq) {
-//         for (int i = 2; i < seq.size(); i++) {
-//             if (seq.get(i - 2) + seq.get(i - 1) != seq.get(i)) {
-//                 return false; 
-//             }
-//         }
-//         return true;
-//     }
+// private boolean isFibonacci(List<Integer> seq) {
+// for (int i = 2; i < seq.size(); i++) {
+// if (seq.get(i - 2) + seq.get(i - 1) != seq.get(i)) {
+// return false;
+// }
+// }
+// return true;
+// }
 // }
