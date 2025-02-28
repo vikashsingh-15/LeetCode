6eb517a58ff7ae5a98b80b23889d4188426fa389
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-28 13:42:02 +0530

    Time: 137 ms (14.54%) | Memory: 41.9 MB (99.95%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,65 +1,43 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        if(n>m) return false;
-        int[] arr = new int[26];
+        int n=s1.length();
+        int m=s2.length();
+
+        if(n>m){
+            return false;
+        }
+
+        int arr []=new int [26];
         for(int i=0;i<n;i++){
-            arr[s1.charAt(i)-'a']++;
-         }
-         for(int i = 0;i<m;i++){
-            arr[s2.charAt(i)-'a']--;
-            if(i-n >= 0){
-                arr[s2.charAt(i-n)-'a']++;
+            char c=s1.charAt(i);
+            arr[c-'a']++;
+        }
+
+
+        for(int i=0;i<=m-n;i++){
+            for(int j=i;j<i+n;j++){
+                char c2=s2.charAt(j);
+                arr[c2-'a']--;
             }
-            if(zero(arr)) {
+            if(checkZero(arr)){
                 return true;
             }
+
+            for(int j=i;j<i+n;j++){
+                char c2=s2.charAt(j);
+                arr[c2-'a']++;
+            }
+
         }
         return false;
-
     }
 
-    public boolean zero(int[] arr) {
-        for (int i = 0; i < 26; i++) {
-            if (arr[i] != 0) {
+    public boolean checkZero(int [] arr){
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
                 return false;
             }
         }
         return true;
     }
 }
-
-
-//// way 2
-
-// class Solution {
-
-//     public boolean checkInclusion(String s1, String s2) {
-//         int n1 = s1.length(), n2 = s2.length();
-//         if (n1 > n2) {
-//             return false;
-//         }
-        
-//         int[] freq = new int[26];
-//         for (int i = 0; i < n1; i++) {
-//             freq[s1.charAt(i) - 'a']++;
-//         }
-        
-//         int left = 0, right = 0;
-//         while (right < n2) {
-//             char ch = s2.charAt(right);
-//             freq[ch - 'a']--;
-//             while (freq[ch - 'a'] < 0) {
-//                 freq[s2.charAt(left) - 'a']++;
-//                 left++;
-//             }
-//             if (right - left + 1 == n1) {
-//                 return true;
-//             }
-//             right++;
-//         }
-//         return false;
-//     }
-// }
-
