1bc1ad43464c07874635f0af5d14fe8a7fc168c0
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-28 13:54:33 +0530

    Time: 5 ms (96.05%) | Memory: 43.1 MB (44.67%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -13,7 +13,6 @@
 //             arr[c-'a']++;
 //         }
 
-
 //         for(int i=0;i<=m-n;i++){
 //             for(int j=i;j<i+n;j++){
 //                 char c2=s2.charAt(j);
@@ -42,10 +41,48 @@
 //     }
 // }
 
-
-
 //way 2
 
+// class Solution {
+//     public boolean checkInclusion(String s1, String s2) {
+//         int n = s1.length();
+//         int m = s2.length();
+
+//         if (n > m) {
+//             return false;
+//         }
+
+//         int arr[] = new int[26];
+//         for (int i = 0; i < n; i++) {
+//             arr[s1.charAt(i) - 'a']++;
+//         }
+
+//         for (int i = 0; i <= m - n; i++) { 
+//             int[] tempArr = arr.clone();
+
+//             for (int j = i; j < i + n; j++) {
+//                 char c2 = s2.charAt(j);
+//                 tempArr[c2 - 'a']--;
+//             }
+
+//             if (checkZero(tempArr)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     public boolean checkZero(int[] arr) {
+//         for (int i = 0; i < 26; i++) {
+//             if (arr[i] != 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+////way 3
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
@@ -60,15 +97,14 @@ class Solution {
             arr[s1.charAt(i) - 'a']++;
         }
 
-        for (int i = 0; i <= m - n; i++) { 
-            int[] tempArr = arr.clone();
+        for (int i = 0; i < m; i++) {
+            char c2 = s2.charAt(i);
+            arr[c2 - 'a']--;
 
-            for (int j = i; j < i + n; j++) {
-                char c2 = s2.charAt(j);
-                tempArr[c2 - 'a']--;
+            if (i - n >= 0) {
+                arr[s2.charAt(i - n) - 'a']++;
             }
-
-            if (checkZero(tempArr)) {
+            if (checkZero(arr)) {
                 return true;
             }
         }
