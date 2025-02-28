503f126729bd3c84420fa62f376d70980b29dde8
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-02-28 13:43:07 +0530

    Time: 74 ms (17.69%) | Memory: 44.8 MB (22.97%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,40 +1,83 @@
+// class Solution {
+//     public boolean checkInclusion(String s1, String s2) {
+//         int n=s1.length();
+//         int m=s2.length();
+
+//         if(n>m){
+//             return false;
+//         }
+
+//         int arr []=new int [26];
+//         for(int i=0;i<n;i++){
+//             char c=s1.charAt(i);
+//             arr[c-'a']++;
+//         }
+
+
+//         for(int i=0;i<=m-n;i++){
+//             for(int j=i;j<i+n;j++){
+//                 char c2=s2.charAt(j);
+//                 arr[c2-'a']--;
+//             }
+//             if(checkZero(arr)){
+//                 return true;
+//             }
+
+//             for(int j=i;j<i+n;j++){
+//                 char c2=s2.charAt(j);
+//                 arr[c2-'a']++;
+//             }
+
+//         }
+//         return false;
+//     }
+
+//     public boolean checkZero(int [] arr){
+//         for(int i=0;i<26;i++){
+//             if(arr[i]!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+//way 2
+
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n=s1.length();
-        int m=s2.length();
+        int n = s1.length();
+        int m = s2.length();
 
-        if(n>m){
+        if (n > m) {
             return false;
         }
 
-        int arr []=new int [26];
-        for(int i=0;i<n;i++){
-            char c=s1.charAt(i);
-            arr[c-'a']++;
+        int arr[] = new int[26];
+        for (int i = 0; i < n; i++) {
+            arr[s1.charAt(i) - 'a']++;
         }
 
+        for (int i = 0; i <= m - n; i++) { 
+            int[] tempArr = arr.clone();
 
-        for(int i=0;i<=m-n;i++){
-            for(int j=i;j<i+n;j++){
-                char c2=s2.charAt(j);
-                arr[c2-'a']--;
+            for (int j = i; j < i + n; j++) {
+                char c2 = s2.charAt(j);
+                tempArr[c2 - 'a']--;
             }
-            if(checkZero(arr)){
+
+            if (checkZero(tempArr)) {
                 return true;
             }
-
-            for(int j=i;j<i+n;j++){
-                char c2=s2.charAt(j);
-                arr[c2-'a']++;
-            }
-
         }
         return false;
     }
 
-    public boolean checkZero(int [] arr){
-        for(int i=0;i<26;i++){
-            if(arr[i]!=0){
+    public boolean checkZero(int[] arr) {
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0) {
                 return false;
             }
         }
