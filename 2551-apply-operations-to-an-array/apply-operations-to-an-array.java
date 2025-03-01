c13fcca186cc909a1dcad97dfe062398dd019320
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-03-01 19:27:03 +0530

    Time: 1 ms (96.37%) | Memory: 43.7 MB (56.58%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,8 +1,44 @@
+// class Solution {
+//     public int[] applyOperations(int[] arr) {
+//         int n=arr.length;
+//         Queue<Integer>queue=new LinkedList<>();
+
+//         for(int i=0;i<n-1;i++){
+//             if(arr[i]==arr[i+1] && arr[i]!=0){
+//                 arr[i]*=2;
+//                 arr[i+1]=0;
+//             }
+
+//             if(arr[i]!=0){
+//                 queue.add(arr[i]);
+//             }
+//         }
+
+//         if(arr[n-1]!=0){
+//             queue.add(arr[n-1]);
+//         }
+
+//         int index=0;
+//         while(!queue.isEmpty()){
+//             arr[index++]=queue.poll();
+//         }
+
+//         while(index<n){///after queue is over we  cant left arr as it is we have to set it 0 
+//              arr[index++]=0;
+//         }
+
+//         return arr;
+        
+//     }
+// }
+
+
+////way 2
+
 class Solution {
     public int[] applyOperations(int[] arr) {
         int n=arr.length;
-        Queue<Integer>queue=new LinkedList<>();
-
+        int index=0;
         for(int i=0;i<n-1;i++){
             if(arr[i]==arr[i+1] && arr[i]!=0){
                 arr[i]*=2;
@@ -10,22 +46,17 @@ class Solution {
             }
 
             if(arr[i]!=0){
-                queue.add(arr[i]);
+                arr[index++]=arr[i];
             }
         }
 
         if(arr[n-1]!=0){
-            queue.add(arr[n-1]);
+            arr[index++]=arr[n-1];
         }
 
-        int index=0;
-        while(!queue.isEmpty()){
-            arr[index++]=queue.poll();
-        }
-
-        while(index<n){///after queue is over we  cant left arr as it is we have to set it 0 
-             arr[index++]=0;
-        }
+       while(index<n){
+         arr[index++]=0;
+       }
 
         return arr;
         
