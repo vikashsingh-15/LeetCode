12242e42070dda1c3ea3fa43713f8b874562822e
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-03-03 13:36:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (28.42%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,8 +1,29 @@
+////not a vlaid solution see ex "()))((" ans should be 4 but this approach give 0.
+
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//         int open=0;
+//         int minAdd=0;
+//         int close=0;
+//         for(char c : s.toCharArray()){
+//             if(c=='('){
+//                 open++;
+//             }else{
+//                 close++;
+//             }
+//         }
+//         return Math.abs(open-close);
+//     }
+// }
+
+
+////way 2
+
 class Solution {
     public int minAddToMakeValid(String s) {
         int open=0;
         int minAdd=0;
-        // int close=0;
+        int close=0;
         for(char c : s.toCharArray()){
             if(c=='('){
                 open++;
@@ -14,6 +35,6 @@ class Solution {
                 }
             }
         }
-        return minAdd+open;
+        return open+minAdd;
     }
 }
