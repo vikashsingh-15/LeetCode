a852007d803aa1fa383d1ff12d8322169daaa484
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-03-03 13:18:44 +0530

    Time: 5 ms (85.39%) | Memory: 68.4 MB (15.73%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,41 +1,71 @@
+////way 1 
+
+// class Solution {
+//     public int[] pivotArray(int[] nums, int pivot) {
+//         int ans[]=new int[nums.length];
+//         ArrayList<Integer> list=new ArrayList();
+//         int same=0;
+//         int small=0;
+//         int large=0;
+
+//         for(int num:nums){
+//             list.add(num);
+//             if(num==pivot){
+//                 same++;
+//             }
+//             else if(num<pivot){
+//                 small++;
+//             }
+//             else{
+//                 large++;
+//             }
+//         }
+//         int constSmall=small;
+//         int constLarge=large;
+//         int constSame=same;
+
+//         for(int num:list){
+//             if(num<pivot){
+//                 ans[constSmall-(small--)]=num;
+//             }
+//             if(num==pivot){
+//                 ans[constSmall +  constSame-(same--)]=pivot;
+//             }
+
+//             if(num>pivot){
+//                 ans[constSmall+constSame+constLarge - (large--)]=num;
+//             }
+
+//         }
+//         return ans;
+//     }
+// }
+
+////way 2
+
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-
         int ans[]=new int[nums.length];
-        ArrayList<Integer> list=new ArrayList();
-        int same=0;
-        int small=0;
-        int large=0;
+        int index=0;
+        for(int num:nums){
+           if(num<pivot){
+            ans[index++]=num;
+           }
+        }
 
         for(int num:nums){
-            list.add(num);
-            if(num==pivot){
-                same++;
-            }
-            else if(num<pivot){
-                small++;
-            }
-            else{
-                large++;
-            }
+           if(num==pivot){
+            ans[index++]=num;
+           }
         }
-        int constSmall=small;
-        int constLarge=large;
-        int constSame=same;
 
-        for(int num:list){
-            if(num<pivot){
-                ans[constSmall-(small--)]=num;
-            }
-            if(num==pivot){
-                ans[constSmall +  constSame-(same--)]=pivot;
-            }
-
-            if(num>pivot){
-                ans[constSmall+constSame+constLarge - (large--)]=num;
-            }
-
+         for(int num:nums){
+           if(num>pivot){
+            ans[index++]=num;
+           }
         }
+        
         return ans;
     }
 }
+
