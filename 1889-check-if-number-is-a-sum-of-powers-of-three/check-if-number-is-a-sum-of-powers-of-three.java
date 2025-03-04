7fe915a5c916381c5aea488d046a40ad2d6dce33
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-03-04 11:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (60.76%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,14 +1,26 @@
+// class Solution {
+//     public boolean checkPowersOfThree(int n) {
+//         while(n>1){
+//             int rem=n%3;
+//             if(rem==2){
+//                 return false;
+//             }
+//             n/=3;
+//         }
+//        return true; 
+//     }
+// }
+
+//// way 2
 class Solution {
     public boolean checkPowersOfThree(int n) {
-
-
-        while(n>1){
-            int rem=n%3;
-            if(rem==2){
+        while (n > 0) {
+            if (n % 3 == 1 || n % 3 == 0) {
+                n /= 3;
+            } else {
                 return false;
             }
-            n/=3;
         }
-       return true; 
+        return true;
     }
 }
