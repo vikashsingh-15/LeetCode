2fcdebca1376f449839cceff9e9c8436eabe6974
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-03-08 14:15:21 +0530

    Time: 2 ms (21.93%) | Memory: 41.7 MB (40.60%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,24 +1,58 @@
+// class Solution {
+//     public int minimumRecolors(String str, int k) {
+
+//         int left = 0;
+//         int numWhite = 0;
+//         int numRecol = Integer.MAX_VALUE;
+
+//         for (int right = 0; right < str.length(); right++) {
+//             if (str.charAt(right) == 'W') {
+//                 numWhite++;
+//             }
+
+//             if (right - left + 1 == k) {
+//                 numRecol = Math.min(numWhite, numRecol);
+//                 if (str.charAt(left) == 'W') {
+//                     numWhite--;
+//                 }
+//                 left++;
+//             }
+
+//         }
+//         return numRecol;
+//     }
+// }
+
+////way  2
+
 class Solution {
     public int minimumRecolors(String str, int k) {
-
+        Queue<Character> queue = new LinkedList<>();
         int left = 0;
         int numWhite = 0;
-        int numRecol = Integer.MAX_VALUE;
 
-        for (int right = 0; right < str.length(); right++) {
-            if (str.charAt(right) == 'W') {
+        for (int i = 0; i < k; i++) {
+            queue.add(str.charAt(i));
+            if (str.charAt(i) == 'W') {
                 numWhite++;
             }
-
-            if (right - left + 1 == k) {
-                numRecol = Math.min(numWhite, numRecol);
-                if (str.charAt(left) == 'W') {
-                    numWhite--;
-                }
-                left++;
-            }
-
         }
-        return numRecol;
+
+        int numRecolors=numWhite;
+
+        for(int i=k;i<str.length();i++){
+            char c=str.charAt(i);
+            if(c=='W'){
+                numWhite++;
+            }
+            if(queue.poll()=='W'){
+                numWhite--;
+            }
+            queue.add(c);
+
+            numRecolors = Math.min(numRecolors, numWhite);
+        }
+
+        return numRecolors;
     }
 }
