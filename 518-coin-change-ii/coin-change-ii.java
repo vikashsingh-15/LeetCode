450d83940c63bc05b022c3ccb1698986e316ead7
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-03-15 15:36:35 +0530

    Time: 429 ms (5.00%) | Memory: 140.3 MB (5.08%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -20,34 +20,60 @@
 // }
 
 ////way 2 
+// class Solution {
+//     int ans = 0;
+//     HashMap<String, Integer> map = new HashMap<>();
+
+//     public int change(int amount, int[] coins) {
+//         helper(amount, coins, 0);
+//         return ans;
+//     }
+
+//     public void helper(int amount, int[] coins, int index) {
+//         if (amount == 0) {
+//             ans++;
+//             return;
+//         }
+
+//         if (amount < 0 || index >= coins.length) {
+//             return;
+//         }
+
+//         String key = amount + "," + index;
+//         if (map.containsKey(key)) {
+//             ans += map.get(key); // Use previously computed result
+//             return;
+//         }
+
+//         int before = ans;
+//         helper(amount - coins[index], coins, index);
+//         helper(amount, coins, index + 1);
+//         map.put(key, ans - before);
+//     }
+// }
+
+////way 3
+
+import java.util.*;
+
 class Solution {
-    int ans = 0;
     HashMap<String, Integer> map = new HashMap<>();
 
     public int change(int amount, int[] coins) {
-        helper(amount, coins, 0);
-        return ans;
+        return helper(amount, coins, 0);
     }
 
-    public void helper(int amount, int[] coins, int index) {
-        if (amount == 0) {
-            ans++;
-            return;
-        }
-
-        if (amount < 0 || index >= coins.length) {
-            return;
-        }
+    public int helper(int amount, int[] coins, int index) {
+        if (amount == 0) return 1;
+        if (amount < 0 || index >= coins.length) return 0;
 
         String key = amount + "," + index;
-        if (map.containsKey(key)) {
-            ans += map.get(key); // Use previously computed result
-            return;
-        }
+        if (map.containsKey(key)) return map.get(key);
 
-        int before = ans;
-        helper(amount - coins[index], coins, index);
-        helper(amount, coins, index + 1);
-        map.put(key, ans - before);
+        int take = helper(amount - coins[index], coins, index);
+        int skip = helper(amount, coins, index + 1);
+
+        map.put(key, take + skip);
+        return take + skip;
     }
 }
