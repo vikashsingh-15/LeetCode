7e37dd0a0117616577f39191b1efc01bb21bf44f
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-03-15 18:26:01 +0530

    Time: 19 ms (71.39%) | Memory: 55.8 MB (82.84%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.java
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -1,28 +1,100 @@
+// ////Memory exceed
+// class Solution {
+//     int min = Integer.MAX_VALUE;
+
+//     public int minCapability(int[] nums, int k) {
+//         List<List<Integer>> list = new ArrayList<>();
+//         helper(nums, k, list, new ArrayList<>(), 0);
+
+//         for (List<Integer> subset : list) {
+//             min = Math.min(min, Collections.max(subset)); 
+//         }
+
+//         return min;
+//     }
+
+//     public void helper(int[] nums, int k, List<List<Integer>> list, List<Integer> l1, int index) {
+//         if (l1.size() == k) {
+//             list.add(new ArrayList<>(l1));
+//             return;
+//         }
+
+//         if (index >= nums.length) {
+//             return;
+//         }
+//         l1.add(nums[index]);
+//         helper(nums, k, list, l1, index + 2);
+//         l1.remove(l1.size() - 1); // Backtrack
+
+//         helper(nums, k, list, l1, index + 1);
+//     }
+// }
+
+
+////way 2 TLE
+
+// class Solution {
+//     int min = Integer.MAX_VALUE;
+
+//     public int minCapability(int[] nums, int k) {
+//         helper(nums, k, new ArrayList<>(), 0);
+//         return min;
+//     }
+
+//     public void helper(int[] nums, int k, List<Integer> l1, int index) {
+//         if (l1.size() == k) {
+//             int maxInSubset = Collections.max(l1);
+//             min = Math.min(min, maxInSubset);
+//             return;
+//         }
+
+//         if (index >= nums.length) {
+//             return;
+//         }
+
+//         l1.add(nums[index]);
+//         helper(nums, k, l1, index + 2);
+//         l1.remove(l1.size() - 1); // Backtrack
+
+       
+//         helper(nums, k, l1, index + 1);
+//     }
+// }
+
+////way 3 
 class Solution {
+       public int minCapability(int[] nums, int k) {
+        int left = Integer.MAX_VALUE, right = Integer.MIN_VALUE;
 
-    public int minCapability(int[] nums, int k) {
-        // Store the maximum nums value in maxReward.
-        int minReward = 1;
-        int maxReward = Arrays.stream(nums).max().getAsInt();
-        int totalHouses = nums.length;
-
-        // Use binary search to find the minimum reward possible.
-        while (minReward < maxReward) {
-            int midReward = (minReward + maxReward) / 2;
-            int possibleThefts = 0;
-
-            for (int index = 0; index < totalHouses; ++index) {
-                if (nums[index] <= midReward) {
-                    possibleThefts += 1;
-                    index++; // Skip the next house to maintain the
-                    // non-adjacent condition
-                }
-            }
-
-            if (possibleThefts >= k) maxReward = midReward;
-            else minReward = midReward + 1;
+        for (int num : nums) {
+            left = Math.min(left, num);
+            right = Math.max(right, num);
         }
 
-        return minReward;
+        while(left<right){
+            int mid = left + (right - left) / 2;
+            if (canPick(nums, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean canPick(int[] nums, int k, int maxCap) {
+        int count = 0;
+        int i = 0;
+
+        while (i < nums.length) {
+           if(nums[i]<=maxCap){
+            count++;
+            i += 2;
+           }else{
+            i++;
+           }
+           if (count >= k) return true;
+        }
+        return false;
     }
 }
