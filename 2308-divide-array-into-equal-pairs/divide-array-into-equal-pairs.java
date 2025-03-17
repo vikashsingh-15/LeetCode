e1e712e36a00a21d8cbccf8d5a1b181ae2192174
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-03-17 13:32:42 +0530

    Time: 3 ms (77.44%) | Memory: 44.9 MB (19.23%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+
+        Arrays.sort(nums);
+        
+
+        for(int i=0;i<nums.length;i=i+2){
+            if(nums[i]!=nums[i+1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
