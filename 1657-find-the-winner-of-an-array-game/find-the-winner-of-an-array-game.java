81a6981fe5606486bc9a6c6076e446295dddb690
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-04-06 15:44:22 +0530

    Time: 1 ms (40.38%) | Memory: 59.8 MB (42.95%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
@@ -33,13 +33,54 @@
 
 
 
+// class Solution {
+//     public int getWinner(int[] arr, int k) {
+//          int maxElement = arr[0];
+//         for (int i = 1; i < arr.length; i++) {
+//             maxElement = Math.max(maxElement, arr[i]);
+//         }
+        
+//         int curr = arr[0];
+//         int winstreak = 0;
+        
+//         for (int i = 1; i < arr.length; i++) {
+//             int opponent = arr[i];
+            
+//             if (curr > opponent) {
+//                 winstreak++;
+//             } else {
+//                 curr = opponent;
+//                 winstreak = 1;
+//             }
+            
+//             if (winstreak == k || curr == maxElement) {
+//                 return curr;
+//             }
+//         }
+        
+//         return -1;
+//     }
+// }
+
+
+
+////way 3
+
 class Solution {
     public int getWinner(int[] arr, int k) {
-         int maxElement = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            maxElement = Math.max(maxElement, arr[i]);
+        int maxNum=Integer.MIN_VALUE;
+        int index=-1;
+        for(int i=0;i<arr.length;i++){
+           if(arr[i]>maxNum){
+            maxNum=arr[i];
+            index=i;
+           }
         }
-        
+
+        if(k>index){
+            return maxNum;
+        }
+
         int curr = arr[0];
         int winstreak = 0;
         
@@ -53,11 +94,12 @@ class Solution {
                 winstreak = 1;
             }
             
-            if (winstreak == k || curr == maxElement) {
+            if (winstreak == k || curr == maxNum) {
                 return curr;
             }
         }
         
         return -1;
+        
     }
 }
