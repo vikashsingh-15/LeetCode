9975454f3610bd2aa8348c64d1e159b775c70096
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-04-08 12:10:16 +0530

    Time: 4 ms (29.35%) | Memory: 44.8 MB (23.39%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,23 +1,54 @@
-class Solution {
+// class Solution {
 
+//     public int minimumOperations(int[] nums) {
+//         int ans = 0;
+//         for (int i = 0; i < nums.length; i += 3, ans++) {
+//             if (checkUnique(nums, i)) {
+//                 return ans;
+//             }
+//         }
+//         return ans;
+//     }
+
+//     private boolean checkUnique(int[] nums, int start) {
+//         HashSet<Integer> cnt = new HashSet<>();
+//         for (int i = start; i < nums.length; i++) {
+//             if (cnt.contains(nums[i])) {
+//                 return false;
+//             }
+//             cnt.add(nums[i]);
+//         }
+//         return true;
+//     }
+// }
+
+////way 2
+import java.util.*;
+
+class Solution {
     public int minimumOperations(int[] nums) {
         int ans = 0;
-        for (int i = 0; i < nums.length; i += 3, ans++) {
-            if (checkUnique(nums, i)) {
+
+        for (int i = 0; i <= nums.length; i += 3) {
+            HashSet<Integer> set = new HashSet<>();
+            boolean hasDuplicate = false;
+
+            for (int j = i; j < nums.length; j++) {
+                if (set.contains(nums[j])) {
+                    hasDuplicate = true;
+                    break;
+                }else{
+                    set.add(nums[j]);
+                }
+            }
+
+            if (hasDuplicate) {
+                ans++;
+            }else{
                 return ans;
             }
         }
-        return ans;
-    }
 
-    private boolean checkUnique(int[] nums, int start) {
-        HashSet<Integer> cnt = new HashSet<>();
-        for (int i = start; i < nums.length; i++) {
-            if (cnt.contains(nums[i])) {
-                return false;
-            }
-            cnt.add(nums[i]);
-        }
-        return true;
+        return ans;
     }
 }
