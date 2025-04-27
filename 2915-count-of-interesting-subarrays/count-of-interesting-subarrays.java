596241a1319acfdfaa3f3eb6490ca8c695dab154
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-04-27 13:48:44 +0530

    Time: 33 ms (55.81%) | Memory: 66.1 MB (15.03%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
@@ -1,20 +1,54 @@
-class Solution {
+////way 1-TLE
+// class Solution {
+//     public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+//          int n = nums.size();
+//         long count = 0;
 
-    public long countInterestingSubarrays(
-        List<Integer> nums,
-        int modulo,
-        int k
-    ) {
+//         for(int i=0;i<n;i++){
+//             int cnt=0;
+
+//             for(int j=i;j<n;j++){
+//                 if(nums.get(j)%modulo==k){
+//                     cnt++;
+//                 }
+//                  if (cnt % modulo == k) {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+////way 2
+
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
         int n = nums.size();
-        HashMap<Integer, Integer> cnt = new HashMap<>();
-        long res = 0;
-        int prefix = 0;
-        cnt.put(0, 1);
+        int[] diff = new int[n];
+        
+        // Precompute the diff array
         for (int i = 0; i < n; i++) {
-            prefix += nums.get(i) % modulo == k ? 1 : 0;
-            res += cnt.getOrDefault((prefix - k + modulo) % modulo, 0);
-            cnt.put(prefix % modulo, cnt.getOrDefault(prefix % modulo, 0) + 1);
+            diff[i] = (nums.get(i) % modulo == k) ? 1 : 0;
         }
-        return res;
+        
+        long count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int prefix = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefix += diff[i];
+            
+            int rem = (prefix - k + modulo) % modulo;
+            if (map.containsKey(rem)) {
+                count += map.get(rem);
+            }
+            
+            int currentMod = prefix % modulo;
+            map.put(currentMod, map.getOrDefault(currentMod, 0) + 1);
+        }
+        
+        return count;
     }
 }
