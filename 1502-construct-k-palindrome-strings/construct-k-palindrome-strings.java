0902b7e6dd72738cdfdf269d8bbc4f047536ec02
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-04-28 23:51:56 +0530

    Time: 4 ms (88.14%) | Memory: 45.7 MB (11.68%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -1,27 +1,52 @@
+// class Solution {
+
+//     public boolean canConstruct(String s, int k) {
+//         // Handle edge cases
+//         if (s.length() < k) return false;
+//         if (s.length() == k) return true;
+
+//         // Initialize frequency array and oddCount
+//         int[] freq = new int[26];
+//         int oddCount = 0;
+
+//         // Increment the value of the index corresponding to the current character
+//         for (char chr : s.toCharArray()) {
+//             freq[chr - 'a']++;
+//         }
+
+//         // Count the number of characters that appear an odd number of times in s
+//         for (int count : freq) {
+//             if (count % 2 == 1) {
+//                 oddCount++;
+//             }
+//         }
+
+//         // Return if the number of odd frequencies is less than or equal to k
+//         return oddCount <= k;
+//     }
+// }
+
+////way 2
+
 class Solution {
 
     public boolean canConstruct(String s, int k) {
-        // Handle edge cases
         if (s.length() < k) return false;
         if (s.length() == k) return true;
 
-        // Initialize frequency array and oddCount
-        int[] freq = new int[26];
-        int oddCount = 0;
+        int freq[]=new  int[26];
 
-        // Increment the value of the index corresponding to the current character
-        for (char chr : s.toCharArray()) {
-            freq[chr - 'a']++;
+        for(char c :s.toCharArray()){
+            freq[c-'a']++;
         }
+        int oddCount=0;
 
-        // Count the number of characters that appear an odd number of times in s
-        for (int count : freq) {
-            if (count % 2 == 1) {
+        for(int count:freq){
+            if(count%2!=0){
                 oddCount++;
             }
         }
 
-        // Return if the number of odd frequencies is less than or equal to k
-        return oddCount <= k;
+        return oddCount<=k;
     }
 }
