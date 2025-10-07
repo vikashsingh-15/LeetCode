c6b7586a4ea67c0b7a364f893f6d9db2d61b353f
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-10-08 02:03:03 +0530

    Time: 120 ms (14.61%) | Memory: 64.1 MB (11.42%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -38,12 +38,43 @@
 
 import java.util.*;
 
+// class Solution {
+//     public int[] avoidFlood(int[] rains) {
+//         int n = rains.length;
+//         int[] ans = new int[n];
+//         Map<Integer, Integer> full = new HashMap<>();
+//         TreeSet<Integer> dryDays = new TreeSet<>();
+
+//         for (int i = 0; i < n; i++) {
+//             if (rains[i] == 0) {
+//                 dryDays.add(i);
+//                 ans[i] = 1;
+//             } else {
+//                 int lake = rains[i];
+//                 ans[i] = -1;
+//                 if (full.containsKey(lake)) {   
+//                     Integer dryDay = dryDays.higher(full.get(lake));
+//                     // at this point dryDay s greted then previous lake day and smaller than new lake
+//                     if (dryDay == null) {
+//                         return new int[0];
+//                     }
+//                     ans[dryDay] = lake;
+//                     dryDays.remove(dryDay);
+//                 }
+//                 full.put(lake, i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int[] avoidFlood(int[] rains) {
         int n = rains.length;
         int[] ans = new int[n];
         Map<Integer, Integer> full = new HashMap<>();
-        TreeSet<Integer> dryDays = new TreeSet<>();
+        List<Integer> dryDays = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             if (rains[i] == 0) {
@@ -52,14 +83,27 @@ class Solution {
             } else {
                 int lake = rains[i];
                 ans[i] = -1;
-                if (full.containsKey(lake)) {   
-                    Integer dryDay = dryDays.higher(full.get(lake));
-                    if (dryDay == null) {
-                        return new int[0];
+
+                if (full.containsKey(lake)) {
+                    int lastRainDay = full.get(lake);
+                    Integer dryDay = null;
+
+                    // \U0001f50d Manually find the next dry day after lastRainDay
+                    for (int j = 0; j < dryDays.size(); j++) {
+                        if (dryDays.get(j) > lastRainDay) {
+                            dryDay = dryDays.get(j);
+                            dryDays.remove(j);
+                            break;
+                        }
                     }
-                    ans[dryDay] = lake;
-                    dryDays.remove(dryDay);
+
+                    if (dryDay == null) {
+                        return new int[0]; // flood unavoidable
+                    }
+
+                    ans[dryDay] = lake; // assign this dry day to dry the lake
                 }
+
                 full.put(lake, i);
             }
         }
