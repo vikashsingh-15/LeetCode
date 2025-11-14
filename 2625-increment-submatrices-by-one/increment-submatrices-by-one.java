4645994743b0c42e2424a0572ca4adde612c886b
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-11-15 00:46:01 +0530

    Time: 456 ms (23.25%) | Memory: 77.2 MB (16.23%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,27 +1,58 @@
+// class Solution {
+//     public int[][] rangeAddQueries(int n, int[][] queries) {
+//         int[][] diff = new int[n + 1][n + 1];
+
+//         // Apply 2D difference for each query
+//         for (int[] q : queries) {
+//             int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
+
+//             diff[r1][c1] += 1;
+//             if (c2 + 1 < n) diff[r1][c2 + 1] -= 1;
+//             if (r2 + 1 < n) diff[r2 + 1][c1] -= 1;
+//             if (r2 + 1 < n && c2 + 1 < n) diff[r2 + 1][c2 + 1] += 1;
+//         }
+
+//         // Prefix sum row-wise
+//         for (int r = 0; r < n; r++) {
+//             for (int c = 1; c < n; c++) {
+//                 diff[r][c] += diff[r][c - 1];
+//             }
+//         }
+
+//         // Prefix sum col-wise
+//         for (int c = 0; c < n; c++) {
+//             for (int r = 1; r < n; r++) {
+//                 diff[r][c] += diff[r - 1][c];
+//             }
+//         }
+
+//         // Build final matrix
+//         int[][] mat = new int[n][n];
+//         for (int r = 0; r < n; r++) {
+//             for (int c = 0; c < n; c++) {
+//                 mat[r][c] = diff[r][c];
+//             }
+//         }
+
+//         return mat;
+//     }
+// }
+
+
 class Solution {
-
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] diff = new int[n + 1][n + 1];
-        for (int[] q : queries) {
-            int row1 = q[0];
-            int col1 = q[1];
-            int row2 = q[2];
-            int col2 = q[3];
-            diff[row1][col1] += 1;
-            diff[row2 + 1][col1] -= 1;
-            diff[row1][col2 + 1] -= 1;
-            diff[row2 + 1][col2 + 1] += 1;
-        }
-
         int[][] mat = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int x1 = (i == 0) ? 0 : mat[i - 1][j];
-                int x2 = (j == 0) ? 0 : mat[i][j - 1];
-                int x3 = (i == 0 || j == 0) ? 0 : mat[i - 1][j - 1];
-                mat[i][j] = diff[i][j] + x1 + x2 - x3;
+
+        for (int[] q : queries) {
+            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
+
+            for (int r = r1; r <= r2; r++) {
+                for (int c = c1; c <= c2; c++) {
+                    mat[r][c]++;
+                }
             }
         }
+
         return mat;
     }
 }
