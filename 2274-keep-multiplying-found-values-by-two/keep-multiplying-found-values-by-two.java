79cd5f1c551ef54cec60246ffc67d2c5dfd11a46
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-11-19 21:36:21 +0530

    Time: 9 ms (7.96%) | Memory: 46.6 MB (7.36%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -13,16 +13,50 @@
 
 //way 2
 
-class Solution {
+// class Solution {
 
+//     public int findFinalValue(int[] nums, int original) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int num : nums) {
+//             set.add(num);
+//         }
+//         while (set.contains(original)) {
+//             original *= 2;
+//         }
+//         return original;
+//     }
+// }
+
+////way 3
+
+import java.util.Arrays;
+
+class Solution {
     public int findFinalValue(int[] nums, int original) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
-        }
-        while (set.contains(original)) {
+        Arrays.sort(nums);
+
+        while (binarySearch(nums, original)) {
             original *= 2;
         }
+
         return original;
+    }
+
+    private boolean binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
     }
 }
