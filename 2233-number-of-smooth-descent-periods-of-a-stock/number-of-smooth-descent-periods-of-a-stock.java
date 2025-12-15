faf5a21068fd1c50e743e31cb8970aaabb3ba9ef
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2025-12-15 10:25:10 +0530

    Time: 6 ms (3.70%) | Memory: 87.7 MB (5.19%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
@@ -1,20 +1,20 @@
-class Solution {
-    public long getDescentPeriods(int[] prices) {
-        long result = 1;   
-        long streak = 1;  
+// class Solution {
+//     public long getDescentPeriods(int[] prices) {
+//         long result = 1;   
+//         long streak = 1;  
 
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] == prices[i - 1] - 1) {
-                streak++;          
-            } else {
-                streak = 1;        
-            }
-            result += streak;  
-        }
+//         for (int i = 1; i < prices.length; i++) {
+//             if (prices[i] == prices[i - 1] - 1) {
+//                 streak++;          
+//             } else {
+//                 streak = 1;        
+//             }
+//             result += streak;  
+//         }
 
-        return result;
-    }
-}
+//         return result;
+//     }
+// }
 
 ////way 2-- TLE
 // class Solution {
@@ -36,3 +36,37 @@ class Solution {
 //         return count;
 //     }
 // }
+
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long[] memo = new long[prices.length];
+        long result = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            result += dfs(prices, i, memo);
+        }
+
+        return result;
+    }
+
+    private long dfs(int[] prices, int i, long[] memo) {
+        // Base case
+        if (i == 0) {
+            return memo[i] = 1;
+        }
+
+        // Already computed
+        if (memo[i] != 0) {
+            return memo[i];
+        }
+
+        // Recursive relation
+        if (prices[i] == prices[i - 1] - 1) {
+            memo[i] = dfs(prices, i - 1, memo) + 1;
+        } else {
+            memo[i] = 1;
+        }
+
+        return memo[i];
+    }
+}
