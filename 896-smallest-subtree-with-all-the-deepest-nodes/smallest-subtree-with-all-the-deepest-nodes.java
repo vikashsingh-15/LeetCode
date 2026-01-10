34669fc996e65c4114d5a7dc5c3a5a70e87482c7
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-01-11 01:02:02 +0530

    Time: 1 ms (21.35%) | Memory: 43.8 MB (9.46%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
@@ -57,36 +57,70 @@
 
 // }
 
+// ////way 2
+// class Solution {
+//     public TreeNode subtreeWithAllDeepest(TreeNode root) {
+//         return dfs(root).node;
+//     }
+
+//     private Pair dfs(TreeNode root) {
+//         if (root == null)
+//             return new Pair(null, 0);
+
+//         Pair left = dfs(root.left);
+//         Pair right = dfs(root.right);
+
+//         if (left.depth > right.depth)
+//             return new Pair(left.node, left.depth + 1);
+
+//         if (right.depth > left.depth)
+//             return new Pair(right.node, right.depth + 1);
+
+//         // equal depth -> this node contains all deepest nodes
+//         return new Pair(root, left.depth + 1);
+//     }
+
+//     class Pair {
+//         TreeNode node;
+//         int depth;
+
+//         Pair(TreeNode n, int d) {
+//             node = n;
+//             depth = d;
+//         }
+//     }
+// }
+
+
+////way 3
 
 class Solution {
-    public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        return dfs(root).node;
+    HashMap<TreeNode, Integer> depthMap = new HashMap<>();
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {   
+         dfs(root);                        
+         return helper(root);         
+     } 
+     private int dfs(TreeNode node){
+             if(node == null) 
+                return 0;
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        int depth = 1 + Math.max(left, right);
+        depthMap.put(node, depth);
+        return depth;    
+      }
+      
+    // finds the smallest subtree with all deepest nodes    
+    private TreeNode helper(TreeNode node){
+            if(node == null)
+                return null;
+        int leftDepth  = node.left  == null ? 0 : depthMap.get(node.left);
+        int rightDepth = node.right == null ? 0 : depthMap.get(node.right);
+        if(leftDepth == rightDepth)
+                    return node;
+        if(leftDepth > rightDepth)
+                    return helper(node.left);
+        return helper(node.right);
+         }
+         
     }
-
-    private Pair dfs(TreeNode root) {
-        if (root == null)
-            return new Pair(null, 0);
-
-        Pair left = dfs(root.left);
-        Pair right = dfs(root.right);
-
-        if (left.depth > right.depth)
-            return new Pair(left.node, left.depth + 1);
-
-        if (right.depth > left.depth)
-            return new Pair(right.node, right.depth + 1);
-
-        // equal depth -> this node contains all deepest nodes
-        return new Pair(root, left.depth + 1);
-    }
-
-    class Pair {
-        TreeNode node;
-        int depth;
-
-        Pair(TreeNode n, int d) {
-            node = n;
-            depth = d;
-        }
-    }
-}
