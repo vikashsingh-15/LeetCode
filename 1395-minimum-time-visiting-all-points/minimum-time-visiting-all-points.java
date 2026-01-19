a300c6d1c66432d6d997a6610a25c0b7dbf7b646
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-01-19 09:37:00 +0530

    Time: 1 ms (96.00%) | Memory: 44.7 MB (82.22%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -1,19 +1,44 @@
+// class Solution {
+//     public int minTimeToVisitAllPoints(int[][] points) {
+//         int ans = 0;
+//         for (int i = 0; i < points.length - 1; i++) {
+//             int currX = points[i][0];
+//             int currY = points[i][1];
+//             int targetX = points[i + 1][0];
+//             int targetY = points[i + 1][1];
+//             ans += Math.max(Math.abs(targetX - currX), Math.abs(targetY - currY));
+//         }
+        
+//         return ans;
+//     }
+// }
+
+////way 2
 class Solution {
-    
-    public int toTime(int[] from, int[] to) {
-        int xDiff = Math.abs(from[0] - to[0]);
-        int yDiff = Math.abs(from[1] - to[1]);
-        
-        return Math.max(xDiff, yDiff);
-    }
-    
     public int minTimeToVisitAllPoints(int[][] points) {
-        int time = 0;
-    
+        int totalTime = 0;
+
         for (int i = 1; i < points.length; i++) {
-            time += toTime(points[i - 1], points[i]);
-        }
+
+            int x1 = points[i - 1][0];
+            int y1 = points[i - 1][1];
+            int x2 = points[i][0];
+            int y2 = points[i][1];
+
+            int dx = Math.abs(x2 - x1);
+            int dy = Math.abs(y2 - y1);
+
+          
+            int diagonalMoves = Math.min(dx, dy);
+            dx -= diagonalMoves;
+            dy -= diagonalMoves;
+
         
-        return time;
+            int straightMoves = dx + dy; 
+
+            totalTime += diagonalMoves + straightMoves;
+        }
+
+        return totalTime;
     }
 }
