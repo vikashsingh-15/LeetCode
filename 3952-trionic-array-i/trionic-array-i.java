c6da3166849a667583b692f9dfeddaec50c2cdb1
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-02-03 20:15:43 +0530

    Time: 1 ms (72.27%) | Memory: 44.8 MB (49.58%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -1,22 +1,52 @@
+// class Solution {
+//     public boolean isTrionic(int[] nums) {
+//         int n = nums.length;
+//         if (n < 3) return false;
+
+//         int i = 0;
+
+//         //increasing
+//         while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+//         if (i == 0 || i >= n - 2) return false; 
+
+//         //decreasing
+//         int peak = i;
+//         while (i + 1 < n && nums[i] > nums[i + 1]) i++;
+//         if (i == peak || i >= n - 1) return false; 
+        
+//         //increasing
+//         while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+
+//         return i == n - 1;
+//     }
+// }
+
+
+
 class Solution {
     public boolean isTrionic(int[] nums) {
         int n = nums.length;
         if (n < 3) return false;
 
-        int i = 0;
+        int upPeak = 0;
+        int downPeak = 0;
 
-        //increasing
-        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
-        if (i == 0 || i >= n - 2) return false; 
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i - 1] == nums[i] || nums[i] == nums[i + 1]) {
+                return false;
+            }
+            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
+                upPeak++;
+            }
+            if (nums[i - 1] > nums[i] && nums[i] < nums[i + 1]) {
+                if (upPeak != 1) {
+                    return false; 
+                }
+                downPeak++;
+            }
+        }
 
-        //decreasing
-        int peak = i;
-        while (i + 1 < n && nums[i] > nums[i + 1]) i++;
-        if (i == peak || i >= n - 1) return false; 
-        
-        //increasing
-        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
-
-        return i == n - 1;
+        return upPeak == 1 && downPeak == 1;
     }
 }
+
