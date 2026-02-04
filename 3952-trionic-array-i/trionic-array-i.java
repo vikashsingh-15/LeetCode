63ea907f86480175ae7271fa6049644c024f2eb5
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-02-05 01:21:14 +0530

    Time: 1 ms (79.15%) | Memory: 45.1 MB (15.42%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -1,27 +1,3 @@
-// class Solution {
-//     public boolean isTrionic(int[] nums) {
-//         int n = nums.length;
-//         if (n < 3) return false;
-
-//         int i = 0;
-
-//         //increasing
-//         while (i + 1 < n && nums[i] < nums[i + 1]) i++;
-//         if (i == 0 || i >= n - 2) return false; 
-
-//         //decreasing
-//         int peak = i;
-//         while (i + 1 < n && nums[i] > nums[i + 1]) i++;
-//         if (i == peak || i >= n - 1) return false; 
-        
-//         //increasing
-//         while (i + 1 < n && nums[i] < nums[i + 1]) i++;
-
-//         return i == n - 1;
-//     }
-// }
-
-
 
 class Solution {
     public boolean isTrionic(int[] nums) {
@@ -49,4 +25,36 @@ class Solution {
         return upPeak == 1 && downPeak == 1;
     }
 }
+
+
+////way 2
+
+// class Solution {
+//     public boolean isTrionic(int[] nums) {
+//         int n = nums.length;
+//         if (n < 3) return false;
+
+//         int i = 0;
+
+       
+//         while (i + 1 < n && nums[i] < nums[i + 1]){
+//             i++;
+//         }
+//         if (i == 0 || i >= n - 2) return false; 
+
+//         int peak = i;
+//         while (i + 1 < n && nums[i] > nums[i + 1]){
+//             i++;
+//         }
+//         if (i == peak || i >= n - 1) return false; 
+        
+//         //increasing
+//         while (i + 1 < n && nums[i] < nums[i + 1]) {
+//             i++;
+//         }
+
+//         return i == n - 1;
+//     }
+// }
+
 
