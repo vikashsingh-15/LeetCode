668adef995db910304c1c004076ad7a7b460667a
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-02-24 18:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (57.04%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
@@ -12,27 +12,50 @@
  *         this.right = right;
  *     }
  * }
- */
+//  */
+// class Solution {
+//     public int sumRootToLeaf(TreeNode root) {
+//          return dfs(root, 0);
+//     }
+
+//      private int dfs(TreeNode node, int current) {
+//         if(node==null){
+//             return 0;
+//         }
+
+//         current=current*2+node.val;
+
+//         if(node.left==null  && node.right==null){
+//             return current;
+//         }
+
+//         int leftVal=dfs(node.left,current);
+//         int  rightVal=dfs(node.right,current);
+
+//         return leftVal+rightVal;
+
+//      }
+// }
+
+//way 2
+
 class Solution {
+
     public int sumRootToLeaf(TreeNode root) {
-         return dfs(root, 0);
+        return dfs(root, 0);
     }
 
-     private int dfs(TreeNode node, int current) {
-        if(node==null){
+    private int dfs(TreeNode node, int current) {
+        if (node == null)
             return 0;
-        }
 
-        current=current*2+node.val;
+        // build binary number
+        current = (current << 1) | node.val;
 
-        if(node.left==null  && node.right==null){
+        if (node.left == null && node.right == null)
             return current;
-        }
 
-        int leftVal=dfs(node.left,current);
-        int  rightVal=dfs(node.right,current);
-
-        return leftVal+rightVal;
-
-     }
+        return dfs(node.left, current)
+             + dfs(node.right, current);
+    }
 }
