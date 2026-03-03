ec7feb4a94573f2fbab7d9060fd173fa8801a63c
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-03-03 13:42:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (39.42%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,31 +1,55 @@
-class Solution {
-    public char findKthBit(int n, int k) {
-       boolean flip=false;
+// class Solution {
+//     public char findKthBit(int n, int k) {
+//        boolean flip=false;
 
-       if(k==1){
-        return '0';
-       }
+//        if(k==1){
+//         return '0';
+//        }
 
-       while(n>1){
-        int len=(1<<n)-1;
-        int mid=(len+1)/2;
+//        while(n>1){
+//         int len=(1<<n)-1;
+//         int mid=(len+1)/2;
 
-        if(k==mid){
-            return flip?'0':'1';
-        }
-        if(k>mid){
-            flip=!flip;
-            int eqvK= len - k + 1; 
-            k=eqvK;
-        }
-        n--;
-       }
+//         if(k==mid){
+//             return flip?'0':'1';
+//         }
+//         if(k>mid){
+//             flip=!flip;
+//             int eqvK= len - k + 1; 
+//             k=eqvK;
+//         }
+//         n--;
+//        }
     
-        return flip ? '1' : '0';
-    }
-}
+//         return flip ? '1' : '0';
+//     }
+// }
 
 
 ////way 2
 
+class Solution {
 
+    public char findKthBit(int n, int k) {
+
+        if (n == 1) {
+            return '0';
+        }
+
+        int len = (1 << n) - 1;
+        int mid = len / 2 + 1;
+
+        if (k == mid) {
+            return '1';
+        }
+
+        if (k < mid) {
+            return findKthBit(n - 1, k);
+        }
+
+        int mirrorIndex = len - k + 1;
+        char ch = findKthBit(n - 1, mirrorIndex);
+        return ch == '0' ? '1' : '0';
+
+    }
+}
