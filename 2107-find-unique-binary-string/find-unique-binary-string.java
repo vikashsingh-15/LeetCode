b5b0990709c127ef3dd388db7fe347fd0683bf13
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-03-08 23:25:49 +0530

    Time: 3 ms (29.39%) | Memory: 43.6 MB (20.75%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,111 +1,30 @@
-// class Solution {
-
-//     public String findDifferentBinaryString(String[] nums) {
-//         StringBuilder sb = new StringBuilder();
-
-//         for (int i = 0; i < nums.length; i++) {
-//             sb.append(nums[i].charAt(i) == '0' ? "1" : "0");
-//         }
-//         return new String(sb);
-//     }
-// }
-
-////way 2
-
-// class Solution {
-
-//     public String findDifferentBinaryString(String[] nums) {
-//         int n = nums.length;
-//         Set<Integer> set = new HashSet<>();
-//         String binaryString = "";
-//         for (String num : nums) {
-//             int decimalValue = Integer.parseInt(num, 2);
-//             set.add(decimalValue);
-//         }
-
-//         for (int i = 0; i <= 16; i++) {
-//             if (set.contains(i)) {
-//                 continue;
-//             } else {
-//                 binaryString = String.format("%" + n + "s", Integer.toBinaryString(i)).replace(' ', '0');
-//                 break;
-//             }
-//         }
-
-//         return binaryString;
-//     }
-// }
-
-
-////Way 3
-
-// class Solution {
-//     public String findDifferentBinaryString(String[] nums) {
-//         int n=nums[0].length();
-//         ArrayList<String>list=new ArrayList<>();
-//          HashSet<String> set = new HashSet<>();
-//         for (String num : nums) {
-//             set.add(num);
-//         }
-//         helper(n, new StringBuilder(),list);
-
-//         for (String binary : list) {
-//             if (!set.contains(binary)) {
-//                 return binary;
-//             }
-//         }
-
-//         return "";
-//     }
-
-//     public void helper(int n,StringBuilder current,ArrayList<String>list){
-//          if (current.length() == n) {
-//             list.add(current.toString());  // Add to the list
-//             return;
-//         }
-         
-//         helper(n,current.append(0),list);
-//         current.deleteCharAt(current.length() - 1);
-
-//         helper(n,current.append(1),list);
-//         current.deleteCharAt(current.length() - 1);
-//     }
-// }
-
-////way 4
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 class Solution {
-    public String findDifferentBinaryString(String[] nums) {
-        int n = nums[0].length();
-        HashSet<String> set = new HashSet<>();
-        for (String num : nums) {
-            set.add(num);
-        }
-
-        ArrayList<String> list = new ArrayList<>();
-        generateBinaryStrings(n, "", list);
-
-        // Find a string that is not in the original set
-        for (String binary : list) {
-            if (!set.contains(binary)) {
-                return binary;
+    int n;
+    Set<String> numsSet = new HashSet();
+    
+    private String generate(String curr) {
+        if (curr.length() == n) {
+            if (!numsSet.contains(curr)) {
+                return curr;
             }
+            
+            return "";
         }
-
-        return "";  // This line should never be reached
+        
+        String addZero = generate(curr + "0");
+        if (addZero.length() > 0) {
+            return addZero;
+        }
+        
+        return generate(curr + "1");
     }
-
-    public void generateBinaryStrings(int n, String current, ArrayList<String> list) {
-        if (current.length() == n) {
-            list.add(current);
-            return;
+    
+    public String findDifferentBinaryString(String[] nums) {
+        n = nums.length;
+        for (String s : nums) {
+            numsSet.add(s);
         }
-
-        generateBinaryStrings(n, current + "0", list);
-        generateBinaryStrings(n, current + "1", list);
+        
+        return generate("");
     }
 }
