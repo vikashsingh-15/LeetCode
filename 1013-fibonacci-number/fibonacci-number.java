64c03493f016d58e541e1a890cbd1574d032c7bb
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-03-10 20:06:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (68.20%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,18 +1,66 @@
+// class Solution {
+
+//     public int fib(int n) {
+//         if(n<1){
+//             return n;
+//         }
+//        int  prev_2=0;//two no back prev 2
+//        int prev_1=1;// onw no back prev 
+
+//        for(int i=2;i<=n;i++){
+//         int current=prev_1+prev_2;
+//         prev_2=prev_1;
+//         prev_1=current;
+
+//        }
+//         return prev_1;
+//     }
+// }
+
+//way 2
+
+// class Solution {
+
+//     public int fib(int n) {
+//         if (n <= 1) {
+//             return n;
+//         }
+//         return fib(n - 1) + fib(n - 2);
+
+//         //way3
+//         // if(n==0 || n==1){
+//         //     return n;
+//         // }
+//         // int fibn1=fib(n-1);
+//         // int fibn2=fib(n-2);
+//         // int fibn=fibn1+fibn2;
+//         // return fibn;
+//     }
+// }
+
+
+////way  3
+
+
 class Solution {
 
     public int fib(int n) {
-        if(n<1){
+        int dp[] = new int[n + 1];
+        return fibMemoize(n, dp);
+    }
+
+    public int fibMemoize(int n, int[] dp) {
+        if (n == 0 || n == 1) {
             return n;
         }
-       int  prev_2=0;//two no back prev 2
-       int prev_1=1;// onw no back prev 
+        if (dp[n] != 0) {
+            return dp[n];
+        }
 
-       for(int i=2;i<=n;i++){
-        int current=prev_1+prev_2;
-        prev_2=prev_1;
-        prev_1=current;
-
-       }
-        return prev_1;
+        int fibn1 = fibMemoize(n - 1,dp);
+        int fibn2 = fibMemoize(n - 2,dp);
+        int fibn = fibn1 + fibn2;
+        dp[n] = fibn;
+        return fibn;
     }
 }
