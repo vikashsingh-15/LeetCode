a87fe5ab56f2d7247136f3fbbcc11d61b2e4a2bc
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-03-11 16:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (50.09%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,17 +1,43 @@
+// class Solution {
+//     public int bitwiseComplement(int n) {
+//           if(n == 0){
+//             return 1;
+//         }
+
+//         int mask = 0;
+//         int temp = n;
+
+//         while(temp > 0){
+//             mask = (mask << 1) | 1;
+//             temp >>= 1;
+//         }
+
+//         return mask ^ n;
+//     }
+// }
+
+////way  2
+
 class Solution {
     public int bitwiseComplement(int n) {
-          if(n == 0){
-            return 1;
+
+        if(n == 0) return 1;
+
+        int ans = 0;
+        int pos = 0;
+
+        while(n > 0){
+
+            int bit = n & 1;      // get last bit
+            int flipped = bit ^ 1; // flip bit
+
+            ans = ans | (flipped << pos);
+
+            pos++;
+
+            n = n >> 1; // remove last bit
         }
 
-        int mask = 0;
-        int temp = n;
-
-        while(temp > 0){
-            mask = (mask << 1) | 1;
-            temp >>= 1;
-        }
-
-        return mask ^ n;
+        return ans;
     }
 }
