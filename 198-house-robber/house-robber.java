3cd31a1643fcfef1c6f958ac0d1f95e8f69afac4
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-03-12 14:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (93.30%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,37 +1,14 @@
-////case 1: TLE
-// class Solution {
-//     int dp[]=new int[101];
-//     public int rob(int[] nums) {
-//         return helper(nums,nums.length,0);
-//     }
-
-//     public int helper(int[] nums,int n,int i){
-//         if(i>=n){
-//             return 0;
-//         }
-
-//         if(dp[i]!=0){
-//             return dp[i];
-//         }
-
-//         int steal=nums[i]+helper(nums,n,i+2);
-//         int skip=helper(nums,n,i+1);
-//         return dp[i]=Math.max(steal,skip);
-//     }
-// }
-
-// CASE 2
-
 class Solution {
     public int rob(int[] nums) {
-       int rob = 0;
-        int norob = 0;
-        for (int i = 0; i < nums.length; i ++) {
-            int newRob = norob + nums[i];
-            int newNoRob = Math.max(norob, rob);
+        int rob=0;
+        int noRob=0;
+
+        for(int i=0;i<nums.length;i++){
+            int newRob=noRob+nums[i];
+            int newNoRob=Math.max(rob,noRob);
             rob = newRob;
-            norob = newNoRob;
+            noRob = newNoRob;
         }
-        return Math.max(rob, norob);
+        return Math.max(rob, noRob);
     }
 }
