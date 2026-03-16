7a201e22529636eb4cbac64d71a8bc7086edfb93
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-03-16 23:53:48 +0530

    Time: 3 ms (56.95%) | Memory: 43.6 MB (53.89%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,90 +1,15 @@
-// class Solution {
-//     public String getHappyString(int n, int k) {
-
-//         ArrayList<String> str = new ArrayList<>();
-//         ArrayList<ArrayList<String>> list = new ArrayList<>();
-
-//         helper(0, n, str, list);
-//         if (k > list.size()) {
-//             return "";
-//         }
-//           StringBuilder result = new StringBuilder();
-//         for (String ch : list.get(k - 1)) {  
-//             result.append(ch);
-//         }
-//         return result.toString();
-//     }
-
-//     public void helper(int index, int n, ArrayList<String> current, ArrayList<ArrayList<String>> list) {
-//         if (current.size() == n) {
-//             list.add(new ArrayList<>(current));
-//             return;
-//         }
-//         String chars = "abc";
-
-//         for (int i = 0; i < chars.length(); i++) {
-//             char c = chars.charAt(i);
-
-//             if (current.isEmpty() || current.get(current.size() - 1).charAt(0) != c) {
-//                 current.add(String.valueOf(c));
-//                 helper(index + 1, n, current, list);
-//                 current.remove(current.size() - 1);
-//             }
-
-//         }
-//     }
-
-// }
-
-
-////
-
-// import java.util.ArrayList;
-
-// class Solution {
-//     public String getHappyString(int n, int k) {
-//         ArrayList<String> list = new ArrayList<>();
-//         helper(0, n, new StringBuilder(), list);
-//         if (k > list.size()) {
-//             return "";
-//         }
-//         return list.get(k - 1);
-//     }
-
-//     public void helper(int index, int n, StringBuilder current, ArrayList<String> list) {
-//         if (current.length() == n) {
-//             list.add(current.toString());
-//             return;
-//         }
-
-//         String chars = "abc";
-//         for (int i = 0; i < chars.length(); i++) {
-//             char c = chars.charAt(i);
-//             if (current.length() == 0 || current.charAt(current.length() - 1) != c) {
-//                 current.append(c);
-//                 helper(index + 1, n, current, list);
-//                 current.deleteCharAt(current.length() - 1);
-//             }
-//         }
-//     }
-// }
-
-////way 3
-
-
-import java.util.ArrayList;
 
 class Solution {
     public String getHappyString(int n, int k) {
         ArrayList<String> list = new ArrayList<>();
-        helper(0, n, new StringBuilder(), list,k);
+        helper(n, new StringBuilder(), list,k);
         if (k > list.size()) {
             return "";
         }
         return list.get(k - 1);
     }
 
-    public void helper(int index, int n, StringBuilder current, ArrayList<String> list,int k) {
+    public void helper( int n, StringBuilder current, ArrayList<String> list,int k) {
         if (current.length() == n) {
             list.add(current.toString());
             return;
@@ -95,12 +20,13 @@ class Solution {
 
         String chars = "abc";
         for (int i = 0; i < chars.length(); i++) {
-            char c = chars.charAt(i);
-            if (current.length() == 0 || current.charAt(current.length() - 1) != c) {
-                current.append(c);
-                helper(index + 1, n, current, list,k);
-                current.deleteCharAt(current.length() - 1);
-            }
+            char ch=chars.charAt(i);
+
+           if(current.length()==0 || current.charAt(current.length()-1)!=ch){
+            current.append(ch);
+            helper(n,current,list,k);
+            current.deleteCharAt(current.length()-1);
+           }
         }
     }
 }
