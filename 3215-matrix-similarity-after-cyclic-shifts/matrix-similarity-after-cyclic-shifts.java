713a7440348ebe5ab816612ba0363321378d33e8
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-03-28 01:11:56 +0530

    Time: 1 ms (99.47%) | Memory: 47.1 MB (41.07%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -1,26 +1,68 @@
+// class Solution {
+//     public boolean areSimilar(int[][] mat, int k) {
+//         int m = mat.length;
+//         int n = mat[0].length;
+
+//         int shift = k % n;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+
+//                 if (i % 2 == 0) {
+//                     if (mat[i][j] != mat[i][(j + shift) % n]) {
+//                         return false;
+//                     }
+//                 } else {
+//                     if (mat[i][j] != mat[i][(j - shift + n) % n]) {
+//                         return false;
+//                     }
+//                 }
+
+//             }
+//         }
+
+//         return true;
+//     }
+// }
+
+////way 2 ->  The row must be periodic with period = k so even if we do ony left shift or right hsift it should pass
+
+// class Solution {
+
+//     public boolean areSimilar(int[][] mat, int k) {
+//         int m = mat.length;
+//         int n = mat[0].length;
+//         k %= n;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (mat[i][j] != mat[i][(j + k) % n]) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+////way 3
+
 class Solution {
+
     public boolean areSimilar(int[][] mat, int k) {
         int m = mat.length;
         int n = mat[0].length;
-
-        int shift = k % n;
+        k %= n;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
-                if (i % 2 == 0) {
-                    if (mat[i][j] != mat[i][(j + shift) % n]) {
-                        return false;
-                    }
-                } else {
-                    if (mat[i][j] != mat[i][(j - shift + n) % n]) {
-                        return false;
-                    }
+                if (mat[i][j] != mat[i][(j - k+n) % n]) {
+                    return false;
                 }
-
             }
         }
-
         return true;
     }
 }
+
