cd2f1eeca381d4d40a017204d70141e37c1fc454
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-03-30 01:21:37 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (87.50%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,9 +1,39 @@
+// class Solution {
+//     public boolean canBeEqual(String s1, String s2) {
+//          return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+//                 (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
+//                ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+//                 (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
+   
+//     }
+// }
+
+//way 2
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-         return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
-                (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
-               ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
-                (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
-   
+        if (s1.length() != s2.length()) return false;
+        return check(s1, s2);
+    }
+
+    public boolean check(String s1, String s2) {
+        int n = s1.length();
+        boolean[] used = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+
+            // search only in same parity group
+            for (int j = i % 2; j < n; j += 2) {
+                if (!used[j] && s1.charAt(i) == s2.charAt(j)) {
+                    used[j] = true;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) return false;
+        }
+
+        return true;
     }
 }
