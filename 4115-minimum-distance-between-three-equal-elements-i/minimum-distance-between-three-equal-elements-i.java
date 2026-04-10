a03944a350527bd8ce1da6e942982901cb446f9c
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-04-11 01:23:32 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (59.48%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -6,15 +6,15 @@ class Solution {
 
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
-                if (nums[i] != nums[j]) {
-                    continue;
-                }
-                for (int k = j + 1; k < n; k++) {
+                if (nums[i] == nums[j]) {
+                    for (int k = j + 1; k < n; k++) {
                     if (nums[j] == nums[k]) {
                         ans = Math.min(ans, k - i);
                         break;
                     }
                 }
+                }
+                
             }
         }
 
