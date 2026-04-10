a91b6992347006fdaf60a78c8c0061238c1279fc
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-04-11 01:30:32 +0530

    Time: 6 ms (47.06%) | Memory: 44.5 MB (59.48%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -1,23 +1,46 @@
-class Solution {
+// class Solution {
 
+//     public int minimumDistance(int[] nums) {
+//         int n = nums.length;
+//         int ans = n + 1;
+
+//         for (int i = 0; i < n - 2; i++) {
+//             for (int j = i + 1; j < n - 1; j++) {
+//                 if (nums[i] == nums[j]) {
+//                     for (int k = j + 1; k < n; k++) {
+//                     if (nums[j] == nums[k]) {
+//                         ans = Math.min(ans, k - i);
+//                         break;
+//                     }
+//                 }
+//                 }
+                
+//             }
+//         }
+
+//         return ans == n + 1 ? -1 : ans * 2;
+//     }
+// }
+
+////way 2
+
+class Solution {
     public int minimumDistance(int[] nums) {
         int n = nums.length;
-        int ans = n + 1;
+        int ans = Integer.MAX_VALUE;
 
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
-                if (nums[i] == nums[j]) {
-                    for (int k = j + 1; k < n; k++) {
-                    if (nums[j] == nums[k]) {
-                        ans = Math.min(ans, k - i);
-                        break;
+                for (int k = j + 1; k < n; k++) {
+                    
+                    if (nums[i] == nums[j] && nums[j] == nums[k]) {
+                        int dist = (j - i) + (k - j) + (k - i); // simple formula
+                        ans = Math.min(ans, dist);
                     }
                 }
-                }
-                
             }
         }
 
-        return ans == n + 1 ? -1 : ans * 2;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
