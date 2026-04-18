fb63371dc550dea0c0a498912bf6871ec4f1b08d
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-04-18 23:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (99.11%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,96 +1,20 @@
 class Solution {
-
     public int singleNonDuplicate(int[] nums) {
-        // int left = 0, right = nums.length - 1;
-        // while (left < right) {
-        //     int mid = (left + right) / 2;
-        //     if (mid % 2 == 1) {
-        //         mid--;
-        //     }
-        //     if (nums[mid] != nums[mid + 1]) {
-        //         right = mid;
-        //     } else {
-        //         left = mid + 2;
-        //     }
-        // }
-        // return nums[left];
+        int lo = 0, hi = nums.length - 1;
 
-        //way 2
-
-        //  int ans = 0;
-        // for (int i = 0; i < nums.length; i++) {
-        //     ans = ans ^ nums[i];
-        // }
-        // return ans;
-
-        //         //way 3
-        // int left = 0;
-        // int right = nums.length - 1;
-        // int n = nums.length;
-
-        // if (n == 1) {
-        //     return nums[0];
-        // } else if (nums[0] != nums[1]) {
-        //     return nums[0];
-        // } else if (nums[n - 1] != nums[n - 2]) {
-        //     return nums[n - 1];
-        // }
-
-        // while (left < right) {
-        //     int mid = left + (right - left) / 2;
-
-        //     if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
-        //         return nums[mid];
-        //     }
-        //     else if(nums[mid]==nums[mid-1]){
-        //         int leftCount=mid-left+1; //no of element left of mid mid included.
-        //         if(leftCount%2==0){//eg 11224466899 //missing element odd no of element ke side hoga
-        //             left=mid+1;
-        //         }else{
-        //             right=mid-2;
-        //         }
-
-        //     }else if (nums[mid]==nums[mid+1]){
-        //         int rightCount=right-mid+1;//no of element left of mid mid included.
-        //         if(rightCount%2==0){//eg 11224668899 //missing element odd no of element ke side hoga
-        //             right=mid-1;
-        //         }else{
-        //             left=mid+2;
-        //         }
-        //     }
-        // }
-
-        // return nums[left];
-
-
-
-        //way 4
-         int left = 0;
-        int right = nums.length - 1;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            // Make sure mid is even (start of a pair)
+            if (mid % 2 == 1) {
+                mid--;
+            }
             if (nums[mid] == nums[mid + 1]) {
-                // If mid is even, the left side has pairs, and single is on the right
-                if (mid % 2 == 0) {
-                    left = mid + 2;
-                } else {
-                    // If mid is odd, the right side has pairs, and single is on the left
-                    right = mid - 1;
-                }
+                lo = mid + 2;
             } else {
-                // If mid is even, the left side has pairs, and single is on the left
-                if (mid % 2 == 0) {
-                    right = mid;
-                } else {
-                    // If mid is odd, the right side has pairs, and single is on the right
-                    left = mid + 1;
-                }
+                hi = mid;
             }
         }
 
-        return nums[left];
-
+        return nums[lo];
     }
 }
