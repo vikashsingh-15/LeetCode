91f2271687ecb1b2db27cbc601ce6c67a89958a3
Vikash Singh <55023442+vikashsingh-15@users.noreply.github.com>
2026-05-11 02:32:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (40.87%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,34 @@
+// class Solution {
+
+//     public boolean rotateString(String s, String goal) {
+       
+//         if (s.length() != goal.length()) return false;
+
+     
+//         String doubledString = s + s;
+
+//         return doubledString.contains(goal);
+//     }
+// }
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        int n = s.length();
+        for (int shift = 0; shift < n; shift++) {
+            boolean match = true;
+            for (int i = 0; i < n; i++) {
+                if (s.charAt((i + shift) % n) != goal.charAt(i)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
